e7aae05b64ef230114b5c16411cf610e6085e68b
Neon <neon@beyond-aion.com>
2015-08-30 14:56:20 +0200

    fixed whisper to GM with special custom tags (group invite was also affected)

--- game-server/src/com/aionemu/gameserver/utils/ChatUtil.java
+++ game-server/src/com/aionemu/gameserver/utils/ChatUtil.java
@@ -1,5 +1,6 @@
 package com.aionemu.gameserver.utils;
 
+import com.aionemu.gameserver.configs.administration.AdminConfig;
 import com.aionemu.gameserver.world.WorldPosition;
 
 /**
@@ -27,12 +28,32 @@ public class ChatUtil {
 	public static String quest(int questId) {
 		return String.format("[quest: %d]", questId);
 	}
-	
+
 	public static String getRealAdminName(String name) {
-		int index = name.lastIndexOf(" ");
-		if (index == -1)
+		// don't perform expensive checks if name is already qualified
+		if (name.matches("^[A-Za-z]+$"))
 			return name;
-		return name.substring(index + 1);	
+
+		String nameFormat;
+		String nameFlag = "%s";
+		for (int i = 1; i < 99; i++) {
+			try {
+				nameFormat = AdminConfig.class.getField("CUSTOMTAG_ACCESS" + i).get(null).toString();
+				if (nameFormat != null && nameFormat.contains(nameFlag)) {
+					int nameStartIndex = nameFormat.indexOf(nameFlag);
+					String namePrefix = nameFormat.substring(0, nameStartIndex > 0 ? nameStartIndex : 0);
+					String nameSuffix = nameFormat.substring(nameStartIndex + nameFlag.length(), nameFormat.length());
+					if ((namePrefix+nameSuffix).length() > 0 && name.startsWith(namePrefix) && name.endsWith(nameSuffix)) {
+						int endIndex = name.indexOf(nameSuffix) - 1;
+						name = name.substring(namePrefix.length(), endIndex > 0 ? endIndex : name.length());
+						break;
+					}
+				}
+			} catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException | SecurityException e) {
+				break; // (expecting NoSuchFieldException to trigger loop break)
+			}
+		}
+		
+		return name;
 	}
-	
 }
