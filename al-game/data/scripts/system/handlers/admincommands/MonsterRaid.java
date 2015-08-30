be973d62526b9e2516ee506e06566d28373f71a8
Neon <neon@beyond-aion.com>
2015-08-30 16:27:37 +0200

    adapted monsterraid command to new chat command system

--- al-game/data/scripts/system/handlers/admincommands/MonsterRaid.java
+++ al-game/data/scripts/system/handlers/admincommands/MonsterRaid.java
@@ -4,64 +4,53 @@ import org.apache.commons.lang3.math.NumberUtils;
 
 import com.aionemu.gameserver.model.gameobjects.player.Player;
 import com.aionemu.gameserver.services.MonsterRaidService;
-import com.aionemu.gameserver.utils.PacketSendUtility;
 import com.aionemu.gameserver.utils.chathandlers.AdminCommand;
-
 
 /**
  * @author Whoop
- *
  */
 public class MonsterRaid extends AdminCommand {
-	
+
 	private static final String START = "start";
 	private static final String STOP = "stop";
 
 	public MonsterRaid() {
-		super("monsterraid");
+		super("monsterraid", "Starts or stops the Beritra Invasion event");
+		
+		setParamInfo("<start|stop> <location id> - starts or stops the Beritra Invasion event for a specific location");
 	}
 
 	@Override
-	public void execute(Player admin, String... params) {
+	public void execute(Player player, String... params) {
 		if (params.length != 2 || !NumberUtils.isDigits(params[1])) {
-			onFail(admin, "SYNTAX: //monsterraid start|stop <LocationId>");
+			sendInfo(player);
 			return;
 		}
-		
+
 		int raidLocId = NumberUtils.toInt(params[1]);
-		if (!isValidRaidLocation(admin, raidLocId))
+		if (!isValidRaidLocation(raidLocId)) {
+			sendInfo(player, "ID " + raidLocId + " is invalid");
 			return;
+		}
 
 		if (START.equalsIgnoreCase(params[0])) {
 			if (MonsterRaidService.getInstance().isRaidInProgress(raidLocId)) {
-				PacketSendUtility.sendMessage(admin, "Raid Location " + raidLocId + " is already active");
-			}
-			else {
-				PacketSendUtility.sendMessage(admin, "Raid Location " + raidLocId + " - starting monster raid!");
+				sendInfo(player, "Raid Location " + raidLocId + " is already active");
+			} else {
+				sendInfo(player, "Raid Location " + raidLocId + " - starting monster raid!");
 				MonsterRaidService.getInstance().startRaid(raidLocId);
-			}			
+			}
 		} else if (STOP.equalsIgnoreCase(params[0])) {
 			if (!MonsterRaidService.getInstance().isRaidInProgress(raidLocId)) {
-				PacketSendUtility.sendMessage(admin, "Raid Location " + raidLocId + " is not active");
-			}
-			else {
-				PacketSendUtility.sendMessage(admin, "Raid Location " + raidLocId + " - stopping monster raid!");
+				sendInfo(player, "Raid Location " + raidLocId + " is not active");
+			} else {
+				sendInfo(player, "Raid Location " + raidLocId + " - stopping monster raid!");
 				MonsterRaidService.getInstance().stopRaid(raidLocId);
 			}
 		}
 	}
-	
-	private boolean isValidRaidLocation(Player player, int locId) {
-		
-		if (!MonsterRaidService.getInstance().getMonsterRaidLocations().keySet().contains(locId)){
-			PacketSendUtility.sendMessage(player, "Id " + locId + " is invalid");
-			return false;
-		}			
-		return true;
-	}
-	
-	@Override
-	public void onFail(Player player, String msg) {
-		PacketSendUtility.sendMessage(player, msg);
+
+	private boolean isValidRaidLocation(int locId) {
+		return MonsterRaidService.getInstance().getMonsterRaidLocations().keySet().contains(locId);
 	}
 }
