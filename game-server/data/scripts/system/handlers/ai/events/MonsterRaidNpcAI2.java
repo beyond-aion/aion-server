f57ec2f50d41e4aaa7d66c06ccb3544a85d03b32
Neon <neon@beyond-aion.com>
2015-09-08 15:39:15 +0200

    fixed buggy file staging status via eclipse git (different directory cases)

---
+++ game-server/data/scripts/system/handlers/ai/events/MonsterRaidNpcAI2.java
@@ -0,0 +1,15 @@
+package ai.events;
+
+import ai.AggressiveNpcAI2;
+
+import com.aionemu.gameserver.ai2.AIName;
+
+
+/**
+ * @author Whoop
+ *
+ */
+@AIName("monsterraid")
+public class MonsterRaidNpcAI2 extends AggressiveNpcAI2 {
+	//TODO: Interesting Fight
+}
