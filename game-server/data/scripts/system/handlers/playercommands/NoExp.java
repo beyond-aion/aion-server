0431776d1433c4d5fcebd586555a262738b7512a
Neon <neon@beyond-aion.com>
2015-10-12 08:49:38 +0200

    added description to noexp command

---
+++ game-server/data/scripts/system/handlers/playercommands/NoExp.java
@@ -0,0 +1,27 @@
+package playercommands;
+
+import java.awt.Color;
+
+import com.aionemu.gameserver.model.gameobjects.player.Player;
+import com.aionemu.gameserver.model.gameobjects.player.PlayerCommonData;
+import com.aionemu.gameserver.utils.ChatUtil;
+import com.aionemu.gameserver.utils.chathandlers.PlayerCommand;
+
+/**
+ * @author Wakizashi
+ * @modified Neon
+ */
+public class NoExp extends PlayerCommand {
+
+	public NoExp() {
+		super("noexp", "Enables/disables your ability to gain experience.");
+	}
+
+	@Override
+	public void execute(Player player, String... params) {
+		PlayerCommonData pcd = player.getCommonData();
+
+		pcd.setNoExp(!pcd.getNoExp());
+		sendInfo(player, "Experience rewards are now " + (pcd.getNoExp() ? ChatUtil.color("inactive", Color.RED) : ChatUtil.color("active", Color.GREEN)) + ".");
+	}
+}
