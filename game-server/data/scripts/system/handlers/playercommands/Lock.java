37b8b4c65d841eccaa56d91aa81189209401ba82
Neon <neon@beyond-aion.com>
2015-10-14 02:11:42 +0200

    added description to lock command, removed ip locking since its pointless

---
+++ game-server/data/scripts/system/handlers/playercommands/Lock.java
@@ -0,0 +1,45 @@
+package playercommands;
+
+import com.aionemu.gameserver.model.gameobjects.player.Player;
+import com.aionemu.gameserver.network.loginserver.LoginServer;
+import com.aionemu.gameserver.network.loginserver.serverpackets.SM_CHANGE_ALLOWED_HDD_SERIAL;
+import com.aionemu.gameserver.utils.chathandlers.PlayerCommand;
+
+/**
+ * @author ViAl
+ * @modified Neon
+ */
+public class Lock extends PlayerCommand {
+
+	public Lock() {
+		super("lock", "Enables/disables blocking logins from other computers.");
+
+		setParamInfo(
+			"<enable> - Allows login from only this computer.",
+			"<disable> - Allows login from any computer."
+		);
+	}
+
+	@Override
+	public void execute(Player player, String... params) {
+		if (params.length == 0) {
+			sendInfo(player);
+			return;
+		}
+
+		if ("enable".equalsIgnoreCase(params[0])) {
+			String hddSerial = player.getClientConnection().getHddSerial();
+			if (hddSerial == null || hddSerial.isEmpty()) {
+				sendInfo(player, "Couldn't lock your account. Please re-log and try again.");
+				return;
+			}
+			LoginServer.getInstance().sendPacket(new SM_CHANGE_ALLOWED_HDD_SERIAL(player.getPlayerAccount().getId(), hddSerial));
+			sendInfo(player, "Your account is now locked. You will not be able to login from any other computer from now on.");
+		} else if ("disable".equalsIgnoreCase(params[0])) {
+			LoginServer.getInstance().sendPacket(new SM_CHANGE_ALLOWED_HDD_SERIAL(player.getPlayerAccount().getId(), ""));
+			sendInfo(player, "Your account is unlocked. You can login from any computer again.");
+		} else {
+			sendInfo(player, "Invalid parameter.");
+		}
+	}
+}
