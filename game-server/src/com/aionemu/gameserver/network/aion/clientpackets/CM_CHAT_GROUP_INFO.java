476d28ba03ff1aefb25a57af5646a1560715004b
Neon <neon@beyond-aion.com>
2015-10-25 07:35:06 +0100

    small client packet improvements

--- game-server/src/com/aionemu/gameserver/network/aion/clientpackets/CM_CHAT_GROUP_INFO.java
+++ game-server/src/com/aionemu/gameserver/network/aion/clientpackets/CM_CHAT_GROUP_INFO.java
@@ -6,7 +6,6 @@ import com.aionemu.gameserver.network.aion.AionConnection.State;
 import com.aionemu.gameserver.network.aion.serverpackets.SM_CHAT_WINDOW;
 import com.aionemu.gameserver.network.aion.serverpackets.SM_SYSTEM_MESSAGE;
 import com.aionemu.gameserver.utils.ChatUtil;
-import com.aionemu.gameserver.utils.PacketSendUtility;
 import com.aionemu.gameserver.world.World;
 
 /**
@@ -31,12 +30,11 @@ public class CM_CHAT_GROUP_INFO extends AionClientPacket {
 
 	@Override
 	protected void runImpl() {
-		Player player = getConnection().getActivePlayer();
 		Player target = World.getInstance().findPlayer(ChatUtil.getRealAdminName(playerName));
 		if (target == null) {
-			PacketSendUtility.sendPacket(player, SM_SYSTEM_MESSAGE.STR_MSG_ASK_PCINFO_LOGOFF);
+			sendPacket(SM_SYSTEM_MESSAGE.STR_NO_SUCH_USER(playerName));
 			return;
 		}
-		PacketSendUtility.sendPacket(player, new SM_CHAT_WINDOW(target, true));
+		sendPacket(new SM_CHAT_WINDOW(target, true));
 	}
 }
--- game-server/src/com/aionemu/gameserver/network/aion/clientpackets/CM_CHAT_PLAYER_INFO.java
+++ game-server/src/com/aionemu/gameserver/network/aion/clientpackets/CM_CHAT_PLAYER_INFO.java
@@ -6,7 +6,6 @@ import com.aionemu.gameserver.network.aion.AionConnection.State;
 import com.aionemu.gameserver.network.aion.serverpackets.SM_CHAT_WINDOW;
 import com.aionemu.gameserver.network.aion.serverpackets.SM_SYSTEM_MESSAGE;
 import com.aionemu.gameserver.utils.ChatUtil;
-import com.aionemu.gameserver.utils.PacketSendUtility;
 import com.aionemu.gameserver.world.World;
 
 /**
@@ -23,18 +22,17 @@ public class CM_CHAT_PLAYER_INFO extends AionClientPacket {
 
 	@Override
 	protected void readImpl() {
-		playerName = ChatUtil.getRealAdminName(readS());
+		playerName = readS();
 	}
 
 	@Override
 	protected void runImpl() {
-		Player player = getConnection().getActivePlayer();
 		Player target = World.getInstance().findPlayer(ChatUtil.getRealAdminName(playerName));
 		if (target == null) {
-			PacketSendUtility.sendPacket(player, SM_SYSTEM_MESSAGE.STR_MSG_ASK_PCINFO_LOGOFF);
+			sendPacket(SM_SYSTEM_MESSAGE.STR_NO_SUCH_USER(playerName));
 			return;
 		}
-		if (!player.getKnownList().knowns(target))
-			PacketSendUtility.sendPacket(player, new SM_CHAT_WINDOW(target, false));
+		if (!getConnection().getActivePlayer().getKnownList().knowns(target))
+			sendPacket(new SM_CHAT_WINDOW(target, false));
 	}
 }
