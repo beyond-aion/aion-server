f256f78aa64f3ee0fc95798fcd49605d90ac672e
Neon <neon@beyond-aion.com>
2015-11-19 03:00:54 +0100

    fixed #BA343 and added check to abort on packet hack https://beyond-aion.com/forum/thread/343-item-164020006-integrierte-r%C3%BCckkehr-schriftrolle/

--- game-server/src/com/aionemu/gameserver/model/templates/item/actions/MultiReturnAction.java
+++ game-server/src/com/aionemu/gameserver/model/templates/item/actions/MultiReturnAction.java
@@ -3,6 +3,7 @@ package com.aionemu.gameserver.model.templates.item.actions;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlTransient;
 import javax.xml.bind.annotation.XmlType;
 
 import com.aionemu.gameserver.controllers.observer.ItemUseObserver;
@@ -12,6 +13,7 @@ import com.aionemu.gameserver.model.TaskId;
 import com.aionemu.gameserver.model.gameobjects.Item;
 import com.aionemu.gameserver.model.gameobjects.player.Player;
 import com.aionemu.gameserver.model.templates.item.ReturnLocList;
+import com.aionemu.gameserver.network.aion.serverpackets.SM_ITEM_USAGE_ANIMATION;
 import com.aionemu.gameserver.network.aion.serverpackets.SM_SYSTEM_MESSAGE;
 import com.aionemu.gameserver.services.teleport.TeleportService2;
 import com.aionemu.gameserver.utils.PacketSendUtility;
@@ -27,6 +29,9 @@ public class MultiReturnAction extends AbstractItemAction {
 	@XmlAttribute(name = "id")
 	protected int id;
 
+	@XmlTransient
+	private static final short USAGE_DELAY = 5000;
+
 	@Override
 	public boolean canAct(Player player, Item item, Item targetItem) {
 		return true;
@@ -34,6 +39,7 @@ public class MultiReturnAction extends AbstractItemAction {
 
 	@Override
 	public void act(final Player player, final Item item, final Item targetItem) {
+		PacketSendUtility.broadcastPacket(player, new SM_ITEM_USAGE_ANIMATION(player.getObjectId(), item.getObjectId(), item.getItemId(), USAGE_DELAY, 0, 0), true);
 
 		final ItemUseObserver observer = new ItemUseObserver() {
 
@@ -41,7 +47,8 @@ public class MultiReturnAction extends AbstractItemAction {
 			public void abort() {
 				player.getController().cancelTask(TaskId.ITEM_USE);
 				player.removeItemCoolDown(item.getItemTemplate().getUseLimits().getDelayId());
-				PacketSendUtility.sendPacket(player, new SM_SYSTEM_MESSAGE(1402147, new DescriptionId(targetItem.getNameId())));
+				PacketSendUtility.sendPacket(player, SM_SYSTEM_MESSAGE.STR_ITEM_CANCELED(new DescriptionId(item.getNameId())));
+				PacketSendUtility.broadcastPacket(player, new SM_ITEM_USAGE_ANIMATION(player.getObjectId(), item.getObjectId(), item.getItemId(), 0, 2, 0), true);
 				player.getObserveController().removeObserver(this);
 			}
 		};
@@ -50,18 +57,18 @@ public class MultiReturnAction extends AbstractItemAction {
 
 			@Override
 			public void run() {
-				player.getObserveController().removeObserver(observer);
-
 				ReturnLocList loc = DataManager.MULTIRETURN_DATA.getReturnLocListById(id).get(item.getIndexReturn());
-				if (loc != null) {
-					if (loc.getAlias() != null && loc.getWorldid() > 0) {
-						TeleportService2.useTeleportScroll(player, loc.getAlias().toUpperCase(), loc.getWorldid());
-						PacketSendUtility.sendPacket(player, SM_SYSTEM_MESSAGE.STR_USE_ITEM(new DescriptionId(item.getNameId())));
-						player.getInventory().decreaseByObjectId(item.getObjectId(), 1);
+				if (loc != null && loc.getAlias() != null && loc.getWorldid() > 0) {
+					if (!player.getInventory().decreaseByObjectId(item.getObjectId(), 1)) {
+						observer.abort();
+						return;
 					}
+					player.getObserveController().removeObserver(observer);
+					TeleportService2.useTeleportScroll(player, loc.getAlias().toUpperCase(), loc.getWorldid());
+					PacketSendUtility.sendPacket(player, SM_SYSTEM_MESSAGE.STR_USE_ITEM(new DescriptionId(item.getNameId())));
 				}
 			}
 
-		}, 5000));
+		}, USAGE_DELAY));
 	}
 }
