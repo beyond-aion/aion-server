bdabab1cf0e75103e4c2b6352623a5d48c122cc7
Estrayl <galawaynkrueger@yahoo.de>
2015-11-22 23:16:54 +0100

    missing player drop calc for global drops

--- game-server/src/com/aionemu/gameserver/services/drop/DropRegistrationService.java
+++ game-server/src/com/aionemu/gameserver/services/drop/DropRegistrationService.java
@@ -334,7 +334,7 @@ public class DropRegistrationService {
 					if (!rule.isFixedChance()) {
 						chance *= getRankModifier(npc) * getRatingModifier(npc);
 						if (!rule.getNoReduction())
-							chance *= dropRate;
+							chance *= boostDropRate;
 					}
 					// add drop rate multiplier
 					//float percent = chance * dropRate;
