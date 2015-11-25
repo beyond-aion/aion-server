3421f7d07ebae10795c3def2076ddabf8c1eef5d
Neon <neon@beyond-aion.com>
2015-11-25 04:17:24 +0100

    forgot to edit param info

--- game-server/data/scripts/system/handlers/admincommands/Dye.java
+++ game-server/data/scripts/system/handlers/admincommands/Dye.java
@@ -24,7 +24,7 @@ public class Dye extends AdminCommand {
 	public Dye() {
 		super("dye", "Dyes a players visible equipment.");
 
-		setParamInfo("<color> - Dyes the selected player in the specified color (can be dye item link/ID or color HEX code). 0 removes all dyeing.");
+		setParamInfo("<color> - Dyes the selected player in the specified color (can be dye item link/ID, color name or color HEX code). 0 removes all dyeing.");
 	}
 
 	@Override
