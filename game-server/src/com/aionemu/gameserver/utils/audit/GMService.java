63cfe3126eafb34dbd76a90e6ed66a3e884316a2
Neon <neon@beyond-aion.com>
2015-11-26 21:16:02 +0100

    implemented delay for GM login announces (as of #TB13)

--- game-server/src/com/aionemu/gameserver/utils/audit/GMService.java
+++ game-server/src/com/aionemu/gameserver/utils/audit/GMService.java
@@ -9,6 +9,7 @@ import javolution.util.FastSet;
 
 import com.aionemu.commons.objects.filter.ObjectFilter;
 import com.aionemu.gameserver.configs.administration.AdminConfig;
+import com.aionemu.gameserver.model.gameobjects.player.FriendList.Status;
 import com.aionemu.gameserver.model.gameobjects.player.Player;
 import com.aionemu.gameserver.model.gameobjects.state.CreatureSeeState;
 import com.aionemu.gameserver.model.gameobjects.state.CreatureVisualState;
@@ -17,6 +18,7 @@ import com.aionemu.gameserver.network.aion.serverpackets.SM_SYSTEM_MESSAGE;
 import com.aionemu.gameserver.skillengine.effect.AbnormalState;
 import com.aionemu.gameserver.utils.ChatUtil;
 import com.aionemu.gameserver.utils.PacketSendUtility;
+import com.aionemu.gameserver.utils.ThreadPoolManager;
 
 /**
  * @author MrPoke
@@ -47,9 +49,6 @@ public class GMService {
 
 	public void onPlayerLogin(Player player) {
 		if (player.isGM()) {
-			gms.put(player.getObjectId(), player);
-			broadcastGMConnectionMessage(player, true);
-
 			String delimiter = "=============================";
 			StringBuilder sb = new StringBuilder(delimiter);
 			if (AdminConfig.INVULNERABLE_GM_CONNECTION) {
@@ -83,16 +82,19 @@ public class GMService {
 			}
 			if (sb.length() > delimiter.length())
 				PacketSendUtility.sendMessage(player, sb.append("\n" + delimiter).toString());
+
+			gms.put(player.getObjectId(), player);
+			scheduleBroadcastLogin(player);
 		}
 	}
 
 	public void onPlayerLogout(Player player) {
-		if (gms.remove(player.getObjectId()) != null)
-			broadcastGMConnectionMessage(player, false);
+		if (gms.remove(player.getObjectId()) != null && isAnnounceable(player))
+			broadcastConnectionStatus(player, false);
 	}
 
 	public boolean isAnnounceable(Player gm) {
-		return gm.isGM() && gm.isWispable() && (announceList == null || announceList.contains(gm.getAccessLevel()));
+		return gm.isGM() && gm.isWispable() && gm.getFriendList().getStatus() != Status.OFFLINE && (announceList == null || announceList.contains(gm.getAccessLevel()));
 	}
 
 	public void broadcastMessageToGMs(String message) {
@@ -101,14 +103,11 @@ public class GMService {
 		}
 	}
 
-	private void broadcastGMConnectionMessage(Player gm, boolean connected) {
-		if (!isAnnounceable(gm))
-			return;
-
+	private void broadcastConnectionStatus(Player gm, boolean connected) {
 		String name = AdminConfig.CUSTOMTAG_ENABLE ? ChatUtil.name(gm.getName(true)) : "GM: " + ChatUtil.name(gm.getName());
 		SM_SYSTEM_MESSAGE sysMsg = connected ? SM_SYSTEM_MESSAGE.STR_NOTIFY_LOGIN_BUDDY(name) : SM_SYSTEM_MESSAGE.STR_NOTIFY_LOGOFF_BUDDY(name);
 
-		if ((connected && AdminConfig.ANNOUNCE_LOGOUT_TO_ALL_PLAYERS) || (!connected && AdminConfig.ANNOUNCE_LOGIN_TO_ALL_PLAYERS)) {
+		if ((connected && AdminConfig.ANNOUNCE_LOGIN_TO_ALL_PLAYERS) || (!connected && AdminConfig.ANNOUNCE_LOGOUT_TO_ALL_PLAYERS)) {
 			PacketSendUtility.broadcastFilteredPacket(sysMsg, new ObjectFilter<Player>() {
 
 				@Override
@@ -125,6 +124,26 @@ public class GMService {
 				}
 			});
 		}
+	}
+
+	private void scheduleBroadcastLogin(Player gm) {
+		if (!isAnnounceable(gm))
+			return;
+
+		byte delay = 15;
+		PacketSendUtility.sendMessage(gm, "Your login will be announced in " + delay + "s.\nYou can disable this by setting whisper off or changing your online status to invisible.");
+		ThreadPoolManager.getInstance().schedule(new Runnable() {
+
+			@Override
+			public void run() {
+				if (isAnnounceable(gm)) {
+					broadcastConnectionStatus(gm, true);
+					PacketSendUtility.sendMessage(gm, "Your login has been announced.");
+				} else {
+					PacketSendUtility.sendMessage(gm, "Your login has not been announced.");
+				}
+			}
+		}, delay * 1000);
 	}
 
 	private static class SingletonHolder {
