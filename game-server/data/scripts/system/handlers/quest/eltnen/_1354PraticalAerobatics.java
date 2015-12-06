f3f7de24fe84cf79605f3419bdbcc12c05baf335
Pad <lucas@luckev.info>
2015-12-07 00:28:31 +0100

    Quests _1354PracticalAerobatics #BA558

--- game-server/data/scripts/system/handlers/quest/eltnen/_1354PraticalAerobatics.java
+++ game-server/data/scripts/system/handlers/quest/eltnen/_1354PraticalAerobatics.java
@@ -8,15 +8,18 @@ import com.aionemu.gameserver.questEngine.model.QuestEnv;
 import com.aionemu.gameserver.questEngine.model.QuestState;
 import com.aionemu.gameserver.questEngine.model.QuestStatus;
 import com.aionemu.gameserver.services.QuestService;
+import com.aionemu.gameserver.skillengine.SkillEngine;
 
 /**
  * @author Ritsu
+ * @modified Pad
  */
 public class _1354PraticalAerobatics extends QuestHandler {
 
 	private final static int questId = 1354;
-	private String[] rings = { "ERACUS_TEMPLE_210020000_1", "ERACUS_TEMPLE_210020000_2", "ERACUS_TEMPLE_210020000_3", "ERACUS_TEMPLE_210020000_4",
-		"ERACUS_TEMPLE_210020000_5", "ERACUS_TEMPLE_210020000_6", "ERACUS_TEMPLE_210020000_7" };
+	// chronological order of the flight rings has been changed
+	private String[] rings = { "ERACUS_TEMPLE_210020000_1", "ERACUS_TEMPLE_210020000_4", "ERACUS_TEMPLE_210020000_3", 
+		"ERACUS_TEMPLE_210020000_6", "ERACUS_TEMPLE_210020000_5", "ERACUS_TEMPLE_210020000_2", "ERACUS_TEMPLE_210020000_7" };
 
 	public _1354PraticalAerobatics() {
 		super(questId);
@@ -37,30 +40,25 @@ public class _1354PraticalAerobatics extends QuestHandler {
 		Player player = env.getPlayer();
 		QuestState qs = player.getQuestStateList().getQuestState(questId);
 		if (qs != null && qs.getStatus() == QuestStatus.START) {
-			if (rings[0].equals(flyingRing)) {
+			if (rings[0].equals(flyingRing))
 				changeQuestStep(env, 1, 2, false);
-				return true;
-			} else if (rings[1].equals(flyingRing)) {
+			else if (rings[1].equals(flyingRing))
 				changeQuestStep(env, 2, 3, false);
-				return true;
-			} else if (rings[2].equals(flyingRing)) {
+			else if (rings[2].equals(flyingRing))
 				changeQuestStep(env, 3, 4, false);
-				return true;
-			} else if (rings[3].equals(flyingRing)) {
+			else if (rings[3].equals(flyingRing))
 				changeQuestStep(env, 4, 5, false);
-				return true;
-			} else if (rings[4].equals(flyingRing)) {
+			else if (rings[4].equals(flyingRing))
 				changeQuestStep(env, 5, 6, false);
-				return true;
-			} else if (rings[5].equals(flyingRing)) {
+			else if (rings[5].equals(flyingRing))
 				changeQuestStep(env, 6, 7, false);
-				return true;
-			} else if (rings[6].equals(flyingRing)) {
+			else if (rings[6].equals(flyingRing)) {
 				qs.setQuestVarById(0, 8);
 				changeQuestStep(env, 8, 8, true);
 				QuestService.questTimerEnd(env);
-				return true;
 			}
+			applyFlightRingEffect(env);
+			return true;
 		}
 		return false;
 	}
@@ -93,13 +91,17 @@ public class _1354PraticalAerobatics extends QuestHandler {
 				else
 					return sendQuestStartDialog(env);
 			}
-		} else if (qs.getStatus() == QuestStatus.START) {
+		} 
+		else if (qs.getStatus() == QuestStatus.START) {
 			if (targetId == 203983) {
+				int var0 = qs.getQuestVarById(0);
 				switch (dialog) {
 					case QUEST_SELECT:
-						if (qs.getQuestVarById(0) == 0)
+						if (var0 == 0)
 							return sendQuestDialog(env, 1003);
-						if (qs.getQuestVarById(0) == 8)
+						else if (var0 == 1)
+							return sendQuestDialog(env, 2717);
+						else if (var0 == 8)
 							return sendQuestDialog(env, 2375);
 					case SETPRO1:
 						if (qs.getQuestVarById(0) == 0) {
@@ -110,11 +112,18 @@ public class _1354PraticalAerobatics extends QuestHandler {
 						return sendQuestEndDialog(env);
 				}
 			}
-		} else if (qs.getStatus() == QuestStatus.REWARD) {
+		} 
+		else if (qs.getStatus() == QuestStatus.REWARD) {
 			if (targetId == 203983)
 				return sendQuestEndDialog(env);
 		}
 		return false;
+	}
+	
+	private void applyFlightRingEffect(QuestEnv env) {
+		Player player = env.getPlayer();
+		player.getLifeStats().increaseFp(7);
+		SkillEngine.getInstance().applyEffectDirectly(1856, player, player, 0);
 	}
 
 }
