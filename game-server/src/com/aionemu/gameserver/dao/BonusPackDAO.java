47946b18ba6f278147938a107f84f97ec9501d17
Neon <neon@beyond-aion.com>
2015-12-07 03:37:47 +0100

    more bonus pack safety changes (hopefully the last time)

--- game-server/src/com/aionemu/gameserver/dao/BonusPackDAO.java
+++ game-server/src/com/aionemu/gameserver/dao/BonusPackDAO.java
@@ -1,16 +1,15 @@
 package com.aionemu.gameserver.dao;
 
 import com.aionemu.commons.database.dao.DAO;
-import com.aionemu.gameserver.model.gameobjects.player.Player;
 
 /**
  * @author Estrayl
  */
 public abstract class BonusPackDAO implements DAO {
 
-	public abstract int loadReceivingPlayer(final Player player);
+	public abstract int loadReceivingPlayer(int accountId);
 
-	public abstract void storeReceivingPlayer(int accountId, int playerId);
+	public abstract boolean storeReceivingPlayer(int accountId, int playerId);
 
 	@Override
 	public String getClassName() {
