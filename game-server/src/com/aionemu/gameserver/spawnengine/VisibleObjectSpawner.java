546fbc1ff6d7181f09c5e754a9ff0f1facefb980
Estrayl <galawaynkrueger@yahoo.de>
2016-01-04 06:29:11 +0100

    Transfer: SpawnEngine

--- game-server/src/com/aionemu/gameserver/spawnengine/VisibleObjectSpawner.java
+++ game-server/src/com/aionemu/gameserver/spawnengine/VisibleObjectSpawner.java
@@ -434,8 +434,8 @@ public class VisibleObjectSpawner {
 		SpawnTemplate spawn = SpawnEngine.createSpawnTemplate(worldId, npcId, x, y, z, heading);
 		NpcTemplate npcTemplate = DataManager.NPC_DATA.getNpcTemplate(npcId);
 
-		skillLevel = SkillLearnService.getSkillLearnLevel(skillId, creator.getCommonData().getLevel(), skillLevel);
-		byte level = (byte) SkillLearnService.getSkillMinLevel(skillId, creator.getCommonData().getLevel(), skillLevel);
+		skillLevel = SkillLearnService.getSummonSkillLearnLevel(skillId, creator.getCommonData().getLevel(), skillLevel);
+		byte level = (byte) SkillLearnService.getSummonSkillMinLevel(skillId, creator.getCommonData().getLevel(), skillLevel);
 		boolean isSiegeWeapon = npcTemplate.getAi().equals("siege_weapon");
 		Summon summon = new Summon(IDFactory.getInstance().nextId(), isSiegeWeapon ? new SiegeWeaponController(npcId) : new SummonController(), spawn,
 			npcTemplate, isSiegeWeapon ? npcTemplate.getLevel() : level, time);
