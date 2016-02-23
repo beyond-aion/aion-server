52e31460849978dce6b7974bb7385380a32167c9
Neon <neon@beyond-aion.com>
2016-02-23 04:34:04 +0100

    merged legion dominion sqls

--- game-server/sql/updates/20160116 - legion_dominion_locations and participants.sql
+++ game-server/sql/updates/20160116 - legion_dominion_locations and participants.sql
@@ -6,6 +6,13 @@ CREATE TABLE `legion_dominion_locations` (
 	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+INSERT INTO `legion_dominion_locations` VALUES ('1', '0', CURRENT_TIMESTAMP);
+INSERT INTO `legion_dominion_locations` VALUES ('2', '0', CURRENT_TIMESTAMP);
+INSERT INTO `legion_dominion_locations` VALUES ('3', '0', CURRENT_TIMESTAMP);
+INSERT INTO `legion_dominion_locations` VALUES ('4', '0', CURRENT_TIMESTAMP);
+INSERT INTO `legion_dominion_locations` VALUES ('5', '0', CURRENT_TIMESTAMP);
+INSERT INTO `legion_dominion_locations` VALUES ('6', '0', CURRENT_TIMESTAMP);
+
 DROP TABLE IF EXISTS `legion_dominion_participants`;
 CREATE TABLE `legion_dominion_participants` (
 	`legion_dominion_id` int(11) NOT NULL DEFAULT 0,
