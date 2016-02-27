d9364ac7d0c24e4e2cddeac0b16431e25da426a5
Pad <lucas@luckev.info>
2016-02-27 01:59:12 +0100

    Quests Rentus Base updated to 4.8 Part I

---
+++ game-server/data/scripts/system/handlers/quest/rentus_base/_30504TheSearchforPaios.java
@@ -0,0 +1,66 @@
+package quest.rentus_base;
+
+import com.aionemu.gameserver.model.DialogAction;
+import com.aionemu.gameserver.model.gameobjects.player.Player;
+import com.aionemu.gameserver.questEngine.handlers.QuestHandler;
+import com.aionemu.gameserver.questEngine.model.QuestEnv;
+import com.aionemu.gameserver.questEngine.model.QuestState;
+import com.aionemu.gameserver.questEngine.model.QuestStatus;
+
+/**
+ * @author Pad
+ */
+public class _30504TheSearchforPaios extends QuestHandler {
+
+	private static final int questId = 30504;
+	private static final int[] npcIds = { 205438, 701098, 799536 };
+
+	public _30504TheSearchforPaios() {
+		super(questId);
+	}
+
+	@Override
+	public void register() {
+		qe.registerQuestNpc(npcIds[0]).addOnQuestStart(questId);
+		for (int npcId : npcIds)
+			qe.registerQuestNpc(npcId).addOnTalkEvent(questId);
+	}
+
+	@Override
+	public boolean onDialogEvent(QuestEnv env) {
+		Player player = env.getPlayer();
+		QuestState qs = player.getQuestStateList().getQuestState(questId);
+		DialogAction dialog = env.getDialog();
+		int targetId = env.getTargetId();
+
+		if (qs == null || qs.getStatus() == QuestStatus.NONE || qs.canRepeat()) {
+			if (targetId == npcIds[0]) {
+				if (dialog == DialogAction.QUEST_SELECT)
+					return sendQuestDialog(env, 4762);
+				else
+					return sendQuestStartDialog(env);
+			}
+		} else if (qs.getStatus() == QuestStatus.START) {
+			if (targetId == npcIds[1]) {
+				if (dialog == DialogAction.USE_OBJECT) {
+					changeQuestStep(env, 0, 1, false);
+					return closeDialogWindow(env);
+				}
+			} else if (targetId == npcIds[2]) {
+				if (dialog == DialogAction.QUEST_SELECT)
+					return sendQuestDialog(env, 1352);
+				else if (dialog == DialogAction.SET_SUCCEED)
+					return defaultCloseDialog(env, 1, 1, true, false);
+			}
+		} else if (qs.getStatus() == QuestStatus.REWARD) {
+			if (targetId == npcIds[0]) {
+				if (dialog == DialogAction.USE_OBJECT)
+					return sendQuestDialog(env, 5);
+				else
+					return sendQuestEndDialog(env);
+			}
+		}
+		return false;
+	}
+
+}
