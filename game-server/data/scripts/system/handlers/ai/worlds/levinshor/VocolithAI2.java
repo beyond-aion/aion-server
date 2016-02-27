17d9479228b6e3d6a5ac84e880c09394c485bf5f
Neon <neon@beyond-aion.com>
2016-02-27 09:20:00 +0100

    vocolith despawn/spawn improvements

--- game-server/data/scripts/system/handlers/ai/worlds/levinshor/VocolithAI2.java
+++ game-server/data/scripts/system/handlers/ai/worlds/levinshor/VocolithAI2.java
@@ -3,26 +3,33 @@ package ai.worlds.levinshor;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import com.aionemu.commons.utils.Rnd;
+import com.aionemu.gameserver.ai2.AI2Actions;
 import com.aionemu.gameserver.ai2.AIName;
+import com.aionemu.gameserver.ai2.poll.AIAnswer;
+import com.aionemu.gameserver.ai2.poll.AIAnswers;
+import com.aionemu.gameserver.ai2.poll.AIQuestion;
+import com.aionemu.gameserver.controllers.NpcController;
+import com.aionemu.gameserver.controllers.observer.ActionObserver;
 import com.aionemu.gameserver.controllers.observer.ItemUseObserver;
+import com.aionemu.gameserver.controllers.observer.ObserverType;
 import com.aionemu.gameserver.model.EmotionType;
 import com.aionemu.gameserver.model.TaskId;
+import com.aionemu.gameserver.model.gameobjects.Creature;
+import com.aionemu.gameserver.model.gameobjects.Npc;
 import com.aionemu.gameserver.model.gameobjects.player.Player;
-import com.aionemu.gameserver.model.templates.spawns.SpawnTemplate;
 import com.aionemu.gameserver.network.aion.serverpackets.SM_DIALOG_WINDOW;
 import com.aionemu.gameserver.network.aion.serverpackets.SM_EMOTION;
 import com.aionemu.gameserver.network.aion.serverpackets.SM_SYSTEM_MESSAGE;
 import com.aionemu.gameserver.network.aion.serverpackets.SM_USE_OBJECT;
-import com.aionemu.gameserver.spawnengine.SpawnEngine;
+import com.aionemu.gameserver.services.RespawnService;
 import com.aionemu.gameserver.utils.PacketSendUtility;
 import com.aionemu.gameserver.utils.ThreadPoolManager;
-import com.aionemu.gameserver.world.World;
-import com.aionemu.gameserver.world.knownlist.Visitor;
 
 import ai.GeneralNpcAI2;
 
 /**
  * @author Yeats
+ * @modified Neon
  */
 @AIName("LDF4_Advance_Vocolith")
 public class VocolithAI2 extends GeneralNpcAI2 {
@@ -72,13 +79,6 @@ public class VocolithAI2 extends GeneralNpcAI2 {
 	private void handleUsed(Player player) {
 		if (player.getInventory().decreaseByItemId(185000216, 1)) {
 			PacketSendUtility.sendPacket(player, SM_SYSTEM_MESSAGE.STR_MSG_LDF4_ADVANCE_FNAMED_SPAWN_ITEM());
-			World.getInstance().getWorldMap(getOwner().getWorldId()).getMainWorldMapInstance().doOnAllPlayers(new Visitor<Player>() {
-
-				@Override
-				public void visit(Player player) {
-					PacketSendUtility.sendPacket(player, SM_SYSTEM_MESSAGE.STR_MSG_LDF4_ADVANCE_FNAMED_SPAWN());
-				}
-			});
 			spawnRandomBoss();
 		} else {
 			PacketSendUtility.sendPacket(player, SM_SYSTEM_MESSAGE.STR_MSG_LDF4_ADVANCE_FNAMED_FAIL());
@@ -87,13 +87,26 @@ public class VocolithAI2 extends GeneralNpcAI2 {
 	}
 
 	private void spawnRandomBoss() {
-		PacketSendUtility.broadcastPacket(getOwner(), new SM_EMOTION(getOwner(), EmotionType.DIE, 0, 0));
-		ThreadPoolManager.getInstance().schedule(() -> getOwner().getController().onDieSilence(), 7000);
-		ThreadPoolManager.getInstance().schedule((Runnable) () -> {
-				int npcId = 235217 + Rnd.get(0, 3);
-				SpawnTemplate spawn = SpawnEngine.addNewSingleTimeSpawn(getOwner().getWorldId(), npcId, getOwner().getX(), getOwner().getY(), getOwner()
-					.getZ(), getOwner().getHeading());
-				SpawnEngine.spawnObject(spawn, getOwner().getInstanceId());
-		}, 2000);
+		Npc vocolith = getOwner();
+		NpcController vocolithController = vocolith.getController();
+		AI2Actions.die(this); // kill vocolith
+		vocolithController.addTask(TaskId.DECAY, RespawnService.scheduleDecayTask(vocolith, 7500)); // schedule late despawn to show full death animation
+		ThreadPoolManager.getInstance().schedule((Runnable) () -> { // schedule boss spawn
+			int npcId = 235217 + Rnd.get(0, 3);
+			Npc boss = (Npc) spawn(npcId, vocolith.getX(), vocolith.getY(), vocolith.getZ(), vocolith.getHeading());
+			boss.getObserveController().addObserver(new ActionObserver(ObserverType.DEATH) { // enable vocolith respawn after boss is killed
+
+				@Override
+				public void died(Creature creature) {
+					vocolithController.addTask(TaskId.RESPAWN, vocolithController.scheduleRespawn());
+				}
+			});
+			boss.getPosition().getWorldMapInstance().doOnAllPlayers(player -> PacketSendUtility.sendPacket(player, SM_SYSTEM_MESSAGE.STR_MSG_LDF4_ADVANCE_FNAMED_SPAWN()));
+		} , 5000);
+	}
+
+	@Override
+	protected AIAnswer pollInstance(AIQuestion question) {
+		return question == AIQuestion.SHOULD_RESPAWN ? AIAnswers.NEGATIVE : super.pollInstance(question);
 	}
 }
