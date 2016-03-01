3e80493f474c650e787ab7a6993ebacd825e6697
Yeats <pooya_dialeh@hotmail.de>
2016-03-01 13:23:33 +0100

    fixed #BA908 https://beyond-aion.com/forum/thread/908-id-4296-widerhall-des-angriffs-id-4299-durchdringender-reibelaut/

--- game-server/src/com/aionemu/gameserver/controllers/attack/AttackUtil.java
+++ game-server/src/com/aionemu/gameserver/controllers/attack/AttackUtil.java
@@ -487,7 +487,8 @@ public class AttackUtil {
 		if (shared && !effect.getSkill().getEffectedList().isEmpty())
 			damage /= effect.getSkill().getEffectedList().size();
 
-		if (template instanceof ProcAtkInstantEffect && !effect.getStack().startsWith("ITEM_SKILL_PROC")) // FIXME: I'm not very amused, what I've seen!
+		if (template instanceof ProcAtkInstantEffect && !effect.getStack().startsWith("ITEM_SKILL_PROC") &&
+				!effect.getStack().startsWith("BA_N_")) // FIXME: I'm not very amused, what I've seen!
 			damage = skillDamage;
 
 		if (damage < 0)
