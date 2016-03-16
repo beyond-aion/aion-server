ff894d790a6a899fe2372b7010e67fbf42a00790
Pad <lucas@luckev.info>
2016-03-16 01:01:59 +0100

    Quests _13708ProximityProtect & _23708SoundtheAlarm

---
+++ game-server/data/scripts/system/handlers/quest/levinshor/_23708SoundtheAlarm.java
@@ -0,0 +1,82 @@
+package quest.levinshor;
+
+import com.aionemu.gameserver.model.DialogAction;
+import com.aionemu.gameserver.model.gameobjects.Item;
+import com.aionemu.gameserver.model.gameobjects.player.Player;
+import com.aionemu.gameserver.questEngine.handlers.HandlerResult;
+import com.aionemu.gameserver.questEngine.handlers.QuestHandler;
+import com.aionemu.gameserver.questEngine.model.QuestEnv;
+import com.aionemu.gameserver.questEngine.model.QuestState;
+import com.aionemu.gameserver.questEngine.model.QuestStatus;
+import com.aionemu.gameserver.world.zone.ZoneName;
+
+/**
+ * @author Pad
+ */
+public class _23708SoundtheAlarm extends QuestHandler {
+
+	private static final int questId = 23708;
+	private static final int npcId = 802344; // Kestum
+	private static final int itemId = 182215535;
+
+	public _23708SoundtheAlarm() {
+		super(questId);
+	}
+
+	@Override
+	public void register() {
+		qe.registerQuestNpc(npcId).addOnQuestStart(questId);
+		qe.registerQuestNpc(npcId).addOnTalkEvent(questId);
+		qe.registerQuestItem(itemId, questId);
+	}
+
+	@Override
+	public boolean onDialogEvent(QuestEnv env) {
+		Player player = env.getPlayer();
+		QuestState qs = player.getQuestStateList().getQuestState(questId);
+		DialogAction dialog = env.getDialog();
+		int targetId = env.getTargetId();
+
+		if (qs == null || qs.getStatus() == QuestStatus.NONE || qs.canRepeat()) {
+			if (targetId == npcId) {
+				if (dialog == DialogAction.QUEST_SELECT) {
+					return sendQuestDialog(env, 1011);
+				} else {
+					return sendQuestStartDialog(env, itemId, 1);
+				}
+			}
+		} else if (qs.getStatus() == QuestStatus.START) {
+			int var = qs.getQuestVarById(0);
+			if (targetId == npcId) {
+				switch (dialog) {
+					case QUEST_SELECT: {
+						if (var == 1) {
+							return sendQuestDialog(env, 2375);
+						}
+					}
+					case SELECT_QUEST_REWARD: {
+						return defaultCloseDialog(env, 1, 1, true, true);
+					}
+				}
+			}
+		} else if (qs.getStatus() == QuestStatus.REWARD) {
+			if (targetId == npcId) {
+				return sendQuestEndDialog(env);
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public HandlerResult onItemUseEvent(QuestEnv env, Item item) {
+		Player player = env.getPlayer();
+		QuestState qs = player.getQuestStateList().getQuestState(questId);
+		if (qs != null && qs.getStatus() == QuestStatus.START) {
+			if (player.isInsideZone(ZoneName.get("LDF4_ADVANCE_ITEMUSEAREA_Q23708_600100000"))) {
+				return HandlerResult.fromBoolean(useQuestItem(env, item, 0, 1, false));
+			}
+		}
+		return HandlerResult.FAILED;
+	}
+	
+}
