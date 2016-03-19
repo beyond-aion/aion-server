253caef99123daced16d5f8d59469c673db6ad08
Yeats <pooya_dialeh@hotmail.de>
2016-03-19 03:16:48 +0100

    removed duplicate method call in ServantGameStats (since 15% pve multiplier applies to all skills)

--- game-server/src/com/aionemu/gameserver/model/stats/container/ServantGameStats.java
+++ game-server/src/com/aionemu/gameserver/model/stats/container/ServantGameStats.java
@@ -10,33 +10,6 @@ public class ServantGameStats extends SummonedObjectGameStats {
 
 	private int fixedMBoost;
 
-	//TODO other stats might have a fixed value too
-	@Override
-	public Stat2 getStat(StatEnum statEnum, int base) {
-		Stat2 stat = super.getStat(statEnum, base);
-		if (owner.getMaster() == null)
-			return stat;
-		switch (statEnum) {
-			case MAGICAL_ATTACK:
-			case MAGICAL_ACCURACY:
-			case MAGICAL_RESIST:
-				stat.setBonusRate(0.2f);
-				return owner.getMaster().getGameStats().getItemStatBoost(statEnum, stat);
-			case PHYSICAL_ACCURACY:
-				stat.setBonusRate(0.2f);
-				owner.getMaster().getGameStats().getItemStatBoost(StatEnum.MAIN_HAND_ACCURACY, stat);
-				return owner.getMaster().getGameStats().getItemStatBoost(statEnum, stat);
-			case PHYSICAL_ATTACK:
-				stat.setBonusRate(0.2f);
-				owner.getMaster().getGameStats().getItemStatBoost(StatEnum.MAIN_HAND_POWER, stat);
-				return owner.getMaster().getGameStats().getItemStatBoost(statEnum, stat);
-			case PVE_ATTACK_RATIO:
-				stat.setBonus(150); // equals 15% pve bonus
-				return stat;
-		}
-		return stat;
-	}
-
 	public ServantGameStats(Npc owner) {
 		super(owner);
 	}
