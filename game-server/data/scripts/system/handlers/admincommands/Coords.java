2028a58cce0126ea0b813ab4619994c9fa6691f4
Neon <neon@beyond-aion.com>
2016-03-23 01:57:05 +0100

    fixed wish console command (npc spawning), and made coords command work with no target

--- game-server/data/scripts/system/handlers/admincommands/Coords.java
+++ game-server/data/scripts/system/handlers/admincommands/Coords.java
@@ -2,7 +2,6 @@ package admincommands;
 
 import com.aionemu.gameserver.model.gameobjects.VisibleObject;
 import com.aionemu.gameserver.model.gameobjects.player.Player;
-import com.aionemu.gameserver.utils.PacketSendUtility;
 import com.aionemu.gameserver.utils.chathandlers.AdminCommand;
 
 /**
@@ -16,15 +15,7 @@ public class Coords extends AdminCommand {
 
 	@Override
 	public void execute(Player admin, String... params) {
-		VisibleObject target = admin.getTarget();
-		if (target == null)
-			return;
-		PacketSendUtility.sendMessage(admin, 
-			"MapID: " + target.getPosition().getMapId() +
-			" / X: " + target.getPosition().getX() + 
-			" / Y: " + target.getPosition().getY() + 
-			" / Z: " + target.getPosition().getZ() + 
-			" / Heading: " + target.getPosition().getHeading());
+		VisibleObject target = admin.getTarget() == null ? admin : admin.getTarget();
+		sendInfo(admin, target.getPosition().toString());
 	}
-
 }
