c4d9593b981841a2ad4cd2b00bf95ac74b2eae00
Neon <neon@beyond-aion.com>
2016-03-26 03:03:10 +0100

    added sys message when searching npc failed

--- game-server/src/com/aionemu/gameserver/network/aion/clientpackets/CM_OBJECT_SEARCH.java
+++ game-server/src/com/aionemu/gameserver/network/aion/clientpackets/CM_OBJECT_SEARCH.java
@@ -6,6 +6,7 @@ import com.aionemu.gameserver.model.templates.spawns.SpawnSearchResult;
 import com.aionemu.gameserver.network.aion.AionClientPacket;
 import com.aionemu.gameserver.network.aion.AionConnection.State;
 import com.aionemu.gameserver.network.aion.serverpackets.SM_SHOW_NPC_ON_MAP;
+import com.aionemu.gameserver.network.aion.serverpackets.SM_SYSTEM_MESSAGE;
 
 /**
  * @author Lyahim
@@ -21,20 +22,13 @@ public class CM_OBJECT_SEARCH extends AionClientPacket {
 	 */
 	public CM_OBJECT_SEARCH(int opcode, State state, State... restStates) {
 		super(opcode, state, restStates);
-
 	}
 
-	/**
-	 * Nothing to do
-	 */
 	@Override
 	protected void readImpl() {
 		this.npcId = readD();
 	}
 
-	/**
-	 * Logging
-	 */
 	@Override
 	protected void runImpl() {
 		Player activePlayer = getConnection().getActivePlayer();
@@ -42,9 +36,10 @@ public class CM_OBJECT_SEARCH extends AionClientPacket {
 			return;
 		}
 		SpawnSearchResult searchResult = DataManager.SPAWNS_DATA2.getNearestSpawnByNpcId(activePlayer, npcId, activePlayer.getWorldId());
-		if (searchResult != null) {
+		if (searchResult != null)
 			sendPacket(new SM_SHOW_NPC_ON_MAP(activePlayer, npcId, searchResult.getWorldId(), searchResult.getSpot().getX(), searchResult.getSpot().getY(),
 				searchResult.getSpot().getZ()));
-		}
+		else
+			sendPacket(SM_SYSTEM_MESSAGE.STR_FIND_POS_UNKNOWN_NAME());
 	}
 }
