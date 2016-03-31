330c5a9b8ecfef3776b7c0431656c12efeb02a6f
Yeats <pooya_dialeh@hotmail.de>
2016-03-31 17:47:20 +0200

    Added retail-like system message when a cast is canceled by opponents death

--- game-server/src/com/aionemu/gameserver/controllers/observer/DieObserver.java
+++ game-server/src/com/aionemu/gameserver/controllers/observer/DieObserver.java
@@ -1,6 +1,7 @@
 package com.aionemu.gameserver.controllers.observer;
 
 import com.aionemu.gameserver.model.gameobjects.Creature;
+import com.aionemu.gameserver.network.aion.serverpackets.SM_SYSTEM_MESSAGE;
 import com.aionemu.gameserver.skillengine.model.Skill;
 
 /**
@@ -18,7 +19,7 @@ public class DieObserver extends ActionObserver {
 	@Override
 	public void died(Creature creature) {
 		if (skill != null) {
-			skill.getEffector().getController().cancelCurrentSkill(null);
+			skill.getEffector().getController().cancelCurrentSkill(null, SM_SYSTEM_MESSAGE.STR_SKILL_TARGET_LOST());
 		}
 	}
 }
