706b7af9a645a7261c8c0cc7804ec4153e73ac77
Neon <neon@beyond-aion.com>
2016-05-30 12:28:37 +0200

    implemented veteran rewards

---
+++ game-server/sql/updates/20160529 - player_veteran_rewards.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS `player_veteran_rewards`;
+CREATE TABLE `player_veteran_rewards` (
+  `player_id` int(11) NOT NULL,
+  `received_months` tinyint(3) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`player_id`),
+  CONSTRAINT `player_veteran_rewards_ibfk_1` FOREIGN KEY (`player_id`) REFERENCES `players` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
