c3ad5664bf22db352de15628c41bcfd0543c544e
Neon <neon@beyond-aion.com>
2016-06-05 17:08:59 +0200

    improved HDD serial readout

--- game-server/src/com/aionemu/gameserver/network/aion/clientpackets/CM_MAC_ADDRESS.java
+++ game-server/src/com/aionemu/gameserver/network/aion/clientpackets/CM_MAC_ADDRESS.java
@@ -1,5 +1,8 @@
 package com.aionemu.gameserver.network.aion.clientpackets;
 
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,83 +22,85 @@ import com.aionemu.gameserver.services.ban.HDDBanService;
 import com.aionemu.gameserver.utils.ThreadPoolManager;
 
 /**
- * In this packet client is sending Mac Address - haha.
+ * In this packet client is sending connection and system information (IPs, MAC Address and HDD serial).
  * 
- * @author -Nemesiss-, KID, ViAl
+ * @author -Nemesiss-, KID, ViAl, Neon
  */
 public class CM_MAC_ADDRESS extends AionClientPacket {
 
 	private static final Logger log = LoggerFactory.getLogger(CM_MAC_ADDRESS.class);
-	/**
-	 * Mac Addres send by client in the same format as: ipconfig /all [ie: xx-xx-xx-xx-xx-xx]
-	 */
-	private String macAddress;
-	/**
-	 * Client HDD serial
-	 */
-	private String hddSerial;
+	private String macAddress, hddSerial;
 
-	/**
-	 * Constructs new instance of <tt>CM_MAC_ADDRESS </tt> packet
-	 * 
-	 * @param opcode
-	 */
 	public CM_MAC_ADDRESS(int opcode, State state, State... restStates) {
 		super(opcode, state, restStates);
 	}
 
 	@Override
 	protected void readImpl() {
-		readC();
-		short counter = (short) readH();
-		for (short i = 0; i < counter; i++)
-			readD();
+		readC(); // unk
+		int routeSteps = readH();
+		for (int i = 0; i < routeSteps; i++)
+			readD(); // ip traceroute, see -> System.out.println(NetworkUtils.intToIpString(readD()))
 		macAddress = readS();
-		hddSerial = readS();
+		hddSerial = fixHddSerial(readS());
+		readD(); // local IP, see -> System.out.println(NetworkUtils.intToIpString(readD()))
 	}
 
 	@Override
 	protected void runImpl() {
 		AionConnection con = getConnection();
 		if (BannedMacManager.getInstance().isBanned(macAddress)) {
-			// TODO some information packets
 			con.close();
 			log.info("[MAC_AUDIT] " + macAddress + " (" + con.getIP() + ") was kicked due to mac ban");
 		} else if (HDDBanService.getInstance().isBanned(hddSerial)) {
 			con.close();
-			log.info("[MAC_AUDIT] Account:" + con.getAccount().getName() + ", HDD Serial " + hddSerial + " (" + con.getIP()
-				+ ") was kicked due to hdd serial ban");
+			log.info("[MAC_AUDIT] Account:" + con.getAccount() + ", HDD Serial " + hddSerial + " (" + con.getIP() + ") was kicked due to hdd serial ban");
 		} else {
 			con.setMacAddress(macAddress);
 			con.setHddSerial(hddSerial);
-		}
-		ThreadPoolManager.getInstance().schedule(new Runnable() {
 
-			@Override
-			public void run() {
-				if (getConnection() == null)
-					return;
-				Account account = getConnection().getAccount();
-				LoginServer.getInstance().sendPacket(new SM_MAC(account.getId(), macAddress));
-				LoginServer.getInstance().sendPacket(new SM_HDD_SERIAL(account.getId(), hddSerial));
-				LoginServer.getInstance()
-					.sendPacket(new SM_ACCOUNT_LOGIN_LOG(account.getId(), System.currentTimeMillis(), getConnection().getIP(), macAddress, hddSerial));
-				if (account.getAllowedHddSerial() == null) {
-					if (SecurityConfig.HDD_SERIAL_LOCK_NEW_ACCOUNTS && !hddSerial.isEmpty()) {
-						account.setAllowedHddSerial(hddSerial);
-						LoginServer.getInstance().sendPacket(new SM_CHANGE_ALLOWED_HDD_SERIAL(account));
-					}
-				} else if (!account.getAllowedHddSerial().equalsIgnoreCase(hddSerial)) {
-					if (SecurityConfig.HDD_SERIAL_HACKED_ACCOUNTS_KICK) {
-						getConnection().sendPacket(SM_SYSTEM_MESSAGE.STR_L2AUTH_S_SYSTEM_ERROR());
-						getConnection().close();
-						log.info("[MAC_AUDIT] " + account + ",  HDD Serial " + hddSerial + " (" + getConnection().getIP()
-							+ ") was kicked due to allowed hdd serial mismatch");
-					} else {
-						account.setHacked(true);
+			ThreadPoolManager.getInstance().schedule(new Runnable() {
+
+				@Override
+				public void run() {
+					AionConnection con = getConnection();
+					if (con == null || con.getAccount() == null)
+						return;
+					Account account = con.getAccount();
+					LoginServer.getInstance().sendPacket(new SM_MAC(account.getId(), macAddress));
+					LoginServer.getInstance().sendPacket(new SM_HDD_SERIAL(account.getId(), hddSerial));
+					LoginServer.getInstance()
+						.sendPacket(new SM_ACCOUNT_LOGIN_LOG(account.getId(), System.currentTimeMillis(), con.getIP(), macAddress, hddSerial));
+					if (account.getAllowedHddSerial() == null) {
+						if (SecurityConfig.HDD_SERIAL_LOCK_NEW_ACCOUNTS && !hddSerial.isEmpty()) {
+							account.setAllowedHddSerial(hddSerial);
+							LoginServer.getInstance().sendPacket(new SM_CHANGE_ALLOWED_HDD_SERIAL(account));
+						}
+					} else if (!account.getAllowedHddSerial().equalsIgnoreCase(hddSerial)) {
+						if (SecurityConfig.HDD_SERIAL_HACKED_ACCOUNTS_KICK) {
+							con.close(SM_SYSTEM_MESSAGE.STR_L2AUTH_S_SYSTEM_ERROR());
+							log.info("[MAC_AUDIT] " + account + ",  HDD Serial " + hddSerial + " (" + con.getIP() + ") was kicked due to allowed hdd serial mismatch");
+						} else {
+							account.setHacked(true);
+						}
 					}
 				}
+			}, 5 * 1000);
+		}
+	}
+
+	private static String fixHddSerial(String hddSerial) {
+		if (!hddSerial.matches("[0-9a-zA-Z _-]+")) { // most likely not a serial number string, so we need to convert it
+			byte[] bytes = hddSerial.getBytes();
+			if (bytes.length % Integer.BYTES == 0) { // check if the length fits exactly one or more ints
+				StringBuilder sb = new StringBuilder();
+				for (IntBuffer intBuffer = ByteBuffer.wrap(bytes).asIntBuffer(); intBuffer.hasRemaining();)
+					sb.append(intBuffer.get()); // concatenate all ints of this buffer to build the serial number
+				return sb.toString();
 			}
-		}, 10 * 1000);
+		} else if (hddSerial.matches("^[a-zA-Z0-9] [a-zA-Z0-9_-].*|.*[a-zA-Z0-9_-] [a-zA-Z0-9]$")) { // check if second or penultimate char is a space
+			return hddSerial.replaceAll("(.)(.)", "$2$1").trim(); // for some reason some clients send switched chars
+		}
+		return hddSerial;
 	}
 }
