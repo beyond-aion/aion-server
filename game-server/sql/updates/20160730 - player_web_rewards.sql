6cd0e56e0b8aa7d2add45df07641e9d75b6982f9
Neon <neon@beyond-aion.com>
2016-07-01 02:46:22 +0200

    changed sql update file to avoid mysql issues

--- game-server/sql/updates/20160730 - player_web_rewards.sql
+++ game-server/sql/updates/20160730 - player_web_rewards.sql
@@ -1,9 +1,12 @@
-ALTER TABLE `web_reward`
-DROP COLUMN `rewarded`,
-CHANGE COLUMN `unique` `entry_id`  int(11) NOT NULL AUTO_INCREMENT FIRST ,
-CHANGE COLUMN `item_owner` `player_id`  int(11) NOT NULL AFTER `entry_id`,
-MODIFY COLUMN `item_id`  int(9) NOT NULL AFTER `player_id`,
-MODIFY COLUMN `item_count`  bigint(20) NOT NULL DEFAULT 1 AFTER `item_id`,
-MODIFY COLUMN `added`  timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `item_count`,
-MODIFY COLUMN `received`  timestamp NULL DEFAULT NULL AFTER `added`;
-RENAME TABLE web_reward TO player_web_rewards;
+DROP TABLE `web_reward`;
+CREATE TABLE `player_web_rewards` (
+  `entry_id` int(11) NOT NULL AUTO_INCREMENT,
+  `player_id` int(11) NOT NULL,
+  `item_id` int(9) NOT NULL,
+  `item_count` bigint(20) NOT NULL DEFAULT '1',
+  `added` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `received` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`entry_id`),
+  KEY `item_owner` (`player_id`),
+  CONSTRAINT `player_web_rewards_ibfk_1` FOREIGN KEY (`player_id`) REFERENCES `players` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
