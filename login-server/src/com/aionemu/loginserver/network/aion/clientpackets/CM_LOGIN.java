951552956a73bc44299374ef500a376a15b6f914
Neon <neon@beyond-aion.com>
2017-01-12 22:15:43 +0100

    fixed exception on login without -loginex parameter, proper support for both modes (before, long usernames would not be fully read) client limit without -loginex: 14 character user names / 16 character passwords client limit with -loginex: 64 character user names / 32 character passwords

--- login-server/src/com/aionemu/loginserver/network/aion/clientpackets/CM_LOGIN.java
+++ login-server/src/com/aionemu/loginserver/network/aion/clientpackets/CM_LOGIN.java
@@ -1,9 +1,9 @@
 package com.aionemu.loginserver.network.aion.clientpackets;
 
 import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.security.GeneralSecurityException;
 import java.sql.Timestamp;
-import java.util.Iterator;
 
 import javax.crypto.Cipher;
 
@@ -21,11 +21,9 @@ import com.aionemu.loginserver.network.aion.SessionKey;
 import com.aionemu.loginserver.network.aion.serverpackets.SM_LOGIN_FAIL;
 import com.aionemu.loginserver.network.aion.serverpackets.SM_LOGIN_OK;
 import com.aionemu.loginserver.utils.BruteForceProtector;
-import com.google.common.primitives.Bytes;
-import com.mysql.jdbc.StringUtils;
 
 /**
- * @author -Nemesiss-, KID, Lyahim, Rolandas
+ * @author -Nemesiss-, KID, Lyahim, Rolandas, Neon
  */
 public class CM_LOGIN extends AionClientPacket {
 
@@ -35,15 +33,47 @@ public class CM_LOGIN extends AionClientPacket {
 	private static final Logger log = LoggerFactory.getLogger(CM_LOGIN.class);
 
 	/**
-	 * byte array contains encrypted login and password.
+	 * Byte arrays containing RSA encrypted user name and password. These parts are 128 bytes long and currently are known to appear in a quantity of
+	 * one or two (depending on the -loginex parameter).<br>
+	 * <br>
+	 * Starting the client without -loginex parameter, the username and pw are sent together in one dataPart. In this case, the user name can be 14
+	 * characters long and the password 16<br>
+	 * The offset where user and pw start seem to be fixed. Unused characters are zero padded, excessive characters are truncated.<br>
+	 * The last part always ends with 4 0xFF bytes (after decryption)<br>
+	 * Decrypted dataPart example for user: abcdefghijklmn pw: abcdefghijklmnop
+	 * 
+	 * <pre>
+	 * 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+	 * 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+	 * 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 61 62
+	 * 63 64 65 66 67 68 69 6A 6B 6C 6D 6E 61 62 63 64 65 66 67 68 69 6A 6B 6C 6D 6E 6F 70 FF FF FF FF
+	 * abcdefghijklmnabcdefghijklmnop????
+	 * </pre>
+	 * 
+	 * Using -loginex, the username can be up to 64 characters long and the password 32. Both are sent split over two dataParts<br>
+	 * Decrypted dataPart example for user: abcdefghijklmnopqrsabcdefghijklmnopqrsabcdefghijklmno3432432pqrs pw: 11111111111111111111111111111111
+	 * 
+	 * <pre>
+	 * 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 
+	 * 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 
+	 * 00 00 00 00 00 00 00 00 00 00 00 00 00 00 61 62 63 64 65 66 67 68 69 6A 6B 6C 6D 6E 6F 70 71 72 
+	 * 73 61 62 63 64 65 66 67 68 69 6A 6B 6C 6D 6E 6F 70 71 72 73 61 62 63 64 65 66 67 68 69 6A 6B 6C
+	 * abcdefghijklmnopqrsabcdefghijklmnopqrsabcdefghijkl
+	 * </pre>
+	 * 
+	 * <pre>
+	 * 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 
+	 * 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 
+	 * 00 00 00 00 00 00 00 00 00 00 00 00 00 00 6D 6E 6F 33 34 33 32 34 33 32 70 71 72 73 31 31 31 31 
+	 * 31 31 31 31 31 31 31 31 31 31 31 31 31 31 31 31 31 31 31 31 31 31 31 31 31 31 31 31 FF FF FF FF
+	 * mno3432432pqrs11111111111111111111111111111111????
+	 * </pre>
 	 */
-	private byte[] data1;
-	private byte[] data2;
-	@SuppressWarnings("unused")
+	private byte[][] dataParts;
 	private int sessionId;
 
 	/**
-	 * Constructs new instance of <tt>CM_LOGIN </tt> packet.
+	 * Constructs new instance of <tt>CM_LOGIN</tt> packet.
 	 * 
 	 * @param buf
 	 * @param client
@@ -54,54 +84,49 @@ public class CM_LOGIN extends AionClientPacket {
 
 	@Override
 	protected void readImpl() {
-		data1 = readB(128);
-		data2 = readB(128);
+		int dataPartCount = (getRemainingBytes() - 55) / 128;
+		dataParts = new byte[dataPartCount][];
+		for (int i = 0; i < dataPartCount; i++)
+			dataParts[i] = readB(128);
 		sessionId = readD();
-		readB(16); // zeroes
-		readB(7); // 0x20
-		readB(16); // static bytes: 34 3A 0E F7 AE F3 9A E8 2D 09 7F 0B E0 19 78 89
-		readD(); // 0x0
+		readB(16); // 0
+		readB(7); // static bytes: 20 00 00 00 00 00 01
+		readB(16); // static bytes: 9D DA 47 A7 21 C0 A6 A5 4B B7 5E E3 CE C9 26 AA
+		readD(); // 0
 		readD(); // unk
-		readD(); // 0x0
+		readD(); // 0
 	}
 
 	@Override
 	protected void runImpl() {
-		byte[] decrypted1, decrypted2;
-		try {
-			Cipher rsaCipher = Cipher.getInstance("RSA/ECB/nopadding");
-			rsaCipher.init(Cipher.DECRYPT_MODE, getConnection().getRSAPrivateKey());
-			decrypted1 = rsaCipher.doFinal(data1, 0, 128);
-		} catch (GeneralSecurityException e) {
+		if (getConnection().getSessionId() != sessionId) {
 			sendPacket(new SM_LOGIN_FAIL(AionAuthResponse.STR_L2AUTH_S_SYSTEM_ERROR));
 			return;
 		}
 
+		String decrypted = "";
 		try {
 			Cipher rsaCipher = Cipher.getInstance("RSA/ECB/nopadding");
 			rsaCipher.init(Cipher.DECRYPT_MODE, getConnection().getRSAPrivateKey());
-			decrypted2 = rsaCipher.doFinal(data2, 0, 128);
-		} catch (GeneralSecurityException e) {
-			sendPacket(new SM_LOGIN_FAIL(AionAuthResponse.STR_L2AUTH_S_SYSTEM_ERROR));
-			return;
-		}
-
-		Iterator<Byte> bytes = Bytes.asList(decrypted1).iterator();
-		String user = readStringFromBlock(bytes);
-		String password = readStringFromBlock(bytes);
-
-		if (StringUtils.isNullOrEmpty(password)) {
-			// Second block if -loginex switch in client was used.
-			bytes = Bytes.asList(decrypted2).iterator();
-			password = readStringFromBlock(bytes);
-			if (StringUtils.isNullOrEmpty(password)) {
-				sendPacket(new SM_LOGIN_FAIL(AionAuthResponse.STR_L2AUTH_S_INCORRECT_PWD));
-				return;
+			int leadingNulls = -1;
+			for (byte[] dataPart : dataParts) {
+				String decryptedDataPart = new String(rsaCipher.doFinal(dataPart), StandardCharsets.UTF_8);
+				if (leadingNulls == -1) // init once, since subsequent parts may also have \u0000's where the end of very long user names would be
+					leadingNulls = decryptedDataPart.indexOf(decryptedDataPart.trim());
+				decrypted += decryptedDataPart.substring(leadingNulls);
 			}
+			decrypted = decrypted.substring(0, decrypted.indexOf(0xFFFD));
+		} catch (GeneralSecurityException e) {
+			sendPacket(new SM_LOGIN_FAIL(AionAuthResponse.STR_L2AUTH_S_SYSTEM_ERROR));
+			return;
 		}
+
+		int userNameLength = dataParts.length == 1 ? 14 : 64; // if more than 1 dataParts, -loginex parameter was used (long user name / pw support)
+		String userName = decrypted.substring(0, userNameLength).trim();
+		String password = decrypted.substring(userNameLength).trim();
 
 		LoginConnection client = getConnection();
-		AionAuthResponse response = AccountController.login(user, password, client);
+		AionAuthResponse response = AccountController.login(userName, password, client);
 		if (response == null) // e.g. when account is banned
 			return;
 		switch (response) {
@@ -117,7 +142,7 @@ public class CM_LOGIN extends AionClientPacket {
 					if ((!ip.equals("127.0.0.1")) && BruteForceProtector.getInstance().addFailedConnect(ip)) {
 						Timestamp newTime = new Timestamp(System.currentTimeMillis() + Config.WRONG_LOGIN_BAN_TIME * 60000);
 						BannedIpController.banIp(ip, newTime);
-						log.debug(user + " on " + ip + " banned for " + Config.WRONG_LOGIN_BAN_TIME + " min. bruteforce");
+						log.debug(userName + " on " + ip + " banned for " + Config.WRONG_LOGIN_BAN_TIME + " min. bruteforce");
 						client.close(new SM_LOGIN_FAIL(AionAuthResponse.STR_L2AUTH_S_BLOCKED_IP));
 						break;
 					}
@@ -128,20 +153,5 @@ public class CM_LOGIN extends AionClientPacket {
 				client.close(new SM_LOGIN_FAIL(response));
 				break;
 		}
-	}
-
-	private String readStringFromBlock(Iterator<Byte> bytes) {
-		StringBuilder sb = new StringBuilder();
-		while (bytes.hasNext()) {
-			char ch = (char) bytes.next().byteValue();
-			if (ch == 0 || (byte) ch == -1) {
-				if (sb.length() == 0)
-					continue;
-				else
-					break;
-			}
-			sb.append(ch);
-		}
-		return sb.toString();
 	}
 }
