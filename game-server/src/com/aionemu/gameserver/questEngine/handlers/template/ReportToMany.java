beyond-aion/aion-server

commit 397443b67aeb0badd32694034a9938e015378e20

ReportToMany quest rework: - fixed some quests would skip one step and directly rewarded the player - fixed no work items would be added during quest steps - fixed skipped dialogs in some cases - removed obsolete xml dialog attributes since it now sends the correct ids automatically - fixed some conditions where quests could get bugged (e.g. when deleting work items)

game-server/src/com/aionemu/gameserver/questEngine/handlers/template/ReportToMany.java
@@ -1,8 +1,11 @@
 package com.aionemu.gameserver.questEngine.handlers.template;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+
+import org.slf4j.LoggerFactory;
 
 import com.aionemu.gameserver.dataholders.DataManager;
 import com.aionemu.gameserver.model.DialogAction;
@@ -17,56 +20,29 @@ import com.aionemu.gameserver.questEngine.model.QuestState;
 import com.aionemu.gameserver.questEngine.model.QuestStatus;
 import com.aionemu.gameserver.services.QuestService;
 
-import javolution.util.FastMap;
-
 /**
  * @author Hilgert
  * @modified vlog, Pad
+ * @reworked Neon
  */
 public class ReportToMany extends QuestHandler {
 
 	private final int startItemId;
 	private final Set<Integer> startNpcIds = new HashSet<>();
-	private final Set<Integer> endNpcIds = new HashSet<>();
 	private final int startDialogId;
-	private final int endDialogId;
-	private final int maxVar;
-	private final FastMap<List<Integer>, NpcInfos> npcInfos;
-	private boolean mission;
-	private boolean isDataDriven;
-	private QuestItems workItem;
+	private final List<NpcInfos> npcInfos = new ArrayList<>();
+	private final boolean mission;
+	private final boolean isDataDriven;
 
-	/**
-	 * @param questId
-	 * @param startItemId
-	 * @param endNpc
-	 * @param startDialogId
-	 * @param endDialogId
-	 * @param maxVar
-	 */
-	public ReportToMany(int questId, int startItemId, List<Integer> startNpcIds, List<Integer> endNpcIds, FastMap<List<Integer>, NpcInfos> npcInfos,
-		int startDialogId, int endDialogId, int maxVar, boolean mission) {
+	public ReportToMany(int questId, int startItemId, List<Integer> startNpcIds, List<NpcInfos> npcInfos, int startDialogId, boolean mission) {
 		super(questId);
 		this.startItemId = startItemId;
 		if (startNpcIds != null)
 			this.startNpcIds.addAll(startNpcIds);
-		if (endNpcIds != null)
-			this.endNpcIds.addAll(endNpcIds);
-		else
-			this.endNpcIds.addAll(this.startNpcIds);
-		this.npcInfos = npcInfos;
+		this.npcInfos.addAll(npcInfos);
 		this.startDialogId = startDialogId;
-		this.endDialogId = endDialogId;
-		this.maxVar = maxVar;
 		this.mission = mission;
-		isDataDriven = DataManager.QUEST_DATA.getQuestById(questId).isDataDriven();
-	}
-
-	@Override
-	protected void onWorkItemsLoaded() {
-		if (workItems == null)
-			return;
-		workItem = workItems.get(0);
+		this.isDataDriven = DataManager.QUEST_DATA.getQuestById(questId).isDataDriven();
 	}
 
 	@Override
@@ -82,14 +58,10 @@ public class ReportToMany extends QuestHandler {
 				qe.registerQuestNpc(startNpcId).addOnTalkEvent(questId);
 			}
 		}
-		for (List<Integer> npcIds : npcInfos.keySet()) {
-			for (int npcId : npcIds) {
+		for (NpcInfos npcInfo : npcInfos) {
+			for (int npcId : npcInfo.getNpcIds()) {
 				qe.registerQuestNpc(npcId).addOnTalkEvent(questId);
 			}
-		}
-		if (!endNpcIds.equals(startNpcIds)) {
-			for (Integer endNpcId : endNpcIds)
-				qe.registerQuestNpc(endNpcId).addOnTalkEvent(questId);
 		}
 	}
 
@@ -101,121 +73,99 @@ public class ReportToMany extends QuestHandler {
 		int targetId = env.getTargetId();
 
 		if (qs == null || qs.isStartable()) {
-			if (startItemId != 0) {
+			if ((startNpcIds.isEmpty() || startNpcIds.contains(targetId))
+				&& (startItemId == 0 || player.getInventory().getFirstItemByItemId(startItemId) != null)) {
 				switch (dialog) {
 					case QUEST_ACCEPT:
 					case QUEST_ACCEPT_1:
 					case QUEST_ACCEPT_SIMPLE:
-						if (QuestService.startQuest(env)) {
-							if (workItem != null)
-								giveQuestItem(env, workItem.getItemId(), workItem.getCount());
-						}
+						QuestService.startQuest(env);
 						return closeDialogWindow(env);
-					case QUEST_REFUSE:
-					case QUEST_REFUSE_1:
-					case QUEST_REFUSE_2:
-					case QUEST_REFUSE_3:
-					case QUEST_REFUSE_4:
-					case QUEST_REFUSE_SIMPLE:
-						return closeDialogWindow(env);
-				}
-			}
-			if (startNpcIds.isEmpty() || startNpcIds.contains(targetId)) {
-				switch (dialog) {
 					case QUEST_SELECT:
 						return sendQuestDialog(env, startDialogId != 0 ? startDialogId : isDataDriven ? 4762 : 1011);
 					default:
-						return sendQuestStartDialog(env, workItem);
+						return super.onDialogEvent(env);
 				}
 			}
 		} else if (qs.getStatus() == QuestStatus.START) {
-			int var = qs.getQuestVarById(0);
-			NpcInfos targetNpcInfo = null;
-			for (NpcInfos npcInfo : npcInfos.values()) {
-				if (npcInfo.getNpcIds().contains(targetId)) {
-					targetNpcInfo = npcInfo;
-					break;
-				}
+			int step = qs.getQuestVarById(0); // starting from 0
+			if (step > getMaxStep()) {
+				LoggerFactory.getLogger(ReportToMany.class).warn("Missing NpcInfo for quest " + questId + " step #" + (step + 1));
+				return false;
 			}
-			if (var <= maxVar) {
-				if (targetNpcInfo != null) {
-					if (dialog == DialogAction.SET_SUCCEED) {
-						qs.setQuestVar(maxVar);
-						qs.setStatus(QuestStatus.REWARD);
-						updateQuestStatus(env);
-						return closeDialogWindow(env);
-					}
+			NpcInfos targetNpcInfo = npcInfos.get(step);
+			if (!targetNpcInfo.getNpcIds().contains(targetId))
+				return false;
 
-					int closeDialogId = getCloseDialogId(var, targetNpcInfo);
-					if (dialog.id() == closeDialogId) {
-						if (dialog != DialogAction.CHECK_USER_HAS_QUEST_ITEM && dialog != DialogAction.CHECK_USER_HAS_QUEST_ITEM_SIMPLE
-							|| QuestService.collectItemCheck(env, true)) {
-							if (var == maxVar) {
-								if (closeDialogId == DialogAction.SELECT_QUEST_REWARD.id() || closeDialogId == DialogAction.CHECK_USER_HAS_QUEST_ITEM_SIMPLE.id()) {
-									qs.setStatus(QuestStatus.REWARD);
-									updateQuestStatus(env);
-									return sendQuestDialog(env, 5);
-								}
-							} else {
-								qs.setQuestVarById(0, var + 1);
-							}
-							updateQuestStatus(env);
-						}
+			switch (dialog) {
+				case QUEST_SELECT:
+					return sendQuestDialog(env, getDialogId(step));
+				case SETPRO1:
+				case SETPRO2:
+				case SETPRO3:
+				case SETPRO4:
+				case SETPRO5:
+				case SETPRO6:
+				case SETPRO7:
+				case SETPRO8:
+				case SETPRO9:
+				case SETPRO10:
+				case SETPRO11:
+				case SETPRO12:
+					changeQuestStep(env, step, step + 1);
+					if (workItems != null && workItems.size() > step)
+						giveQuestItem(env, workItems.get(step).getItemId(), workItems.get(step).getCount());
+					return closeDialogWindow(env);
+				case SET_SUCCEED:
+				case SELECT_QUEST_REWARD:
+				case CHECK_USER_HAS_QUEST_ITEM:
+				case CHECK_USER_HAS_QUEST_ITEM_SIMPLE:
+					if (dialog == DialogAction.SET_SUCCEED) // set reward from pre-end npc (end npc is another one who will then give the reward)
+						step++;
+					if (step < getMaxStep() || !validateAndRemoveItems(env, dialog))
 						return sendQuestSelectionDialog(env);
-					}
-
-					int dialogId = getDialogId(var, targetNpcInfo);
-					if (dialog == DialogAction.QUEST_SELECT) {
-						return sendQuestDialog(env, dialogId);
-					} else if (dialog.id() == dialogId + 1 && targetNpcInfo.getMovie() != 0) {
-						sendQuestDialog(env, dialogId + 1);
-						return playQuestMovie(env, targetNpcInfo.getMovie());
-					}
-				}
-			} else if (var > maxVar) {
-				if (endNpcIds.contains(targetId)) {
-					if (dialog == DialogAction.QUEST_SELECT) {
-						return sendQuestDialog(env, endDialogId != 0 ? endDialogId : isDataDriven ? 10002 : 2375);
-					} else if (dialog == DialogAction.SELECT_QUEST_REWARD) {
-						if (startItemId != 0) {
-							if (!removeQuestItem(env, startItemId, 1)) {
-								return false;
-							}
-						}
-						if (workItem != null) {
-							long count = player.getInventory().getItemCountByItemId(workItem.getItemId());
-							if (count < workItem.getCount()) {
-								return sendQuestSelectionDialog(env);
-							}
-							removeQuestItem(env, workItem.getItemId(), count, QuestStatus.COMPLETE);
-						}
-						qs.setStatus(QuestStatus.REWARD);
-						updateQuestStatus(env);
-						return sendQuestEndDialog(env);
-					}
-				}
+					qs.setStatus(QuestStatus.REWARD);
+					updateQuestStatus(env);
+					return sendQuestEndDialog(env);
+				default:
+					if (targetNpcInfo.getMovie() != 0)
+						playQuestMovie(env, targetNpcInfo.getMovie());
+					return super.onDialogEvent(env);
 			}
-		} else if (qs.getStatus() == QuestStatus.REWARD && endNpcIds.contains(targetId)) {
-			int var = qs.getQuestVarById(0);
-			NpcInfos targetNpcInfo = null;
-			for (NpcInfos npcInfo : npcInfos.values()) {
-				if (npcInfo.getNpcIds().contains(targetId)) {
-					targetNpcInfo = npcInfo;
-					break;
-				}
-			}
-			if (var >= maxVar && targetNpcInfo != null) {
-				int closeDialogId = getCloseDialogId(var, targetNpcInfo);
-				if (dialog == DialogAction.USE_OBJECT) {
-					if (closeDialogId == DialogAction.SELECT_QUEST_REWARD.id() || closeDialogId == DialogAction.CHECK_USER_HAS_QUEST_ITEM_SIMPLE.id())
-						return sendQuestEndDialog(env);
-					int dialogId = getDialogId(var, targetNpcInfo);
-					return sendQuestDialog(env, dialogId);
-				}
-				return sendQuestEndDialog(env);
-			}
+		} else if (qs.getStatus() == QuestStatus.REWARD) {
+			NpcInfos endNpcInfo = npcInfos.get(getMaxStep());
+			if (!endNpcInfo.getNpcIds().contains(targetId))
+				return false;
+			int step = qs.getQuestVarById(0);
+			boolean rewardStatusFromRewardNpc = step == getMaxStep(); // false if reward status was set by SET_SUCCEED action
+			if (dialog == DialogAction.USE_OBJECT && !rewardStatusFromRewardNpc)
+				return sendQuestDialog(env, isDataDriven ? 10002 : 2375); // show full reward dialog instead of last page
+			return sendQuestEndDialog(env);
 		}
 		return false;
+	}
+
+	private int getMaxStep() {
+		return npcInfos.size() - 1;
+	}
+
+	private int getDialogId(int var) {
+		if (var == getMaxStep())
+			return isDataDriven ? 10002 : 2375;
+		else
+			return (isDataDriven ? 1011 : 1352) + var * 341;
+	}
+
+	private boolean validateAndRemoveItems(QuestEnv env, DialogAction dialog) {
+		if (!QuestService.collectItemCheck(env, true))
+			return false;
+		if (startItemId != 0 && !removeQuestItem(env, startItemId, 1))
+			return false;
+		if (workItems != null) {
+			for (QuestItems workItem : workItems)
+				removeQuestItem(env, workItem.getItemId(), workItem.getCount(), QuestStatus.COMPLETE);
+		}
+		return true;
 	}
 
 	@Override
@@ -233,27 +183,5 @@ public class ReportToMany extends QuestHandler {
 	@Override
 	public void onLevelChangedEvent(Player player) {
 		defaultOnLevelChangedEvent(player);
-	}
-
-	private int getCloseDialogId(int var, NpcInfos targetNpcInfo) {
-		if (targetNpcInfo.getCloseDialog() == 0) {
-			if (var == maxVar)
-				return 1009;
-			else
-				return 10000 + var;
-		} else {
-			return targetNpcInfo.getCloseDialog();
-		}
-	}
-
-	private int getDialogId(int var, NpcInfos targetNpcInfo) {
-		if (targetNpcInfo.getQuestDialog() == 0) {
-			if (var == maxVar)
-				return isDataDriven ? 10002 : 2375;
-			else
-				return (isDataDriven ? 1011 : 1352) + var * 341;
-		} else {
-			return targetNpcInfo.getQuestDialog();
-		}
 	}
 }

game-server/data/static_data/quest_script_data/adma_stronghold.xml
@@ -11,7 +11,7 @@
 	-->
 	
 	<!-- REPORTING QUESTS -->
-	<report_to_many id="3116" start_npc_ids="798155" end_npc_ids="800420">
+	<report_to_many id="3116" start_npc_ids="798155">
 		<npc_infos npc_ids="798163"/>
 		<npc_infos npc_ids="800420"/>
 	</report_to_many>

game-server/data/static_data/quest_script_data/alabaster_order.xml
@@ -64,59 +64,59 @@
 
 	<!-- REPORTING QUESTS -->
 	<report_to id="38003" start_npc_ids="203726" end_npc_ids="799803"/>
-	<report_to_many id="35010" end_npc_ids="799805">
+	<report_to_many id="35010">
 		<npc_infos npc_ids="204560"/>
 		<npc_infos npc_ids="799805 799806"/>
 	</report_to_many>
-	<report_to_many id="35011" end_npc_ids="799805">
+	<report_to_many id="35011">
 		<npc_infos npc_ids="204504"/>
 		<npc_infos npc_ids="799805 799806"/>
 	</report_to_many>
-	<report_to_many id="35017" end_npc_ids="799806">
+	<report_to_many id="35017">
 		<npc_infos npc_ids="798155"/>
 		<npc_infos npc_ids="799805 799806"/>
 	</report_to_many>
-	<report_to_many id="35018" end_npc_ids="799806">
+	<report_to_many id="35018">
 		<npc_infos npc_ids="798135"/>
 		<npc_infos npc_ids="799805 799806"/>
 	</report_to_many>
-	<report_to_many id="35024" end_npc_ids="799800 799801">
+	<report_to_many id="35024">
 		<npc_infos npc_ids="798946"/>
 		<npc_infos npc_ids="799800 799801"/>
 	</report_to_many>
-	<report_to_many id="35025" end_npc_ids="799800 799801">
+	<report_to_many id="35025">
 		<npc_infos npc_ids="798972"/>
 		<npc_infos npc_ids="799800 799801"/>
 	</report_to_many>
-	<report_to_many id="35026" end_npc_ids="799800 799801">
+	<report_to_many id="35026">
 		<npc_infos npc_ids="798996"/>
 		<npc_infos npc_ids="799800 799801"/>
 	</report_to_many>
-	<report_to_many id="35027" end_npc_ids="799800 799801">
+	<report_to_many id="35027">
 		<npc_infos npc_ids="799004"/>
 		<npc_infos npc_ids="799800 799801"/>
 	</report_to_many>
-	<report_to_many id="35028" end_npc_ids="799800 799801">
+	<report_to_many id="35028">
 		<npc_infos npc_ids="799015"/>
 		<npc_infos npc_ids="799800 799801"/>
 	</report_to_many>
-	<report_to_many id="35029" end_npc_ids="799800 799801">
+	<report_to_many id="35029">
 		<npc_infos npc_ids="799043"/>
 		<npc_infos npc_ids="799800 799801"/>
 	</report_to_many>
-	<report_to_many id="35030" end_npc_ids="799800 799801">
+	<report_to_many id="35030">
 		<npc_infos npc_ids="799049"/>
 		<npc_infos npc_ids="799800 799801"/>
 	</report_to_many>
-	<report_to_many id="35055" end_npc_ids="804942">
+	<report_to_many id="35055">
 		<npc_infos npc_ids="804874"/>
 		<npc_infos npc_ids="804942"/>
 	</report_to_many>
-	<report_to_many id="35056" end_npc_ids="804943">
+	<report_to_many id="35056">
 		<npc_infos npc_ids="804704"/>
 		<npc_infos npc_ids="804943"/>
 	</report_to_many>
-	<report_to_many id="35057" end_npc_ids="804944">
+	<report_to_many id="35057">
 		<npc_infos npc_ids="804709"/>
 		<npc_infos npc_ids="804944"/>
 	</report_to_many>

game-server/data/static_data/quest_script_data/altgard.xml
@@ -151,7 +151,7 @@
 	<report_to id="2260" start_npc_ids="203649" end_npc_ids="203557"/>
 	<report_to id="2261" start_npc_ids="203559" end_npc_ids="203665"/>
 	<report_to id="2262" start_npc_ids="798030" end_npc_ids="798036"/>
-	<report_to_many id="2274" start_item_id="182203249" end_npc_ids="203560">
+	<report_to_many id="2274" start_item_id="182203249">
 		<npc_infos npc_ids="203668"/>
 		<npc_infos npc_ids="203560"/>
 	</report_to_many>
@@ -160,7 +160,7 @@
 	<report_to id="2286" start_npc_ids="203557" end_npc_ids="203560"/>
 	<report_to id="2287" start_npc_ids="203557" end_npc_ids="203558"/>
 	<report_to id="2291" start_npc_ids="203639" end_npc_ids="203617"/>
-	<report_to_many id="2296" start_item_id="182203263" end_npc_ids="798036">
+	<report_to_many id="2296" start_item_id="182203263">
 		<npc_infos npc_ids="203656"/>
 		<npc_infos npc_ids="798036"/>
 	</report_to_many>

game-server/data/static_data/quest_script_data/brusthonin.xml
@@ -133,11 +133,11 @@
 	<report_to id="4088" start_npc_ids="798063" end_npc_ids="205203"/>
 	<report_to id="4097" start_npc_ids="205158" end_npc_ids="730104"/>
 	<report_to id="4104" start_npc_ids="205193" end_npc_ids="205159"/>
-	<report_to_many id="39000" start_npc_ids="800165" end_npc_ids="800500">
+	<report_to_many id="39000" start_npc_ids="800165">
 		<npc_infos npc_ids="800501"/>
 		<npc_infos npc_ids="800500"/>
 	</report_to_many>
-	<report_to_many id="39003" start_npc_ids="800500" end_npc_ids="800504">
+	<report_to_many id="39003" start_npc_ids="800500">
 		<npc_infos npc_ids="800512"/>
 		<npc_infos npc_ids="800504"/>
 	</report_to_many>

game-server/data/static_data/quest_script_data/daevanion.xml
@@ -70,10 +70,10 @@
 	
 	<!-- REPORTING QUESTS -->
 	<report_to_many id="80310" start_npc_ids="831384">
-		<npc_infos npc_ids="831384" quest_dialog="1003"/>
+		<npc_infos npc_ids="831384"/>
 	</report_to_many>
 	<report_to_many id="80311" start_npc_ids="831387">
-		<npc_infos npc_ids="831387" quest_dialog="1003"/>
+		<npc_infos npc_ids="831387"/>
 	</report_to_many>
 	
 	<!-- COLLECTING QUESTS -->

game-server/data/static_data/quest_script_data/empyrean_crucible.xml
@@ -58,7 +58,7 @@
 	<!-- REPORTING QUESTS -->
 	<report_to id="18200" start_npc_ids="798604" end_npc_ids="799088"/>
 	<report_to id="18207" start_npc_ids="205309" end_npc_ids="205316"/>
-	<report_to_many id="18210" start_npc_ids="205315" end_npc_ids="205985">
+	<report_to_many id="18210" start_npc_ids="205315">
 		<npc_infos npc_ids="205985"/>
 	</report_to_many>
 	<report_to_many id="18215" start_npc_ids="205315">
@@ -66,7 +66,7 @@
 	</report_to_many>
 	<report_to id="28200" start_npc_ids="798804" end_npc_ids="799428"/>
 	<report_to id="28207" start_npc_ids="205321" end_npc_ids="205320"/>
-	<report_to_many id="28210" start_npc_ids="205323" end_npc_ids="205986">
+	<report_to_many id="28210" start_npc_ids="205323">
 		<npc_infos npc_ids="205986"/>
 	</report_to_many>
 	<report_to_many id="28215" start_npc_ids="205323">

game-server/data/static_data/quest_script_data/fatebound_abbey.xml
@@ -37,13 +37,13 @@
 	-->
 	
 	<!-- REPORTING QUESTS -->
-	<report_to_many id="29601" start_npc_ids="804662" end_npc_ids="804666">
+	<report_to_many id="29601" start_npc_ids="804662">
 		<npc_infos npc_ids="804663"/>
 		<npc_infos npc_ids="804664"/>
 		<npc_infos npc_ids="804665"/>
 		<npc_infos npc_ids="804666"/>
 	</report_to_many>
-	<report_to_many id="29602" start_npc_ids="804663" end_npc_ids="205150">
+	<report_to_many id="29602" start_npc_ids="804663">
 		<npc_infos npc_ids="205986"/>
 		<npc_infos npc_ids="205150"/>
 	</report_to_many>
@@ -51,7 +51,7 @@
 	<report_to id="29605" start_npc_ids="804663" end_npc_ids="800421"/>
 	<report_to id="29606" start_npc_ids="804663" end_npc_ids="205225"/>
 	<report_to id="29607" start_npc_ids="804663" end_npc_ids="204837"/>
-	<report_to_many id="29608" start_npc_ids="804664" end_npc_ids="799225">
+	<report_to_many id="29608" start_npc_ids="804664">
 		<npc_infos npc_ids="205986"/>
 		<npc_infos npc_ids="799225"/>
 	</report_to_many>

commit be57d9b0974a93c407ff735c6a5361b58efd98ba

fixed incorrect quest end npc shown in quest log in some cases (introduced by 397443b), added workaround to keep the end npc dialog behavior the same

game-server/src/com/aionemu/gameserver/questEngine/handlers/template/ReportToMany.java
@@ -33,6 +33,7 @@ public class ReportToMany extends QuestHandler {
 	private final List<NpcInfos> npcInfos = new ArrayList<>();
 	private final boolean mission;
 	private final boolean isDataDriven;
+	private boolean rewardStatusFromRewardNpc = true; // workaround flag for end npc dialog behavior (see below)
 
 	public ReportToMany(int questId, int startItemId, List<Integer> startNpcIds, List<NpcInfos> npcInfos, int startDialogId, boolean mission) {
 		super(questId);
@@ -122,10 +123,13 @@ public class ReportToMany extends QuestHandler {
 				case SELECT_QUEST_REWARD:
 				case CHECK_USER_HAS_QUEST_ITEM:
 				case CHECK_USER_HAS_QUEST_ITEM_SIMPLE:
-					if (dialog == DialogAction.SET_SUCCEED) // set reward from pre-end npc (end npc is another one who will then give the reward)
+					if (dialog == DialogAction.SET_SUCCEED) { // set reward from pre-end npc (end npc is another one who will then give the reward)
+						rewardStatusFromRewardNpc = false;
 						step++;
+					}
 					if (step < getMaxStep() || !validateAndRemoveItems(env, dialog))
 						return sendQuestSelectionDialog(env);
+					qs.setQuestVarById(0, step);
 					qs.setStatus(QuestStatus.REWARD);
 					updateQuestStatus(env);
 					return sendQuestEndDialog(env);
@@ -138,10 +142,8 @@ public class ReportToMany extends QuestHandler {
 			NpcInfos endNpcInfo = npcInfos.get(getMaxStep());
 			if (!endNpcInfo.getNpcIds().contains(targetId))
 				return false;
-			int step = qs.getQuestVarById(0);
-			boolean rewardStatusFromRewardNpc = step == getMaxStep(); // false if reward status was set by SET_SUCCEED action
-			if (dialog == DialogAction.USE_OBJECT && !rewardStatusFromRewardNpc)
-				return sendQuestDialog(env, isDataDriven ? 10002 : 2375); // show full reward dialog instead of last page
+			if (dialog == DialogAction.USE_OBJECT && !rewardStatusFromRewardNpc) // if talking to an end npc who did not set the reward state himself
+				return sendQuestDialog(env, isDataDriven ? 10002 : 2375); // show full reward dialog instead of only last page (otherwise it's never readable)
 			return sendQuestEndDialog(env);
 		}
 		return false;