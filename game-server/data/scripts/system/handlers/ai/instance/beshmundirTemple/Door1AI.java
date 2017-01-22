df3ba4401884736c9fa546b0d2321c1089e0b898
Neon <neon@beyond-aion.com>
2017-01-22 23:21:15 +0100

    bt door AI improvements and prequest id fix

--- game-server/data/scripts/system/handlers/ai/instance/beshmundirTemple/Door1AI.java
+++ game-server/data/scripts/system/handlers/ai/instance/beshmundirTemple/Door1AI.java
@@ -2,38 +2,32 @@ package ai.instance.beshmundirTemple;
 
 import com.aionemu.gameserver.ai.AIActions;
 import com.aionemu.gameserver.ai.AIName;
+import com.aionemu.gameserver.model.DialogPage;
 import com.aionemu.gameserver.model.Race;
 import com.aionemu.gameserver.model.gameobjects.player.Player;
 import com.aionemu.gameserver.network.aion.serverpackets.SM_DIALOG_WINDOW;
-import com.aionemu.gameserver.questEngine.model.QuestState;
-import com.aionemu.gameserver.questEngine.model.QuestStatus;
 import com.aionemu.gameserver.utils.PacketSendUtility;
 
 import ai.ActionItemNpcAI;
+import javolution.util.FastTable;
 
 /**
- * @author Tibald
+ * @author Tibald, Neon
  */
 @AIName("door1")
 public class Door1AI extends ActionItemNpcAI {
 
 	@Override
 	protected void handleDialogStart(Player player) {
-		QuestState qsneedasmo = player.getQuestStateList().getQuestState(30307);
-		QuestState qsneedelyos = player.getQuestStateList().getQuestState(30207);
-		if (player.getRace() == Race.ELYOS) {
-			if (qsneedelyos != null && qsneedelyos.getStatus() != QuestStatus.NONE) {// TODO: Only one player in group has to // have this quest
+		int questId = player.getRace() == Race.ELYOS ? 30208 : 30308;
+		// Only one player in group has to have this quest
+		for (Player member : player.isInGroup() ? player.getPlayerGroup().getOnlineMembers() : FastTable.of(player)) {
+			if (member.getQuestStateList().hasQuest(questId)) {
 				super.handleDialogStart(player);
-			} else {
-				PacketSendUtility.sendPacket(player, new SM_DIALOG_WINDOW(getObjectId(), 27));
-			}
-		} else {
-			if (qsneedasmo != null && qsneedasmo.getStatus() != QuestStatus.NONE) { // TODO: Only one player in group has to // have this quest
-				super.handleDialogStart(player);
-			} else {
-				PacketSendUtility.sendPacket(player, new SM_DIALOG_WINDOW(getObjectId(), 27));
+				return;
 			}
 		}
+		PacketSendUtility.sendPacket(player, new SM_DIALOG_WINDOW(getObjectId(), DialogPage.NO_RIGHT.id()));
 	}
 
 	@Override
--- game-server/data/scripts/system/handlers/ai/instance/beshmundirTemple/Door2AI.java
+++ game-server/data/scripts/system/handlers/ai/instance/beshmundirTemple/Door2AI.java
@@ -2,38 +2,32 @@ package ai.instance.beshmundirTemple;
 
 import com.aionemu.gameserver.ai.AIActions;
 import com.aionemu.gameserver.ai.AIName;
+import com.aionemu.gameserver.model.DialogPage;
 import com.aionemu.gameserver.model.Race;
 import com.aionemu.gameserver.model.gameobjects.player.Player;
 import com.aionemu.gameserver.network.aion.serverpackets.SM_DIALOG_WINDOW;
-import com.aionemu.gameserver.questEngine.model.QuestState;
-import com.aionemu.gameserver.questEngine.model.QuestStatus;
 import com.aionemu.gameserver.utils.PacketSendUtility;
 
 import ai.ActionItemNpcAI;
+import javolution.util.FastTable;
 
 /**
- * @author Gigi
+ * @author Gigi, Neon
  */
 @AIName("door2")
 public class Door2AI extends ActionItemNpcAI {
 
 	@Override
 	protected void handleDialogStart(Player player) {
-		QuestState qsneedasmo = player.getQuestStateList().getQuestState(30311);
-		QuestState qsneedelyos = player.getQuestStateList().getQuestState(30211);
-		if (player.getRace() == Race.ELYOS) {
-			if (qsneedelyos != null && qsneedelyos.getStatus() != QuestStatus.NONE) {// TODO: Only one player in group has to // have this quest
+		int questId = player.getRace() == Race.ELYOS ? 30211 : 30311;
+		// Only one player in group has to have this quest
+		for (Player member : player.isInGroup() ? player.getPlayerGroup().getOnlineMembers() : FastTable.of(player)) {
+			if (member.getQuestStateList().hasQuest(questId)) {
 				super.handleDialogStart(player);
-			} else {
-				PacketSendUtility.sendPacket(player, new SM_DIALOG_WINDOW(getObjectId(), 27));
-			}
-		} else {
-			if (qsneedasmo != null && qsneedasmo.getStatus() != QuestStatus.NONE) { // TODO: Only one player in group has to // have this quest
-				super.handleDialogStart(player);
-			} else {
-				PacketSendUtility.sendPacket(player, new SM_DIALOG_WINDOW(getObjectId(), 27));
+				return;
 			}
 		}
+		PacketSendUtility.sendPacket(player, new SM_DIALOG_WINDOW(getObjectId(), DialogPage.NO_RIGHT.id()));
 	}
 
 	@Override
