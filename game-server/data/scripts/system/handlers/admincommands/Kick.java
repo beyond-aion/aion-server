81430649777d91d0963bba314bbbbdb80c9cfc48
Neon <neon@beyond-aion.com>
2017-01-29 12:27:36 +0100

    reworked kick command

--- game-server/data/scripts/system/handlers/admincommands/Kick.java
+++ game-server/data/scripts/system/handlers/admincommands/Kick.java
@@ -2,48 +2,54 @@ package admincommands;
 
 import com.aionemu.gameserver.model.gameobjects.player.Player;
 import com.aionemu.gameserver.network.aion.serverpackets.SM_QUIT_RESPONSE;
+import com.aionemu.gameserver.network.aion.serverpackets.SM_SYSTEM_MESSAGE;
 import com.aionemu.gameserver.utils.PacketSendUtility;
 import com.aionemu.gameserver.utils.Util;
 import com.aionemu.gameserver.utils.chathandlers.AdminCommand;
 import com.aionemu.gameserver.world.World;
 
 /**
- * @author Elusive
+ * @author Elusive, Neon
  */
 public class Kick extends AdminCommand {
 
 	public Kick() {
-		super("kick");
+		super("kick", "Disconnects players from the server.");
+
+		// @formatter:off
+		setParamInfo(
+			"<name> - Disconnects the player with the specified name.",
+			"<ALL> - Disconnects everyone (parameter must be typed in uppercase, for safety)."
+		);
+		// @formatter:on
 	}
 
 	@Override
 	public void execute(Player admin, String... params) {
-		if (params.length < 1) {
-			PacketSendUtility.sendMessage(admin, "syntax //kick <character_name> | <All>");
+		if (params.length == 0) {
+			sendInfo(admin);
 			return;
 		}
 
-		if (params[0] != null && "All".equalsIgnoreCase(params[0])) {
-			for (final Player player : World.getInstance().getAllPlayers()) {
-				if (!player.isGM()) {
-					player.getClientConnection().close(new SM_QUIT_RESPONSE());
-					PacketSendUtility.sendMessage(admin, "Kicked player : " + player.getName());
-				}
+		if ("ALL".equals(params[0])) {
+			if (World.getInstance().getAllPlayers().size() == 1) {
+				sendInfo(admin, "There is nobody online to kick.");
+				return;
 			}
+			World.getInstance().forEachPlayer(player -> {
+				if (!player.equals(admin)) {
+					player.getClientConnection().close(new SM_QUIT_RESPONSE());
+					PacketSendUtility.sendPacket(admin, SM_SYSTEM_MESSAGE.STR_PARTY_HE_BECOME_OFFLINE(player.getName()));
+				}
+			});
 		} else {
 			Player player = World.getInstance().findPlayer(Util.convertName(params[0]));
 			if (player == null) {
-				PacketSendUtility.sendMessage(admin, "The specified player is not online.");
+				PacketSendUtility.sendPacket(admin, SM_SYSTEM_MESSAGE.STR_BUDDYLIST_NO_OFFLINE_CHARACTER());
 				return;
 			}
 			player.getClientConnection().close(new SM_QUIT_RESPONSE());
-			PacketSendUtility.sendMessage(admin, "Kicked player : " + player.getName());
+			PacketSendUtility.sendPacket(admin, SM_SYSTEM_MESSAGE.STR_PARTY_HE_BECOME_OFFLINE(player.getName()));
 		}
-
-	}
-
-	@Override
-	public void info(Player player, String message) {
-		PacketSendUtility.sendMessage(player, "syntax //kick <character_name> | <All>");
 	}
 }
