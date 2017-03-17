396e93aca568c235202dfcf2741079e60b8b8eb7
Neon <neon@beyond-aion.com>
2017-03-18 00:32:38 +0100

    removed ant lib and updated jansi

--- commons/src/com/aionemu/commons/utils/info/VersionInfo.java
+++ commons/src/com/aionemu/commons/utils/info/VersionInfo.java
@@ -1,19 +1,18 @@
 package com.aionemu.commons.utils.info;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.text.SimpleDateFormat;
 import java.util.jar.Attributes;
 import java.util.jar.JarFile;
 
-import org.apache.tools.ant.launch.Locator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * @author lord_rex
- * @modified Neon
+ * @author lord_rex, Neon
  */
 public class VersionInfo {
 
@@ -28,23 +27,25 @@ public class VersionInfo {
 	 * Constructs a VersionInfo object holding the version information of the specified classes source (JAR)
 	 */
 	protected VersionInfo(Class<?> c) {
-		File sourceFile = Locator.getClassSource(c);
-		if (sourceFile.isDirectory()) { // when class is run from IDE
-			this.source = sourceFile.getPath(); // classes folder
-			try { // build date = last modified of the .class file
+		try {
+			File sourceFile = new File(c.getProtectionDomain().getCodeSource().getLocation().toURI());
+			if (!sourceFile.exists())
+				throw new FileNotFoundException();
+
+			if (sourceFile.isDirectory()) { // e.g. when class is run from IDE
+				source = sourceFile.getPath(); // classes folder
 				File classSource = new File(c.getClassLoader().getResource(c.getCanonicalName().replace('.', '/') + ".class").toURI());
-				this.date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'").format(classSource.lastModified());
-			} catch (SecurityException | URISyntaxException e) {
+				date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'").format(classSource.lastModified()); // build date = last modified of the .class file
+				return;
 			}
-			return;
-		}
-		this.source = sourceFile.getName();
-		try (JarFile jarFile = new JarFile(sourceFile)) {
-			Attributes attrs = jarFile.getManifest().getMainAttributes();
-			this.revision = attrs.getValue("Revision");
-			this.date = attrs.getValue("Date");
-			this.branch = attrs.getValue("Branch");
-		} catch (IOException e) {
+			source = sourceFile.getName();
+			try (JarFile jarFile = new JarFile(sourceFile)) {
+				Attributes attrs = jarFile.getManifest().getMainAttributes();
+				revision = attrs.getValue("Revision");
+				date = attrs.getValue("Date");
+				branch = attrs.getValue("Branch");
+			}
+		} catch (IOException | URISyntaxException e) {
 			log.error("Could not get manifest information", e);
 		}
 	}
@@ -66,11 +67,6 @@ public class VersionInfo {
 	}
 
 	public String[] getAllInfo() {
-		return new String[] {
-			"[" + source + "]",
-			" Revision:\t" + revision,
-			" Branch:\t" + branch,
-			" Build Date:\t" + date
-		};
+		return new String[] { "[" + source + "]", " Revision:\t" + revision, " Branch:\t" + branch, " Build Date:\t" + date };
 	}
 }
