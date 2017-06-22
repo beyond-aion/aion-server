25715278223c7359bb40ab76a5de9a39a4d9c082
Neon <neon@beyond-aion.com>
2017-06-22 22:47:38 +0200

    fixed NPE in SiegeWeaponMoveController and removed needless distance check

--- game-server/src/com/aionemu/gameserver/controllers/movement/SiegeWeaponMoveController.java
+++ game-server/src/com/aionemu/gameserver/controllers/movement/SiegeWeaponMoveController.java
@@ -2,6 +2,7 @@ package com.aionemu.gameserver.controllers.movement;
 
 import com.aionemu.gameserver.ai.AISubState;
 import com.aionemu.gameserver.model.gameobjects.Summon;
+import com.aionemu.gameserver.model.gameobjects.VisibleObject;
 import com.aionemu.gameserver.network.aion.serverpackets.SM_MOVE;
 import com.aionemu.gameserver.taskmanager.tasks.MoveTaskManager;
 import com.aionemu.gameserver.utils.PacketSendUtility;
@@ -38,10 +39,11 @@ public class SiegeWeaponMoveController extends SummonMoveController {
 			PacketSendUtility.broadcastPacket(owner, new SM_MOVE(owner));
 		}
 
-		if (PositionUtil.getDistance(owner.getTarget(), pointX, pointY, pointZ) > MOVE_CHECK_OFFSET) {
-			pointX = owner.getTarget().getX();
-			pointY = owner.getTarget().getY();
-			pointZ = owner.getTarget().getZ();
+		VisibleObject target = owner.getTarget();
+		if (target != null) { // update target position, in case target moved
+			pointX = target.getX();
+			pointY = target.getY();
+			pointZ = target.getZ();
 		}
 		moveToLocation(pointX, pointY, pointZ);
 		updateLastMove();
