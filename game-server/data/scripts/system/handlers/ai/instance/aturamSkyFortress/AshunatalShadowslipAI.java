0560529123acb5b077d604b40a9a3937c88b0dcf
Neon <neon@beyond-aion.com>
2017-07-16 06:59:42 +0200

    fixed #BA1596 and ashunatal not walking back as well as missing robot destroy animation https://beyond-aion.com/forum/thread/1596-nastika-aturam-sky-fortress/

--- game-server/data/scripts/system/handlers/ai/instance/aturamSkyFortress/AshunatalShadowslipAI.java
+++ game-server/data/scripts/system/handlers/ai/instance/aturamSkyFortress/AshunatalShadowslipAI.java
@@ -2,18 +2,15 @@ package ai.instance.aturamSkyFortress;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
-import com.aionemu.gameserver.ai.AIActions;
 import com.aionemu.gameserver.ai.AIName;
-import com.aionemu.gameserver.ai.AIState;
+import com.aionemu.gameserver.ai.manager.WalkManager;
 import com.aionemu.gameserver.model.EmotionType;
 import com.aionemu.gameserver.model.gameobjects.Creature;
-import com.aionemu.gameserver.model.gameobjects.Npc;
 import com.aionemu.gameserver.model.gameobjects.state.CreatureState;
+import com.aionemu.gameserver.model.skill.NpcSkillEntry;
 import com.aionemu.gameserver.network.aion.serverpackets.SM_EMOTION;
-import com.aionemu.gameserver.skillengine.SkillEngine;
 import com.aionemu.gameserver.utils.PacketSendUtility;
 import com.aionemu.gameserver.utils.ThreadPoolManager;
-import com.aionemu.gameserver.world.WorldPosition;
 
 import ai.AggressiveNpcAI;
 
@@ -24,7 +21,6 @@ import ai.AggressiveNpcAI;
 public class AshunatalShadowslipAI extends AggressiveNpcAI {
 
 	private AtomicBoolean isHome = new AtomicBoolean(true);
-	private boolean isSummoned;
 	private boolean canThink = true;
 
 	@Override
@@ -33,80 +29,41 @@ public class AshunatalShadowslipAI extends AggressiveNpcAI {
 	}
 
 	@Override
-	protected void handleAttack(Creature creature) {
-		super.handleAttack(creature);
+	protected void handleCreatureAggro(Creature creature) {
+		super.handleCreatureAggro(creature);
 		if (isHome.compareAndSet(true, false)) {
-			getPosition().getWorldMapInstance().getDoors().get(17).setOpen(true);
-			getPosition().getWorldMapInstance().getDoors().get(2).setOpen(true);
-		}
-		checkPercentage(getLifeStats().getHpPercentage());
-	}
-
-	private void checkPercentage(int hpPercentage) {
-		if (hpPercentage <= 50 && !isSummoned) {
-			isSummoned = true;
-			SkillEngine.getInstance().getSkill(getOwner(), 19428, 1, getOwner()).useNoAnimationSkill();
-			doSchedule();
+			getPosition().getWorldMapInstance().getDoors().get(17).setOpen(false);
 		}
 	}
 
 	@Override
 	protected void handleBackHome() {
 		isHome.set(true);
-		isSummoned = false;
 		super.handleBackHome();
 		getPosition().getWorldMapInstance().getDoors().get(17).setOpen(true);
-		getPosition().getWorldMapInstance().getDoors().get(2).setOpen(false);
-		Npc npc = getPosition().getWorldMapInstance().getNpc(219186);
-		if (npc != null) {
-			npc.getController().delete();
+	}
+
+	@Override
+	protected void handleDied() {
+		super.handleDied();
+		// ashunatal got killed early, therefore she could not spawn her shadow which usually would open the door
+		getPosition().getWorldMapInstance().getDoors().get(17).setOpen(true);
+		getPosition().getWorldMapInstance().getDoors().get(2).setOpen(true);
+	}
+
+	@Override
+	public void onEndUseSkill(NpcSkillEntry usedSkill) {
+		super.onEndUseSkill(usedSkill);
+		if (usedSkill.getSkillId() == 19417) {
+			canThink = false;
+			ThreadPoolManager.getInstance().schedule(() -> {
+				getSpawnTemplate().setWalkerId("3002400001");
+				getOwner().setState(CreatureState.ACTIVE, true);
+				WalkManager.startWalking(this);
+				PacketSendUtility.broadcastPacket(getOwner(), new SM_EMOTION(getOwner(), EmotionType.START_EMOTE2, 0, getObjectId()));
+				ThreadPoolManager.getInstance().schedule(() -> getOwner().getController().delete(), 4000);
+			}, 3000);
 		}
 	}
 
-	private void doSchedule() {
-		if (!isDead()) {
-			ThreadPoolManager.getInstance().schedule(new Runnable() {
-
-				@Override
-				public void run() {
-					if (!isDead()) {
-						SkillEngine.getInstance().getSkill(getOwner(), 19417, 49, getOwner()).useNoAnimationSkill();
-						ThreadPoolManager.getInstance().schedule(new Runnable() {
-
-							@Override
-							public void run() {
-								if (!isDead()) {
-									WorldPosition p = getPosition();
-									spawn(219186, p.getX(), p.getY(), p.getZ(), p.getHeading());
-									canThink = false;
-									getSpawnTemplate().setWalkerId("3002400001");
-									setStateIfNot(AIState.WALKING);
-									think();
-									getOwner().setState(CreatureState.ACTIVE, true);
-									PacketSendUtility.broadcastPacket(getOwner(), new SM_EMOTION(getOwner(), EmotionType.START_EMOTE2, 0, getObjectId()));
-									ThreadPoolManager.getInstance().schedule(new Runnable() {
-
-										@Override
-										public void run() {
-											if (!isDead()) {
-												despawn();
-											}
-										}
-
-									}, 4000);
-								}
-							}
-
-						}, 3000);
-					}
-				}
-
-			}, 2000);
-
-		}
-	}
-
-	private void despawn() {
-		AIActions.deleteOwner(this);
-	}
 }
--- game-server/data/scripts/system/handlers/ai/instance/aturamSkyFortress/ExplosionShadowsAI.java
+++ game-server/data/scripts/system/handlers/ai/instance/aturamSkyFortress/ExplosionShadowsAI.java
@@ -1,14 +1,15 @@
 package ai.instance.aturamSkyFortress;
 
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Consumer;
 
 import com.aionemu.gameserver.ai.AIActions;
 import com.aionemu.gameserver.ai.AIName;
-import com.aionemu.gameserver.model.gameobjects.Creature;
+import com.aionemu.gameserver.model.EmotionType;
 import com.aionemu.gameserver.model.gameobjects.Npc;
 import com.aionemu.gameserver.model.gameobjects.player.Player;
-import com.aionemu.gameserver.skillengine.SkillEngine;
+import com.aionemu.gameserver.model.skill.NpcSkillEntry;
+import com.aionemu.gameserver.network.aion.serverpackets.SM_EMOTION;
+import com.aionemu.gameserver.utils.PacketSendUtility;
 import com.aionemu.gameserver.utils.ThreadPoolManager;
 
 import ai.AggressiveNpcAI;
@@ -19,77 +20,40 @@ import ai.AggressiveNpcAI;
 @AIName("explosion_shadows")
 public class ExplosionShadowsAI extends AggressiveNpcAI {
 
-	private AtomicBoolean isHome = new AtomicBoolean(true);
-
-	@Override
-	protected void handleCreatureAggro(Creature creature) {
-		super.handleCreatureAggro(creature);
-		if (isHome.compareAndSet(true, false)) {
-			SkillEngine.getInstance().getSkill(getOwner(), 19428, 1, getOwner()).useNoAnimationSkill();
-			getPosition().getWorldMapInstance().getDoors().get(2).setOpen(true);
-			getPosition().getWorldMapInstance().getDoors().get(17).setOpen(true);
-			doSchedule();
-		}
-	}
-
 	@Override
 	protected void handleBackHome() {
-		isHome.set(true);
 		super.handleBackHome();
+		getPosition().getWorldMapInstance().getDoors().get(17).setOpen(true);
 	}
 
-	private void doSchedule() {
-		if (!isDead()) {
-			ThreadPoolManager.getInstance().schedule(new Runnable() {
+	@Override
+	protected void handleDespawned() {
+		super.handleDespawned();
+		getPosition().getWorldMapInstance().getDoors().get(17).setOpen(true);
+		getPosition().getWorldMapInstance().getDoors().get(2).setOpen(true);
+	}
 
-				@Override
-				public void run() {
-					if (!isDead()) {
-						SkillEngine.getInstance().getSkill(getOwner(), 19425, 49, getOwner()).useNoAnimationSkill();
-
-						ThreadPoolManager.getInstance().schedule(new Runnable() {
-
-							@Override
-							public void run() {
-								if (!isDead()) {
-									check();
-								}
-							}
-
-						}, 1500);
-					}
-				}
-
-			}, 3000);
-
-		}
+	@Override
+	public void onEndUseSkill(NpcSkillEntry usedSkill) {
+		super.onEndUseSkill(usedSkill);
+		if (usedSkill.getSkillId() == 19425) // Self Destruct
+			ThreadPoolManager.getInstance().schedule(() -> check(), 1500);
 	}
 
 	private void check() {
-		getPosition().getWorldMapInstance().getDoors().get(17).setOpen(false);
-		getPosition().getWorldMapInstance().getDoors().get(2).setOpen(false);
-		getKnownList().forEachPlayer(new Consumer<Player>() {
+		if (!isDead()) {
+			getKnownList().forEachPlayer(new Consumer<Player>() {
 
-			@Override
-			public void accept(Player player) {
-				if (player.getEffectController().hasAbnormalEffect(19502)) {
-					final Npc npc = (Npc) spawn(799657, player.getX(), player.getY(), player.getZ(), player.getHeading());
-					player.getEffectController().removeEffect(19502);
-					ThreadPoolManager.getInstance().schedule(new Runnable() {
-
-						@Override
-						public void run() {
-							if (npc != null && !npc.isDead()) {
-								npc.getController().delete();
-							}
-						}
-
-					}, 4000);
+				@Override
+				public void accept(Player player) {
+					if (player.getEffectController().hasAbnormalEffect(19502)) {
+						Npc npc = (Npc) spawn(799657, player.getX(), player.getY(), player.getZ(), player.getHeading());
+						PacketSendUtility.sendPacket(player, new SM_EMOTION(npc, EmotionType.DIE));
+						player.getEffectController().removeEffect(19502);
+					}
 				}
-			}
-
-		});
-		AIActions.deleteOwner(this);
+			});
+			AIActions.deleteOwner(this);
+		}
 	}
-
 }
