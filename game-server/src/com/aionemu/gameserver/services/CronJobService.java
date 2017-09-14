84ffcdf8acd3a38002eadd005c87af5a3202b3a0
Estrayl <galawaynkrueger@yahoo.de>
2017-09-14 23:23:58 +0200

    #TB33 Changed spawn behavior of Idian Depths portals to permanent available and shifting locations.

--- game-server/src/com/aionemu/gameserver/services/CronJobService.java
+++ game-server/src/com/aionemu/gameserver/services/CronJobService.java
@@ -70,72 +70,52 @@ public class CronJobService {
 	}
 
 	private void scheduleAhserionsFlight() {
-		CronService.getInstance().schedule(new Runnable() {
-
-			@Override
-			public void run() {
-				AhserionRaid.getInstance().start();
-			}
-		}, "0 50 18 ? * SUN");
+		CronService.getInstance().schedule(() -> AhserionRaid.getInstance().start(), "0 50 18 ? * SUN");
 	}
-	
+
 	private void scheduleIdianDepthPortalSpawns() {
-		CronService.getInstance().schedule(new Runnable() {
-			
-			@Override
-			public void run() {
-				if (asmodianUndergroundEntrance != null || elyosUndergroundEntrance != null)
-					return;
-				
-				SpawnTemplate elyosSpawn;
-				switch (Rnd.get(1, 4)) {
-					case 1: // Levinshor
-						elyosSpawn = SpawnEngine.newSingleTimeSpawn(600100000, 731631, 721.39f, 268.67f, 291.636f, (byte) 60);
-						break;
-					case 2: // Levinshor
-						elyosSpawn = SpawnEngine.newSingleTimeSpawn(600100000, 731631, 332.40f, 1903.37f, 232.000f, (byte) 110);
-						break;
-					case 3: // Kaldor
-						elyosSpawn = SpawnEngine.newSingleTimeSpawn(600090000, 731631, 1179.58f, 687.52f, 190.625f, (byte) 0);
-						break;
-					default: // Cygnea
-						elyosSpawn = SpawnEngine.newSingleTimeSpawn(210070000, 731631, 777.01f, 1479.86f, 457.375f, (byte) 30);
-						break;
-				}
-				SpawnTemplate asmodianSpawn;
-				switch (Rnd.get(1, 4)) {
-					case 1: // Levinshor
-						asmodianSpawn = SpawnEngine.newSingleTimeSpawn(600100000, 731632, 1478.78f, 1844.20f, 225.987f, (byte) 45);
-						break;
-					case 2: // Levinshor
-						asmodianSpawn = SpawnEngine.newSingleTimeSpawn(600100000, 731632, 1870.49f, 41.64f, 244.711f, (byte) 15);
-						break;
-					case 3: // Kaldor
-						asmodianSpawn = SpawnEngine.newSingleTimeSpawn(600090000, 731632, 415.01f, 564.42f, 142.0f, (byte) 100);
-						break;
-					default: // Enshar
-						asmodianSpawn = SpawnEngine.newSingleTimeSpawn(220080000, 731632, 233.39f, 1137.03f, 225.875f, (byte) 105);
-						break;
-				}
-				elyosUndergroundEntrance = (Npc) SpawnEngine.spawnObject(elyosSpawn, 1);
-				asmodianUndergroundEntrance = (Npc) SpawnEngine.spawnObject(asmodianSpawn, 1);
-				// Despawn task
-				ThreadPoolManager.getInstance().schedule(new Runnable() {
-					
-					@Override
-					public void run() {
-						if (asmodianUndergroundEntrance != null) {
-							asmodianUndergroundEntrance.getController().delete();
-							asmodianUndergroundEntrance = null;
-						}
-						if (elyosUndergroundEntrance != null) {
-							elyosUndergroundEntrance.getController().delete();
-							elyosUndergroundEntrance = null;
-						}
-					}
-				}, 3600 * 1000);
+		ThreadPoolManager.getInstance().scheduleAtFixedRate(() -> {
+			if (asmodianUndergroundEntrance != null) {
+				asmodianUndergroundEntrance.getController().delete();
+				asmodianUndergroundEntrance = null;
+			} else if (elyosUndergroundEntrance != null) {
+				elyosUndergroundEntrance.getController().delete();
+				elyosUndergroundEntrance = null;
 			}
-		}, "0 0 15,21 ? * MON,WED,FRI,SUN");
+
+			SpawnTemplate elyosSpawn;
+			switch (Rnd.get(1, 4)) {
+				case 1: // Levinshor
+					elyosSpawn = SpawnEngine.newSingleTimeSpawn(600100000, 731631, 721.39f, 268.67f, 291.636f, (byte) 60);
+					break;
+				case 2: // Levinshor
+					elyosSpawn = SpawnEngine.newSingleTimeSpawn(600100000, 731631, 332.40f, 1903.37f, 232.000f, (byte) 110);
+					break;
+				case 3: // Kaldor
+					elyosSpawn = SpawnEngine.newSingleTimeSpawn(600090000, 731631, 1179.58f, 687.52f, 190.625f, (byte) 0);
+					break;
+				default: // Cygnea
+					elyosSpawn = SpawnEngine.newSingleTimeSpawn(210070000, 731631, 777.01f, 1479.86f, 457.375f, (byte) 30);
+					break;
+			}
+			SpawnTemplate asmodianSpawn;
+			switch (Rnd.get(1, 4)) {
+				case 1: // Levinshor
+					asmodianSpawn = SpawnEngine.newSingleTimeSpawn(600100000, 731632, 1478.78f, 1844.20f, 225.987f, (byte) 45);
+					break;
+				case 2: // Levinshor
+					asmodianSpawn = SpawnEngine.newSingleTimeSpawn(600100000, 731632, 1870.49f, 41.64f, 244.711f, (byte) 15);
+					break;
+				case 3: // Kaldor
+					asmodianSpawn = SpawnEngine.newSingleTimeSpawn(600090000, 731632, 415.01f, 564.42f, 142.0f, (byte) 100);
+					break;
+				default: // Enshar
+					asmodianSpawn = SpawnEngine.newSingleTimeSpawn(220080000, 731632, 233.39f, 1137.03f, 225.875f, (byte) 105);
+					break;
+			}
+			elyosUndergroundEntrance = (Npc) SpawnEngine.spawnObject(elyosSpawn, 1);
+			asmodianUndergroundEntrance = (Npc) SpawnEngine.spawnObject(asmodianSpawn, 1);
+		}, 100000, Rnd.get(3600, 18000) * 1000);
 	}
 
 	private void scheduleLegionDominionCalculation() {
