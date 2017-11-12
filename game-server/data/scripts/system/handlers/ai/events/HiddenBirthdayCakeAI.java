77e63c22d2e4e4820099b234a7189a3873bc27a9
Estrayl <galawaynkrueger@yahoo.de>
2017-11-12 19:00:18 +0100

    Birthday cake ai

---
+++ game-server/data/scripts/system/handlers/ai/events/HiddenBirthdayCakeAI.java
@@ -0,0 +1,100 @@
+package ai.events;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import com.aionemu.commons.utils.Rnd;
+import com.aionemu.gameserver.ai.AIActions;
+import com.aionemu.gameserver.ai.AIName;
+import com.aionemu.gameserver.model.DescriptionId;
+import com.aionemu.gameserver.model.Race;
+import com.aionemu.gameserver.model.gameobjects.VisibleObject;
+import com.aionemu.gameserver.model.gameobjects.player.Player;
+import com.aionemu.gameserver.model.gameobjects.state.CreatureState;
+import com.aionemu.gameserver.network.aion.serverpackets.SM_SYSTEM_MESSAGE;
+import com.aionemu.gameserver.services.drop.DropRegistrationService;
+import com.aionemu.gameserver.services.drop.DropService;
+import com.aionemu.gameserver.utils.ChatUtil;
+import com.aionemu.gameserver.utils.PacketSendUtility;
+
+import ai.ActionItemNpcAI;
+
+/**
+ * Created on 11th November, 2017.<br>
+ * <br>
+ * TODO: Remove drop registration if global drops or something like that will ever
+ * be able to support variable quantities.
+ * 
+ * @author Estrayl
+ */
+@AIName("hidden_cake")
+public class HiddenBirthdayCakeAI extends ActionItemNpcAI {
+
+	private final static int SNUFFLER_SPAWN_CHANCE = 5;
+
+	@Override
+	protected void handleUseItemFinish(Player player) {
+		if (getOwner().isInState(CreatureState.DEAD))
+			return;
+
+		if (Rnd.chance() < SNUFFLER_SPAWN_CHANCE) {
+			VisibleObject s = spawn(210341, getOwner().getX(), getOwner().getY(), getOwner().getZ(), (byte) 0);
+			PacketSendUtility.sendPacket(player,
+				new SM_SYSTEM_MESSAGE(1400618, ChatUtil.nameId(s.getObjectTemplate().getNameId() * 2 + 1), new DescriptionId(1591869)));
+			AIActions.die(this);
+			return;
+		}
+		analyzeOpening(player);
+	}
+
+	private void analyzeOpening(Player player) {
+		int droppedItem = getDroppedItemId(player.getRace());
+
+		DropRegistrationService.getInstance().registerDrop(getOwner(), player, Arrays.asList(player));
+		DropRegistrationService.getInstance().getCurrentDropMap().get(getObjectId())
+			.add(DropRegistrationService.getInstance().regDropItem(1, player.getObjectId(), getObjectId(), droppedItem, getItemCount(droppedItem)));
+		AIActions.die(this, player);
+		DropService.getInstance().requestDropList(player, getObjectId());
+		super.handleUseItemFinish(player);
+	}
+
+	private int getDroppedItemId(Race playerRace) {
+		List<Integer> items = new ArrayList<>();
+		int rnd = Rnd.get(1000);
+		if (rnd < 65) {
+			items.addAll(Arrays.asList(169610093, 169610140, 168310019, 187060161, 169600005, 188054238, 188053007));
+		} else if (rnd < 315) {
+			items.addAll(
+				Arrays.asList(188052638, 110900673, 110900765, 110900781, 190100077, 190100095, 190100083, 190070016, 190070018, 188053006, 188053618));
+		} else {
+			items.addAll(
+				Arrays.asList(162000137, 162000139, 162000141, 186000399, 160010217, 186000236, 188053295, 166500002, 166020000, 166030007, 186000242));
+			items.add(playerRace == Race.ELYOS ? 184002016 : 184002017);
+		}
+		Collections.shuffle(items);
+		return Rnd.get(items);
+	}
+
+	private int getItemCount(int itemId) {
+		switch (itemId) {
+			case 162000141: // Sublime Wind Serum
+			case 166030007: // [Event] Tempering Solution
+			case 166500002: // Amplification Stone
+			case 186000242: // Ceramium Medal
+				return 3;
+			case 160010217: // [Event] Birthday Cake Piece
+			case 166020000: // Omega Enchantment Stone
+			case 162000137: // Sublime Life Serum
+			case 162000139: // Sublime Mana Serum
+				return 5;
+			case 186000399: // Honorable Conqueror's Mark
+				return 15;
+			case 186000236: // Blood Mark
+				return 30;
+			default:
+				return 1;
+		}
+	}
+}
