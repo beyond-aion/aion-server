e2a60e5f1ff384abafc8bf3c78b2ecf22a956a8f
Neon <neon@beyond-aion.com>
2018-01-13 17:20:40 +0100

    let the unmarshaller do its magic

--- game-server/src/com/aionemu/gameserver/model/templates/event/EventQuestList.java
+++ game-server/src/com/aionemu/gameserver/model/templates/event/EventQuestList.java
@@ -1,66 +1,35 @@
 package com.aionemu.gameserver.model.templates.event;
 
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 
-import javax.xml.bind.Unmarshaller;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlTransient;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlList;
 import javax.xml.bind.annotation.XmlType;
 
 /**
  * @author Rolandas
  */
-@XmlType(name = "EventQuestList", propOrder = { "startable", "maintainable" })
+@XmlType(name = "EventQuestList")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class EventQuestList {
 
-	protected String startable;
-
-	protected String maintainable;
-
-	@XmlTransient
+	@XmlElement(name = "startable")
+	@XmlList
 	private List<Integer> startQuests;
 
-	@XmlTransient
+	@XmlElement(name = "maintainable")
+	@XmlList
 	private List<Integer> maintainQuests;
-
-	void afterUnmarshal(Unmarshaller u, Object parent) {
-		if (startable != null) {
-			startQuests = getQuestsFromData(startable);
-		}
-
-		if (maintainable != null) {
-			maintainQuests = getQuestsFromData(maintainable);
-		}
-	}
-
-	List<Integer> getQuestsFromData(String data) {
-		Set<String> q = new HashSet<>();
-		Collections.addAll(q, data.split(";"));
-		List<Integer> result = new ArrayList<>();
-
-		if (q.size() > 0) {
-			result = new ArrayList<>();
-			Iterator<String> it = q.iterator();
-			while (it.hasNext())
-				result.add(Integer.parseInt(it.next()));
-		}
-
-		return result;
-	}
 
 	/**
 	 * @return the startQuests (automatically started on logon)
 	 */
 	public List<Integer> getStartableQuests() {
 		if (startQuests == null)
-			startQuests = new ArrayList<>();
+			return Collections.emptyList();
 		return startQuests;
 	}
 
@@ -69,7 +38,7 @@ public class EventQuestList {
 	 */
 	public List<Integer> getMaintainQuests() {
 		if (maintainQuests == null)
-			maintainQuests = new ArrayList<>();
+			return Collections.emptyList();
 		return maintainQuests;
 	}
 
