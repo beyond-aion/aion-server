4df84ddcd010ea7eb039e2f01e97eabb9ac5f332
Neon <neon@beyond-aion.com>
2018-03-11 00:51:30 +0100

    try to restart the server on OOM (most likely won't succeed but hey, at least we tried)

--- commons/src/com/aionemu/commons/utils/concurrent/UncaughtExceptionHandler.java
+++ commons/src/com/aionemu/commons/utils/concurrent/UncaughtExceptionHandler.java
@@ -3,22 +3,21 @@ package com.aionemu.commons.utils.concurrent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.aionemu.commons.utils.ExitCode;
+
 /**
  * @author -Nemesiss-
  */
 public class UncaughtExceptionHandler implements Thread.UncaughtExceptionHandler {
 
-	/**
-	 * Logger for this class.
-	 */
 	private static final Logger log = LoggerFactory.getLogger(UncaughtExceptionHandler.class);
 
 	@Override
-	public void uncaughtException(final Thread t, final Throwable e) {
+	public void uncaughtException(Thread t, Throwable e) {
 		log.error("Critical Error - Thread [" + t.getName() + "] terminated abnormally:", e);
 		if (e instanceof OutOfMemoryError) {
-			// TODO try get some memory or restart
+			log.error("Trying to exit gracefully with ExitCode.RESTART..."); // we shouldn't even try to regain memory at this point
+			System.exit(ExitCode.CODE_RESTART);
 		}
-		// TODO! some threads should be "restarted" on error
 	}
 }
