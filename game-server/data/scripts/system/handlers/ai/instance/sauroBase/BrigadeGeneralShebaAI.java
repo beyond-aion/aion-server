9f76255d85009374dbc1beb850448714b2101887
Estrayl <galawaynkrueger@yahoo.de>
2018-03-22 03:29:51 +0100

    Fixed issues with Brigade General Sheba add spawns

--- game-server/data/scripts/system/handlers/ai/instance/sauroBase/BrigadeGeneralShebaAI.java
+++ game-server/data/scripts/system/handlers/ai/instance/sauroBase/BrigadeGeneralShebaAI.java
@@ -1,74 +1,31 @@
 package ai.instance.sauroBase;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import com.aionemu.gameserver.ai.AIName;
-import com.aionemu.gameserver.model.gameobjects.Creature;
 import com.aionemu.gameserver.model.gameobjects.Npc;
 import com.aionemu.gameserver.model.skill.QueuedNpcSkillEntry;
+import com.aionemu.gameserver.model.templates.ai.Percentage;
 import com.aionemu.gameserver.model.templates.npcskill.QueuedNpcSkillTemplate;
 import com.aionemu.gameserver.utils.ThreadPoolManager;
 
-import ai.AggressiveNpcAI;
+import ai.SummonerAI;
 
 /**
- * @author Estrayl
+ * @author Estrayl, modified March 22nd, 2018
  */
 @AIName("brigade_general_sheba")
-public class BrigadeGeneralShebaAI extends AggressiveNpcAI {
-
-	private List<Integer> percents = new ArrayList<>(Arrays.asList(25, 10));
+public class BrigadeGeneralShebaAI extends SummonerAI {
 
 	public BrigadeGeneralShebaAI(Npc owner) {
 		super(owner);
 	}
 
 	@Override
-	protected void handleAttack(Creature creature) {
-		super.handleAttack(creature);
-		checkPercentage(getLifeStats().getHpPercentage());
-	}
-
-	private synchronized void checkPercentage(int hpPercentage) {
-		for (Integer percent : percents) {
-			if (hpPercentage <= percent) {
-				switch (percent) {
-					case 25:
-						spawnSpirits(true);
-						break;
-					case 10:
-						spawnSpirits(false);
-						break;
-				}
-				percents.remove(percent);
-				break;
-			}
+	protected void handleBeforeSpawn(Percentage percent) {
+		if (percent.getPercent() == 25 || percent.getPercent() == 10) {
+			ThreadPoolManager.getInstance().schedule(() -> {
+				if (!isDead())
+					getOwner().getQueuedSkills().offer(new QueuedNpcSkillEntry(new QueuedNpcSkillTemplate(21186, 1, 100)));
+			}, 11000);
 		}
-	}
-
-	private void spawnSpirits(boolean isMid) {
-		if (isMid) {
-			spawn(284436, 900.12497f, 889.17401f, 412.1f, (byte) 0);
-		} else {
-			spawn(284436, 913.12497f, 876.17401f, 412.1f, (byte) 45);
-			spawn(284436, 900.12497f, 870.17401f, 412.1f, (byte) 30);
-			spawn(284436, 886.12497f, 876.17401f, 412.1f, (byte) 16);
-			spawn(284436, 881.12497f, 889.17401f, 412.1f, (byte) 0);
-			spawn(284436, 899.12497f, 909.17401f, 412.1f, (byte) 90);
-			spawn(284436, 913.12497f, 902.17401f, 412.1f, (byte) 78);
-			spawn(284436, 918.12497f, 890.17401f, 412.1f, (byte) 61);
-		}
-		ThreadPoolManager.getInstance().schedule(() -> {
-			if (!isDead())
-				getOwner().getQueuedSkills().offer(new QueuedNpcSkillEntry(new QueuedNpcSkillTemplate(21186, 1, 100)));
-		}, 7000);
-	}
-
-	@Override
-	protected void handleBackHome() {
-		super.handleBackHome();
-		percents = new ArrayList<>(Arrays.asList(25, 10));
 	}
 }
