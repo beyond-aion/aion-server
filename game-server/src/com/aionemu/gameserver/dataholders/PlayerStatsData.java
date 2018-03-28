63ecf28572152928127a3b80a2e36f2fb82ffdb3
Neon <neon@beyond-aion.com>
2018-03-28 23:56:04 +0200

    removed a bunch of unneeded playerclass default data which wasn't even up to date, implemented proper fallbacks

--- game-server/src/com/aionemu/gameserver/dataholders/PlayerStatsData.java
+++ game-server/src/com/aionemu/gameserver/dataholders/PlayerStatsData.java
@@ -1,7 +1,11 @@
 package com.aionemu.gameserver.dataholders;
 
-import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.EnumMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import javax.xml.bind.Unmarshaller;
 import javax.xml.bind.annotation.XmlAccessType;
@@ -9,12 +13,13 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
 
+import org.slf4j.LoggerFactory;
+
+import com.aionemu.gameserver.configs.main.GSConfig;
 import com.aionemu.gameserver.model.PlayerClass;
-import com.aionemu.gameserver.model.templates.stats.CalculatedPlayerStatsTemplate;
 import com.aionemu.gameserver.model.templates.stats.PlayerStatsTemplate;
-
-import gnu.trove.map.hash.TIntObjectHashMap;
 
 /**
  * Created on: 31.07.2009 14:20:03
@@ -26,63 +31,75 @@ import gnu.trove.map.hash.TIntObjectHashMap;
 public class PlayerStatsData {
 
 	@XmlElement(name = "player_stats", required = true)
-	private List<PlayerStatsType> templatesList = new ArrayList<>();
+	private List<PlayerStatsType> templates;
 
-	private final TIntObjectHashMap<PlayerStatsTemplate> playerTemplates = new TIntObjectHashMap<>();
+	@XmlTransient
+	private final Map<PlayerClass, Map<Integer, PlayerStatsTemplate>> playerTemplates = new EnumMap<>(PlayerClass.class);
 
 	void afterUnmarshal(Unmarshaller u, Object parent) {
-		for (PlayerStatsType pt : templatesList) {
-			int code = makeHash(pt.getRequiredPlayerClass(), pt.getRequiredLevel());
+		for (PlayerStatsType pt : templates) {
 			PlayerStatsTemplate template = pt.getTemplate();
 			// XXX template values for hp/mp are the effective values from retail, including health and will modifiers so we need to remove these here
 			// ceil since these values get floored in stat calculation, where health and will are applied as the base rate
 			template.setMaxMp((int) Math.ceil(template.getMaxMp() * 100f / template.getWill()));
 			template.setMaxHp((int) Math.ceil(template.getMaxHp() * 100f / template.getHealth()));
-			playerTemplates.put(code, pt.getTemplate());
+			playerTemplates.compute(pt.getRequiredPlayerClass(), (key, map) -> {
+				if (map == null) {
+					map = new LinkedHashMap<>();
+				}
+				if (map.put(pt.getRequiredLevel(), pt.getTemplate()) != null) {
+					throw new Error("Duplicate stat template for " + key + ", level: " + pt.getRequiredLevel());
+				}
+				return map;
+			});
 		}
 
-		/** for unknown templates **/
-		playerTemplates.put(makeHash(PlayerClass.WARRIOR, 0), new CalculatedPlayerStatsTemplate(PlayerClass.WARRIOR));
-		playerTemplates.put(makeHash(PlayerClass.ASSASSIN, 0), new CalculatedPlayerStatsTemplate(PlayerClass.ASSASSIN));
-		playerTemplates.put(makeHash(PlayerClass.CHANTER, 0), new CalculatedPlayerStatsTemplate(PlayerClass.CHANTER));
-		playerTemplates.put(makeHash(PlayerClass.CLERIC, 0), new CalculatedPlayerStatsTemplate(PlayerClass.CLERIC));
-		playerTemplates.put(makeHash(PlayerClass.GLADIATOR, 0), new CalculatedPlayerStatsTemplate(PlayerClass.GLADIATOR));
-		playerTemplates.put(makeHash(PlayerClass.MAGE, 0), new CalculatedPlayerStatsTemplate(PlayerClass.MAGE));
-		playerTemplates.put(makeHash(PlayerClass.PRIEST, 0), new CalculatedPlayerStatsTemplate(PlayerClass.PRIEST));
-		playerTemplates.put(makeHash(PlayerClass.RANGER, 0), new CalculatedPlayerStatsTemplate(PlayerClass.RANGER));
-		playerTemplates.put(makeHash(PlayerClass.SCOUT, 0), new CalculatedPlayerStatsTemplate(PlayerClass.SCOUT));
-		playerTemplates.put(makeHash(PlayerClass.SORCERER, 0), new CalculatedPlayerStatsTemplate(PlayerClass.SORCERER));
-		playerTemplates.put(makeHash(PlayerClass.SPIRIT_MASTER, 0), new CalculatedPlayerStatsTemplate(PlayerClass.SPIRIT_MASTER));
-		playerTemplates.put(makeHash(PlayerClass.TEMPLAR, 0), new CalculatedPlayerStatsTemplate(PlayerClass.TEMPLAR));
-		playerTemplates.put(makeHash(PlayerClass.RIDER, 0), new CalculatedPlayerStatsTemplate(PlayerClass.RIDER));
-		playerTemplates.put(makeHash(PlayerClass.ARTIST, 0), new CalculatedPlayerStatsTemplate(PlayerClass.ARTIST));
-		playerTemplates.put(makeHash(PlayerClass.ENGINEER, 0), new CalculatedPlayerStatsTemplate(PlayerClass.ENGINEER));
-		playerTemplates.put(makeHash(PlayerClass.BARD, 0), new CalculatedPlayerStatsTemplate(PlayerClass.BARD));
-		playerTemplates.put(makeHash(PlayerClass.GUNNER, 0), new CalculatedPlayerStatsTemplate(PlayerClass.GUNNER));
-
-		templatesList.clear();
-		templatesList = null;
+		for (int expectedLevel = 1; expectedLevel <= GSConfig.PLAYER_MAX_LEVEL; expectedLevel++) {
+			for (Map.Entry<PlayerClass, Map<Integer, PlayerStatsTemplate>> e : playerTemplates.entrySet()) {
+				PlayerClass playerClass = e.getKey();
+				if (playerClass.isStartingClass()) {
+					if (expectedLevel > 9)
+						continue;
+				} else if (expectedLevel < 9) {
+					continue;
+				}
+				if (e.getValue().get(expectedLevel) == null)
+					throw new Error("Missing stat template for " + playerClass + " level " + expectedLevel);
+			}
+		}
+		templates = null;
 	}
 
-	/**
-	 * @param playerClass
-	 * @param level
-	 * @return
-	 */
 	public PlayerStatsTemplate getTemplate(PlayerClass playerClass, int level) {
-		PlayerStatsTemplate template = playerTemplates.get(makeHash(playerClass, level));
-		if (template == null)
-			template = getTemplate(playerClass, 0);
+		PlayerStatsTemplate template = playerTemplates.get(playerClass).get(level);
+		if (template == null) {
+			template = getFallbackTemplate(playerClass, level);
+			if (template == null) {
+				LoggerFactory.getLogger(PlayerStatsData.class).error("Missing stats template for " + playerClass + ", level: " + level);
+				template = getNearestTemplateForLevel(playerClass, level);
+			}
+		}
 		return template;
 	}
 
-	/**
-	 * Size of player templates
-	 * 
-	 * @return
-	 */
+	private PlayerStatsTemplate getFallbackTemplate(PlayerClass playerClass, int level) {
+		if (level < 9) // player was set to low level via command, return the corresponding starting class template
+			return playerTemplates.get(playerClass.getStartingClass()).get(level);
+		else if (level > 9 && playerClass.isStartingClass()) // player changed class via command while being high level
+			return playerTemplates.get(playerClass).get(9);
+		else if (level > GSConfig.PLAYER_MAX_LEVEL)
+			return playerTemplates.get(playerClass).get(GSConfig.PLAYER_MAX_LEVEL);
+		return null;
+	}
+
+	private PlayerStatsTemplate getNearestTemplateForLevel(PlayerClass playerClass, int level) {
+		Set<Map.Entry<Integer, PlayerStatsTemplate>> templates = playerTemplates.get(playerClass).entrySet();
+		// here we search all templates for this playerClass and return the one nearest to the players level
+		return templates.stream().min(Comparator.comparing(e -> Math.abs(level - e.getKey()))).map(Map.Entry::getValue).get();
+	}
+
 	public int size() {
-		return playerTemplates.size();
+		return playerTemplates.values().stream().mapToInt(Map::size).sum();
 	}
 
 	@XmlRootElement(name = "playerStatsTemplateType")
@@ -107,14 +124,5 @@ public class PlayerStatsData {
 		public PlayerStatsTemplate getTemplate() {
 			return template;
 		}
-	}
-
-	/**
-	 * @param playerClass
-	 * @param level
-	 * @return
-	 */
-	private static int makeHash(PlayerClass playerClass, int level) {
-		return level << 8 | playerClass.ordinal();
 	}
 }
