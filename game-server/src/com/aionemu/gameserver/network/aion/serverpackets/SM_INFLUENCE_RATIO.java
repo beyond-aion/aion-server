98e2f4f6ec95013948827dfab2d6ddc2eacaf4d9
Neon <neon@beyond-aion.com>
2018-04-01 17:36:39 +0200

    removed duplicate code, made influence system more dynamic

--- game-server/src/com/aionemu/gameserver/network/aion/serverpackets/SM_INFLUENCE_RATIO.java
+++ game-server/src/com/aionemu/gameserver/network/aion/serverpackets/SM_INFLUENCE_RATIO.java
@@ -1,6 +1,7 @@
 package com.aionemu.gameserver.network.aion.serverpackets;
 
 import com.aionemu.gameserver.model.siege.Influence;
+import com.aionemu.gameserver.model.siege.SiegeRace;
 import com.aionemu.gameserver.network.aion.AionConnection;
 import com.aionemu.gameserver.network.aion.AionServerPacket;
 import com.aionemu.gameserver.services.SiegeService;
@@ -15,26 +16,15 @@ public class SM_INFLUENCE_RATIO extends AionServerPacket {
 		Influence inf = Influence.getInstance();
 
 		writeD(SiegeService.getInstance().getSecondsBeforeHourEnd());
-		writeF(inf.getGlobalElyosInfluence());
-		writeF(inf.getGlobalAsmodiansInfluence());
-		writeF(inf.getGlobalBalaursInfluence());
-		writeH(4); // maps count
-		writeD(210050000);
-		writeF(inf.getInggisonElyosInfluence());
-		writeF(inf.getInggisonAsmodiansInfluence());
-		writeF(inf.getInggisonBalaursInfluence());
-		writeD(220070000);
-		writeF(inf.getGelkmarosElyosInfluence());
-		writeF(inf.getGelkmarosAsmodiansInfluence());
-		writeF(inf.getGelkmarosBalaursInfluence());
-		writeD(400010000);
-		writeF(inf.getAbyssElyosInfluence());
-		writeF(inf.getAbyssAsmodiansInfluence());
-		writeF(inf.getAbyssBalaursInfluence());
-		writeD(600090000);
-		writeF(inf.getKaldorElyosInfluence());
-		writeF(inf.getKaldorAsmodiansInfluence());
-		writeF(inf.getKaldorBalaursInfluence());
+		writeF(inf.getElyosInfluenceRate());
+		writeF(inf.getAsmodianInfluenceRate());
+		writeF(inf.getBalaurInfluenceRate());
+		writeH(inf.getInfluenceRelevantWorldIds().size());
+		for (int worldId : inf.getInfluenceRelevantWorldIds()) {
+			writeD(worldId);
+			writeF(inf.getInfluence(worldId, SiegeRace.ELYOS));
+			writeF(inf.getInfluence(worldId, SiegeRace.ASMODIANS));
+			writeF(inf.getInfluence(worldId, SiegeRace.BALAUR));
+		}
 	}
-
 }
