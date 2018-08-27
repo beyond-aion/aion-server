8482cd57befa809e52d94284eebb1e577c20d30a
Estrayl <galawaynkrueger@yahoo.de>
2018-08-27 20:30:18 +0200

    Implemented teleportation to kisk/obelisk if player dies while passing through aether shields (decided during support meeting on 27.08.2018). Additionally fixes aether shields killing if their generator is already destroyed and fortresses are not under siege. To activate set gameserver.geo.shields.enable = true

--- game-server/src/com/aionemu/gameserver/controllers/observer/CollisionDieActor.java
+++ game-server/src/com/aionemu/gameserver/controllers/observer/CollisionDieActor.java
@@ -5,9 +5,16 @@ import com.aionemu.gameserver.geoEngine.collision.CollisionIntention;
 import com.aionemu.gameserver.geoEngine.collision.CollisionResult;
 import com.aionemu.gameserver.geoEngine.collision.CollisionResults;
 import com.aionemu.gameserver.geoEngine.scene.Spatial;
+import com.aionemu.gameserver.model.EmotionType;
 import com.aionemu.gameserver.model.gameobjects.Creature;
+import com.aionemu.gameserver.model.gameobjects.Kisk;
 import com.aionemu.gameserver.model.gameobjects.player.Player;
+import com.aionemu.gameserver.model.siege.FortressLocation;
+import com.aionemu.gameserver.network.aion.serverpackets.SM_EMOTION;
+import com.aionemu.gameserver.services.SiegeService;
+import com.aionemu.gameserver.services.player.PlayerReviveService;
 import com.aionemu.gameserver.utils.PacketSendUtility;
+import com.aionemu.gameserver.utils.ThreadPoolManager;
 
 /**
  * @author Rolandas
@@ -28,13 +35,28 @@ public class CollisionDieActor extends AbstractCollisionObserver implements IAct
 					PacketSendUtility.sendMessage(player, "Entered " + result.getGeometry().getName());
 				}
 			}
-			act();
+			FortressLocation fortress = SiegeService.getInstance().findFortress(creature.getWorldId(), creature.getX(), creature.getY(), creature.getZ());
+			if (fortress != null && fortress.isUnderShield())
+				act();
 		}
 	}
 
 	@Override
 	public void act() {
+		if (creature.isInvulnerable())
+			return;
 		creature.getController().die();
+		creature.getController().onStopMove();
+		ThreadPoolManager.getInstance().schedule(() -> {
+			if (creature instanceof Player) {
+				Kisk kisk = ((Player) creature).getKisk();
+				if (kisk != null && kisk.isActive())
+					PlayerReviveService.kiskRevive((Player) creature);
+				else
+					PlayerReviveService.bindRevive((Player) creature);
+				PacketSendUtility.sendPacket((Player) creature, new SM_EMOTION(creature, EmotionType.RESURRECT)); // send to remove res option window
+			}
+		}, 2850);
 	}
 
 	@Override
