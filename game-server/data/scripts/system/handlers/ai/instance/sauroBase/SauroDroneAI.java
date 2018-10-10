4a09ee72fd300281b8ee25a5078b7588aa245a8a
Estrayl <galawaynkrueger@yahoo.de>
2018-10-10 23:04:36 +0200

    Fixed #BA1792 not despawning drone https://beyond-aion.com/forum/thread/1792-%C3%BCberwachungsger%C3%A4t-des-kriegsdepots-230843/

---
+++ game-server/data/scripts/system/handlers/ai/instance/sauroBase/SauroDroneAI.java
@@ -0,0 +1,24 @@
+package ai.instance.sauroBase;
+
+import com.aionemu.gameserver.ai.AIName;
+import com.aionemu.gameserver.model.gameobjects.Npc;
+import com.aionemu.gameserver.skillengine.model.SkillTemplate;
+
+import ai.AggressiveNpcAI;
+
+/**
+ * @author Estrayl
+ */
+@AIName("sauro_drone")
+public class SauroDroneAI extends AggressiveNpcAI {
+
+	public SauroDroneAI(Npc owner) {
+		super(owner);
+	}
+
+	@Override
+	public void onEndUseSkill(SkillTemplate skillTemplate) {
+		if (skillTemplate.getSkillId() == 19498)
+			getOwner().getController().delete();
+	}
+}
