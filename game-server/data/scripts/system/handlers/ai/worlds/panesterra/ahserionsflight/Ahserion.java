f67d318dceada0f16b5033e37be55b7a4b4f32f7
Estrayl <galawaynkrueger@yahoo.de>
2018-11-25 22:02:38 +0100

    Improved skill pattern of Ahserion and moved special events based on hp percentage to AI

--- game-server/data/scripts/system/handlers/ai/worlds/panesterra/ahserionsflight/Ahserion.java
+++ game-server/data/scripts/system/handlers/ai/worlds/panesterra/ahserionsflight/Ahserion.java
@@ -1,12 +1,19 @@
 package ai.worlds.panesterra.ahserionsflight;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.aionemu.gameserver.ai.AIName;
+import com.aionemu.gameserver.ai.poll.AIQuestion;
 import com.aionemu.gameserver.controllers.attack.AggroInfo;
+import com.aionemu.gameserver.model.gameobjects.Creature;
 import com.aionemu.gameserver.model.gameobjects.Npc;
 import com.aionemu.gameserver.model.gameobjects.player.Player;
+import com.aionemu.gameserver.model.skill.QueuedNpcSkillEntry;
+import com.aionemu.gameserver.model.templates.npcskill.QueuedNpcSkillTemplate;
 import com.aionemu.gameserver.services.panesterra.ahserion.AhserionRaid;
 import com.aionemu.gameserver.services.panesterra.ahserion.PanesterraFaction;
 import com.aionemu.gameserver.services.panesterra.ahserion.PanesterraTeam;
@@ -14,14 +21,44 @@ import com.aionemu.gameserver.services.panesterra.ahserion.PanesterraTeam;
 import ai.AggressiveNpcAI;
 
 /**
- * @author Yeats
- * @modified Estrayl October 29th, 2017.
+ * @author Yeats, Estrayl
  */
 @AIName("ahserion")
 public class Ahserion extends AggressiveNpcAI {
 
+	private final List<Integer> hpEvents = new ArrayList<>();
+
 	public Ahserion(Npc owner) {
 		super(owner);
+	}
+
+	@Override
+	protected void handleSpawned() {
+		super.handleSpawned();
+		initHpEvents();
+	}
+
+	@Override
+	protected void handleAttack(Creature creature) {
+		super.handleAttack(creature);
+		checkPercentage(getLifeStats().getHpPercentage());
+	}
+
+	private synchronized void checkPercentage(int hpPercentage) {
+		for (Integer hpEvent : hpEvents) {
+			if (hpPercentage <= hpEvent) {
+				hpEvents.remove(hpEvent);
+				switch (hpEvent) {
+					case 75:
+					case 50:
+					case 25:
+					case 10:
+						getOwner().getQueuedSkills().offer(new QueuedNpcSkillEntry(new QueuedNpcSkillTemplate(21571, 1, 100, 0, 3000)));
+						break;
+				}
+				break;
+			}
+		}
 	}
 
 	@Override
@@ -63,5 +100,26 @@ public class Ahserion extends AggressiveNpcAI {
 			}
 		}
 		return winner;
+	}
+
+	private void initHpEvents() {
+		hpEvents.clear();
+		Collections.addAll(hpEvents, new Integer[] { 75, 50, 25, 10 });
+	}
+
+	@Override
+	protected void handleBackHome() {
+		super.handleBackHome();
+		initHpEvents();
+	}
+
+	@Override
+	public boolean ask(AIQuestion question) {
+		switch (question) {
+			case CAN_RESIST_ABNORMAL:
+				return true;
+			default:
+				return super.ask(question);
+		}
 	}
 }
