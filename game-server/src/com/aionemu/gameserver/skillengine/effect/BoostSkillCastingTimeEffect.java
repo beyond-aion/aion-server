bafc0516db3233027e67be4b30bd30a13885ff6d
Estrayl <galawaynkrueger@yahoo.de>
2018-11-25 22:43:35 +0100

    Fixed an issue, where hero and legendary NPCs could be effected with cast speed slow

--- game-server/src/com/aionemu/gameserver/skillengine/effect/BoostSkillCastingTimeEffect.java
+++ game-server/src/com/aionemu/gameserver/skillengine/effect/BoostSkillCastingTimeEffect.java
@@ -5,6 +5,7 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlType;
 
 import com.aionemu.gameserver.model.stats.container.StatEnum;
+import com.aionemu.gameserver.skillengine.change.Change;
 import com.aionemu.gameserver.skillengine.model.Effect;
 
 /**
@@ -16,12 +17,17 @@ public class BoostSkillCastingTimeEffect extends BufEffect {
 
 	@Override
 	public void calculate(Effect effect) {
-		if (effect.getEffected().isEnemy(effect.getEffector()) && value < 0) {
-			super.calculate(effect, StatEnum.SLOW_RESISTANCE, null);
-		} else {
-			super.calculate(effect);
+		if (effect.getEffected().isEnemy(effect.getEffector())) {
+			if (change != null) {
+				for (Change c : change) {
+					if (c.getValue() < 0) {
+						super.calculate(effect, StatEnum.SLOW_RESISTANCE, null);
+						return;
+					}
+				}
+			}
 		}
-
+		super.calculate(effect);
 	}
 
 }
