dd16d7ee73dc9e8c5e82589ec7476b2f77f02fd0
Estrayl <galawaynkrueger@yahoo.de>
2018-11-25 02:42:51 +0100

    Fixed an issue, where static aetheric field generators could reset if attacked on long-range

--- game-server/data/scripts/system/handlers/ai/siege/ShieldNpcAI.java
+++ game-server/data/scripts/system/handlers/ai/siege/ShieldNpcAI.java
@@ -1,10 +1,8 @@
 package ai.siege;
 
-import java.util.function.Consumer;
-
 import com.aionemu.gameserver.ai.AIName;
+import com.aionemu.gameserver.model.gameobjects.Creature;
 import com.aionemu.gameserver.model.gameobjects.Npc;
-import com.aionemu.gameserver.model.gameobjects.player.Player;
 import com.aionemu.gameserver.network.aion.serverpackets.SM_SHIELD_EFFECT;
 import com.aionemu.gameserver.services.SiegeService;
 import com.aionemu.gameserver.utils.PacketSendUtility;
@@ -30,20 +28,21 @@ public class ShieldNpcAI extends SiegeNpcAI {
 		sendShieldPacket(true);
 		super.handleSpawned();
 	}
+	
+	@Override
+	protected void handleAttack(Creature creature) {
+		switch (getOwner().getRace()) {
+			case CONSTRUCT:
+				getOwner().getController().loseAggro(false);
+		}
+	}
 
 	private void sendShieldPacket(boolean shieldStatus) {
 		int id = getSpawnTemplate().getSiegeId();
 		SiegeService.getInstance().getFortress(id).setUnderShield(shieldStatus);
 
 		final SM_SHIELD_EFFECT packet = new SM_SHIELD_EFFECT(id);
-		getPosition().getWorldMapInstance().forEachPlayer(new Consumer<Player>() {
-
-			@Override
-			public void accept(Player player) {
-				PacketSendUtility.sendPacket(player, packet);
-			}
-
-		});
+		getPosition().getWorldMapInstance().forEachPlayer(p -> PacketSendUtility.sendPacket(p, packet));
 	}
 
 }
