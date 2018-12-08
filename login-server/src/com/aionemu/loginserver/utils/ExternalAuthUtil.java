c646d06884df9112fc345a217979a56e34b14c2a
Neon <neon@beyond-aion.com>
2018-12-08 23:04:35 +0100

    removed synchronization for external auth and simplified code. may reduce occasional login delays

--- login-server/src/com/aionemu/loginserver/utils/ExternalAuthUtil.java
+++ login-server/src/com/aionemu/loginserver/utils/ExternalAuthUtil.java
@@ -1,13 +1,16 @@
 package com.aionemu.loginserver.utils;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.Reader;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.util.IllegalFormatException;
 
 import org.slf4j.Logger;
@@ -15,92 +18,62 @@ import org.slf4j.LoggerFactory;
 
 import com.aionemu.loginserver.configs.Config;
 import com.aionemu.loginserver.model.ExternalAuth;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParseException;
-import com.google.gson.JsonParser;
+import com.google.gson.Gson;
 
 /**
  * @author Woge, Neon
  */
 public class ExternalAuthUtil {
 
-	/**
-	 * Logger for this class.
-	 */
 	private static final Logger log = LoggerFactory.getLogger(ExternalAuthUtil.class);
+	private static final URL url;
+	private static final Gson gson = new Gson();
+
+	static {
+		try {
+			url = new URL(Config.AUTH_EXTERNAL_JSON_URL);
+		} catch (MalformedURLException e) {
+			throw new ExceptionInInitializerError(e);
+		}
+	}
 
 	/**
 	 * Returns an ExternalAuth object holding the information how and if the user authenticated.
 	 * 
 	 * @param name
 	 * @param password
-	 * @return ExternalAuth object or null
+	 * @return ExternalAuth object or null on connection error
 	 */
-	public static synchronized ExternalAuth requestInfo(String name, String password) {
-
+	public static ExternalAuth requestInfo(String name, String password) {
 		ExternalAuth info = null;
-		String charset = StandardCharsets.UTF_8.name();
-		String query = "";
-		BufferedReader reader = null;
-		JsonParser parser = new JsonParser();
-
 		try {
-			query = String.format("u=%s&p=%s", URLEncoder.encode(name, charset), URLEncoder.encode(password, charset));
+			String query = String.format("u=%s&p=%s", URLEncoder.encode(name, UTF_8.name()), URLEncoder.encode(password, UTF_8.name())); // strange query format due to php endpoint
+			byte[] payload = query.getBytes(UTF_8);
 
-			HttpURLConnection connection = (HttpURLConnection) new URL(Config.AUTH_EXTERNAL_JSON_URL).openConnection();
-			connection.setRequestMethod("POST");
-			connection.setDoInput(true);
-			connection.setDoOutput(true);
-			connection.setUseCaches(false);
-			connection.setFixedLengthStreamingMode(query.getBytes(charset).length);
-			connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded;charset=" + charset);
-			connection.setRequestProperty("User-Agent", "AionLS");
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("POST");
+			con.setDoOutput(true);
+			con.setUseCaches(false);
+			con.setFixedLengthStreamingMode(payload.length);
+			con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded;charset=" + UTF_8);
+			con.setRequestProperty("User-Agent", "AionLS");
 
 			// send query to auth server
-			connection.getOutputStream().write(query.getBytes(charset));
-			connection.getOutputStream().close();
+			try (OutputStream os = con.getOutputStream()) {
+				os.write(payload);
+			}
 
-			int responseCode = connection.getResponseCode();
-			if (responseCode == HttpURLConnection.HTTP_OK) {
-				reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-
-				// read response from auth server
-				String result = "";
-				for (String line; (line = reader.readLine()) != null;) {
-					result += line;
-				}
-				reader.close();
-				connection.getInputStream().close();
-
-				if (result.length() > 0) {
-					info = new ExternalAuth();
-					JsonObject answer = (JsonObject) parser.parse(result);
-
-					if (answer.has("id")) {
-						String id = answer.get("id").toString();
-						info.setIdentifier(id);
-					}
-
-					if (answer.has("state")) {
-						try {
-							int state = Integer.parseInt(answer.get("state").toString());
-							info.setAuthState(state);
-						} catch (NumberFormatException e) {
-							log.error(ExternalAuthUtil.class.getSimpleName() + ": error parsing auth state - " + e.getMessage());
-						}
-					} else {
-						log.error(ExternalAuthUtil.class.getSimpleName() + ": no auth state received from " + Config.AUTH_EXTERNAL_JSON_URL);
-					}
-				} else {
-					log.error(ExternalAuthUtil.class.getSimpleName() + ": empty response from " + Config.AUTH_EXTERNAL_JSON_URL);
+			if (con.getResponseCode() == HttpURLConnection.HTTP_OK) {
+				try (Reader reader = new BufferedReader(new InputStreamReader(con.getInputStream(), UTF_8))) {
+					info = gson.fromJson(reader, ExternalAuth.class);
 				}
 			} else {
-				log.error(ExternalAuthUtil.class.getSimpleName() + ": " + Config.AUTH_EXTERNAL_JSON_URL + " returned response code " + responseCode);
+				log.error(url + " returned response code " + con.getResponseCode());
 			}
 		} catch (UnsupportedEncodingException | IllegalFormatException e) {
-			log.error(ExternalAuthUtil.class.getSimpleName() + ": error generating query string - " + e.getMessage());
-		} catch (IOException | JsonParseException | ClassCastException e) {
-			log.error(ExternalAuthUtil.class.getSimpleName() + ": " + e.toString());
+			log.error("Error generating query string for user: " + name + ", pw: " + password.replace('.', '*'), e);
+		} catch (Exception e) {
+			log.error(null, e);
 		}
 
 		return info;
