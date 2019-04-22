7f09968527fc800e8a1d8408b2fffc5590249b7d
Estrayl <galawaynkrueger@yahoo.de>
2019-04-22 08:12:17 +0200

    Fixed #BA1961 missing spawns and shitty AI https://beyond-aion.com/forum/thread/1961-lost-rentus-base-verlorene-rentus-basis/

--- game-server/data/scripts/system/handlers/ai/instance/rentusBase/DancingFlameAI.java
+++ game-server/data/scripts/system/handlers/ai/instance/rentusBase/DancingFlameAI.java
@@ -2,111 +2,64 @@ package ai.instance.rentusBase;
 
 import java.util.concurrent.Future;
 
-import com.aionemu.gameserver.ai.AIActions;
 import com.aionemu.gameserver.ai.AIName;
 import com.aionemu.gameserver.ai.poll.AIQuestion;
+import com.aionemu.gameserver.model.gameobjects.Creature;
 import com.aionemu.gameserver.model.gameobjects.Npc;
 import com.aionemu.gameserver.model.gameobjects.player.Player;
 import com.aionemu.gameserver.skillengine.SkillEngine;
+import com.aionemu.gameserver.skillengine.model.Effect;
 import com.aionemu.gameserver.utils.ThreadPoolManager;
-import com.aionemu.gameserver.world.WorldPosition;
 
 import ai.GeneralNpcAI;
 
 /**
  * @author xTz
+ * @modified Estrayl
  */
 @AIName("dancing_flame")
 public class DancingFlameAI extends GeneralNpcAI {
 
-	private Future<?> task;
+	private Future<?> buffTask;
 
 	public DancingFlameAI(Npc owner) {
 		super(owner);
 	}
 
-	private void startTask() {
-		task = ThreadPoolManager.getInstance().scheduleAtFixedRate(new Runnable() {
+	@Override
+	public int modifyDamage(Creature attacker, int damage, Effect effect) {
+		return 0;
+	}
+
+	private void startBuffTask() {
+		buffTask = ThreadPoolManager.getInstance().scheduleAtFixedRate(new Runnable() {
 
 			@Override
 			public void run() {
-				if (isDead()) {
-					cancelTask();
-				} else {
-					if (isPlayerInRange()) {
-						WorldPosition p = getPosition();
-						if (getNpcId() == 282996) {
-							spawn(282998, p.getX(), p.getY(), p.getZ(), p.getHeading());
-						} else {
-							spawn(282999, p.getX(), p.getY(), p.getZ(), p.getHeading());
-						}
-					}
-				}
+				if (isAnyPlayerInRange())
+					SkillEngine.getInstance().getSkill(getOwner(), getNpcId() == 282998 ? 20536 : 20535, 60, getOwner()).useNoAnimationSkill();
 			}
 
-		}, 3000, 3000);
+		}, 10000, 9000);
 	}
 
-	private boolean isPlayerInRange() {
-		for (Player player : getKnownList().getKnownPlayers().values()) {
-			if (isInRange(player, 30)) {
+	private boolean isAnyPlayerInRange() {
+		for (Player player : getKnownList().getKnownPlayers().values())
+			if (isInRange(player, 10))
 				return true;
-			}
-		}
 		return false;
-	}
-
-	private void cancelTask() {
-		if (task != null && !task.isDone()) {
-			task.cancel(true);
-		}
 	}
 
 	@Override
 	protected void handleSpawned() {
 		super.handleSpawned();
-		if (getNpcId() == 282996 || getNpcId() == 282997) {
-			startTask();
-		} else {
-			ThreadPoolManager.getInstance().schedule(new Runnable() {
-
-				@Override
-				public void run() {
-					SkillEngine.getInstance().getSkill(getOwner(), getNpcId() == 282998 ? 20536 : 20535, 60, getOwner()).useNoAnimationSkill();
-				}
-
-			}, 500);
-			starLifeTask();
-		}
-	}
-
-	private void starLifeTask() {
-		ThreadPoolManager.getInstance().schedule(new Runnable() {
-
-			@Override
-			public void run() {
-				despawn();
-			}
-
-		}, 4000);
-	}
-
-	private void despawn() {
-		if (!isDead()) {
-			AIActions.deleteOwner(this);
-		}
+		startBuffTask();
 	}
 
 	@Override
 	protected void handleDespawned() {
+		buffTask.cancel(true);
 		super.handleDespawned();
-		cancelTask();
-	}
-
-	@Override
-	protected void handleDied() {
-		super.handleDied();
-		cancelTask();
 	}
 
 	@Override
@@ -114,6 +67,10 @@ public class DancingFlameAI extends GeneralNpcAI {
 		switch (question) {
 			case CAN_ATTACK_PLAYER:
 				return true;
+			case SHOULD_RESPAWN:
+			case SHOULD_LOOT:
+			case SHOULD_DECAY:
+				return false;
 			default:
 				return super.ask(question);
 		}
