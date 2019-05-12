6d15f1dd1462867e33be4a6539fcf9e904e339df
Neon <neon@beyond-aion.com>
2019-05-12 17:00:07 +0200

    fix npe on postman despawn and removed hard player reference. still unknown how owner can be null in handleSpawned() sometimes, need to investigate further

--- game-server/data/scripts/system/handlers/ai/DeliveryManAI.java
+++ game-server/data/scripts/system/handlers/ai/DeliveryManAI.java
@@ -9,6 +9,7 @@ import com.aionemu.gameserver.model.gameobjects.player.Player;
 import com.aionemu.gameserver.network.aion.serverpackets.SM_DIALOG_WINDOW;
 import com.aionemu.gameserver.utils.PacketSendUtility;
 import com.aionemu.gameserver.utils.ThreadPoolManager;
+import com.aionemu.gameserver.world.World;
 
 /**
  * @author -Nemesiss-, Neon
@@ -16,8 +17,7 @@ import com.aionemu.gameserver.utils.ThreadPoolManager;
 @AIName("deliveryman")
 public class DeliveryManAI extends FollowingNpcAI {
 
-	private static int SERVICE_TIME = 5 * 60 * 1000;
-	private Player owner;
+	private static final int SERVICE_TIME = 5 * 60 * 1000;
 
 	public DeliveryManAI(Npc owner) {
 		super(owner);
@@ -26,7 +26,7 @@ public class DeliveryManAI extends FollowingNpcAI {
 	@Override
 	protected void handleSpawned() {
 		super.handleSpawned();
-		owner = getOwner().getPosition().getWorldMapInstance().getPlayer(getOwner().getCreatorId());
+		Player owner = getPlayer();
 		getOwner().getController().addTask(TaskId.DESPAWN,
 			ThreadPoolManager.getInstance().schedule(() -> AIActions.deleteOwner(DeliveryManAI.this), SERVICE_TIME));
 		handleFollowMe(owner);
@@ -38,15 +38,21 @@ public class DeliveryManAI extends FollowingNpcAI {
 	@Override
 	protected void handleDespawned() {
 		PacketSendUtility.broadcastMessage(getOwner(), 390267); // Whiririkk, let's go!
-		owner.setPostman(null);
+		Player player = World.getInstance().findPlayer(getOwner().getCreatorId());
+		if (player != null && getOwner().equals(player.getPostman()))
+			player.setPostman(null);
 		super.handleDespawned();
 	}
 
 	@Override
 	protected void handleDialogStart(Player player) {
-		if (player.equals(owner))
+		if (player.equals(getPlayer()))
 			PacketSendUtility.sendPacket(player, new SM_DIALOG_WINDOW(getObjectId(), DialogPage.MAIL.id()));
 		else
 			PacketSendUtility.broadcastMessage(getOwner(), 390269); // There is no mail for you, nyerk.
+	}
+
+	private Player getPlayer() {
+		return getOwner().getPosition().getWorldMapInstance().getPlayer(getOwner().getCreatorId());
 	}
 }
