aa5ed08ece05aec2590f88de05b9d5fffd2ccf6a
Sykra <sykraaion@gmail.com>
2019-11-09 11:26:41 +0100

    Hexway rework part 2

--- game-server/data/scripts/system/handlers/instance/abyss/TheHexwayInstance.java
+++ game-server/data/scripts/system/handlers/instance/abyss/TheHexwayInstance.java
@@ -1,17 +1,20 @@
 package instance.abyss;
 
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicLongArray;
 
-import com.aionemu.commons.utils.Rnd;
 import com.aionemu.gameserver.controllers.attack.AttackStatus;
 import com.aionemu.gameserver.dataholders.DataManager;
 import com.aionemu.gameserver.instance.handlers.GeneralInstanceHandler;
 import com.aionemu.gameserver.instance.handlers.InstanceID;
+import com.aionemu.gameserver.model.ChatType;
 import com.aionemu.gameserver.model.EmotionType;
 import com.aionemu.gameserver.model.actions.NpcActions;
 import com.aionemu.gameserver.model.flyring.FlyRing;
@@ -21,9 +24,9 @@ import com.aionemu.gameserver.model.gameobjects.StaticDoor;
 import com.aionemu.gameserver.model.gameobjects.player.Player;
 import com.aionemu.gameserver.model.templates.flyring.FlyRingTemplate;
 import com.aionemu.gameserver.model.utils3d.Point3D;
-import com.aionemu.gameserver.network.aion.AionServerPacket;
 import com.aionemu.gameserver.network.aion.serverpackets.SM_DIE;
 import com.aionemu.gameserver.network.aion.serverpackets.SM_EMOTION;
+import com.aionemu.gameserver.network.aion.serverpackets.SM_MESSAGE;
 import com.aionemu.gameserver.network.aion.serverpackets.SM_QUEST_ACTION;
 import com.aionemu.gameserver.network.aion.serverpackets.SM_SYSTEM_MESSAGE;
 import com.aionemu.gameserver.services.player.PlayerReviveService;
@@ -38,21 +41,27 @@ import com.aionemu.gameserver.world.WorldMapInstance;
 @InstanceID(300700000)
 public class TheHexwayInstance extends GeneralInstanceHandler {
 
-	private final Future<?>[] scheduledBossDespawns = new ScheduledFuture[6];
-	private final AtomicLongArray countdownTimer = new AtomicLongArray(7);
-	private final AtomicInteger currentCountdownIndex = new AtomicInteger(-2);
+	private static final String MSG_TIMER_STARTED = "You have 22 minutes to kill all boss mobs located at the end of each bridge to unlock a bonus chest!";
+	private static final String MSG_TIME_OVER = "Your time to unlock a bonus chest has expired!";
+
+	private final int[] bossTimeLimitsSeconds = new int[] { 120, 210, 150, 150, 210, 120 };
+	private final int[] treasureDoorIds = new int[] { 60, 61, 63, 64, 65, 66 };
+	private final int[] bossNpcIds = new int[] { 219611, 286933, 219612, 219613, 219610, 219614 };
+	private final int bonusChestTimeLimitSeconds = 1320;
+	private final int[] notifyTimesSeconds = new int[] { 900, 600, 300, 120, 60, 30, 10, 3, 2, 1 };
+
+	private final Future<?>[] scheduledBossDespawnTasks = new ScheduledFuture[6];
+	private final AtomicLongArray stageStartMillis = new AtomicLongArray(6);
+	private final Map<Player, Integer> playerStageMapping = new ConcurrentHashMap<>();
+
 	private final AtomicBoolean bonusChestSpawnAllowed = new AtomicBoolean(true);
-	private Future<?> bonusChestTask;
-
-	private final int[] bossTimeLimits = new int[] { 130, 210, 150, 150, 210, 130 };
-	private final int[] treasureDoors = new int[] { 60, 61, 63, 64, 65, 66 };
-	private final int[] bossIds = new int[] { 219611, 286933, 219612, 219613, 219610, 219614 };
-	private final int bonusChestTimeLimit = 1320;
-
+	private final AtomicLong bonusChestStartMillis = new AtomicLong();
+	private final Future<?>[] timerProgressMsgTasks = new ScheduledFuture[notifyTimesSeconds.length];
 	private final AtomicInteger killedBossCount = new AtomicInteger();
 	private final AtomicInteger attackedBossCount = new AtomicInteger();
+	private Future<?> disableBonusChestSpawnTask;
 
-	private AtomicBoolean handlingSecondBoss = new AtomicBoolean();
+	private final AtomicBoolean handlingSecondBoss = new AtomicBoolean();
 
 	@Override
 	public void onInstanceCreate(WorldMapInstance instance) {
@@ -62,27 +71,50 @@ public class TheHexwayInstance extends GeneralInstanceHandler {
 
 	@Override
 	public boolean onPassFlyingRing(Player player, String flyingRing) {
-		if (flyingRing.equals("HEXWAY_WING_1")) {
-			if (countdownTimer.compareAndSet(0, 0, System.currentTimeMillis())) {
-				sendTimerToPlayers(bonusChestTimeLimit);
+		if (flyingRing.equals("HEXWAY_BONUSCHEST")) {
+			if (bonusChestStartMillis.compareAndSet(0, System.currentTimeMillis())) {
 				// schedule bonus chest spawn condition change
-				bonusChestTask = ThreadPoolManager.getInstance().schedule(() -> {
-					bonusChestSpawnAllowed.set(false);
-					if (currentCountdownIndex.get() == 0) {
-						removeTimer();
+				disableBonusChestSpawnTask = ThreadPoolManager.getInstance().schedule(() -> {
+					if (bonusChestSpawnAllowed.compareAndSet(true, false))
+						PacketSendUtility.broadcastToMap(instance, new SM_MESSAGE(0, null, MSG_TIME_OVER, ChatType.BRIGHT_YELLOW_CENTER));
+				}, bonusChestTimeLimitSeconds * 1000);
+
+				// send info message to all players
+				PacketSendUtility.broadcastToMap(instance, new SM_MESSAGE(0, null, MSG_TIMER_STARTED, ChatType.BRIGHT_YELLOW_CENTER));
+
+				// send timer updates to all players
+				for (int i = 0; i < notifyTimesSeconds.length; i++) {
+					int scheduleDelaySeconds = bonusChestTimeLimitSeconds - notifyTimesSeconds[i];
+					if (scheduleDelaySeconds > 0) {
+						final int notifyTimeSecond = notifyTimesSeconds[i];
+						ThreadPoolManager.getInstance().schedule(() -> {
+							// send time info to all players if bonus chest spawn is allowed
+							if (bonusChestSpawnAllowed.get())
+								PacketSendUtility.broadcastToMap(instance, SM_SYSTEM_MESSAGE.STR_MSG_REMAIN_TIME(String.valueOf(notifyTimeSecond)));
+						}, scheduleDelaySeconds * 1000);
 					}
-				}, bonusChestTimeLimit * 1000);
+				}
 			}
 		} else if (flyingRing.startsWith("HEXWAY_BOSS_")) {
-			String number = flyingRing.substring(12);
-			int boss = Integer.parseInt(number);
-			if (boss >= 1 && boss <= 6) {
-				if (countdownTimer.compareAndSet(boss, 0, System.currentTimeMillis())) {
-					int timeLimit = bossTimeLimits[boss - 1];
-					currentCountdownIndex.set(boss);
+			int bossIndex = Integer.parseInt(flyingRing.substring(12));
+			if (bossIndex >= 0 && bossIndex <= 5) {
+				if (stageStartMillis.compareAndSet(bossIndex, 0, System.currentTimeMillis())) {
 					attackedBossCount.incrementAndGet();
-					sendTimerToPlayers(timeLimit);
-					scheduleBossDespawn(boss, boss - 1, timeLimit * 1000);
+					scheduleBossDespawn(bossIndex, bossTimeLimitsSeconds[bossIndex] * 1000);
+				}
+				long stageStartTimeMillis = stageStartMillis.get(bossIndex);
+				if (stageStartTimeMillis > 0) {
+					int bossTimeLimitSeconds = bossTimeLimitsSeconds[bossIndex];
+					int elapsedTimeMillis = (int) (System.currentTimeMillis() - stageStartTimeMillis);
+					if (elapsedTimeMillis <= bossTimeLimitSeconds * 1000 && scheduledBossDespawnTasks[bossIndex] != null) {
+						// add player to stage mapping
+						synchronized (playerStageMapping) {
+							playerStageMapping.put(player, bossIndex);
+						}
+						// send quest timer to player
+						int remainingTimeSeconds = bossTimeLimitSeconds - elapsedTimeMillis / 1000;
+						PacketSendUtility.sendPacket(player, new SM_QUEST_ACTION(0, remainingTimeSeconds));
+					}
 				}
 			}
 		}
@@ -91,38 +123,27 @@ public class TheHexwayInstance extends GeneralInstanceHandler {
 
 	@Override
 	public void onLeaveInstance(Player player) {
-		if (player.isOnline()) {
-			PacketSendUtility.sendPacket(player, new SM_QUEST_ACTION(0, -1));
+		if (player != null && player.isOnline()) {
+			synchronized (playerStageMapping) {
+				playerStageMapping.remove(player);
+			}
+			PacketSendUtility.sendPacket(player, new SM_QUEST_ACTION(0, 0));
 		}
 	}
 
-	private void sendPacketToOnlineInstancePlayers(AionServerPacket packet) {
-		instance.forEachPlayer(p -> {
-			if (p.isOnline())
-				PacketSendUtility.sendPacket(p, packet);
-		});
-	}
+	@Override
+	public void onInstanceDestroy() {
+		// cleanup all scheduled tasks
+		for (Future<?> scheduledBossDespawn : scheduledBossDespawnTasks)
+			if (scheduledBossDespawn != null && !scheduledBossDespawn.isDone())
+				scheduledBossDespawn.cancel(true);
 
-	private void cancelBossDespawn(int bossIndex) {
-		Future<?> scheduledDespawn = scheduledBossDespawns[bossIndex];
-		if (scheduledDespawn != null && !scheduledDespawn.isCancelled())
-			scheduledDespawn.cancel(true);
-		scheduledBossDespawns[bossIndex] = null;
-	}
+		if (disableBonusChestSpawnTask != null && !disableBonusChestSpawnTask.isDone())
+			disableBonusChestSpawnTask.cancel(true);
 
-	private void scheduleBossDespawn(int bridgeNumber, int bossIndex, int despawnDelay) {
-		ScheduledFuture<?> despawnTask = ThreadPoolManager.getInstance().schedule(() -> {
-			int bossId = bossIds[bossIndex];
-			sendPacketToOnlineInstancePlayers(
-				SM_SYSTEM_MESSAGE.STR_MSG_HOUSING_OBJECT_DELETE_USE_COUNT_FINAL(DataManager.NPC_DATA.getNpcTemplate(bossId).getL10n()));
-			despawnNpcs(getNpcs(bossId), true);
-			bonusChestSpawnAllowed.set(false);
-			if (currentCountdownIndex.get() == bridgeNumber) {
-				currentCountdownIndex.set(-1);
-				removeTimer();
-			}
-		}, despawnDelay);
-		scheduledBossDespawns[bossIndex] = despawnTask;
+		for (Future<?> timerProgressMsgTask : timerProgressMsgTasks)
+			if (timerProgressMsgTask != null && !timerProgressMsgTask.isDone())
+				timerProgressMsgTask.cancel(true);
 	}
 
 	@Override
@@ -138,8 +159,8 @@ public class TheHexwayInstance extends GeneralInstanceHandler {
 		switch (npc.getNpcId()) {
 			// manager jarka
 			case 219609:
-				// open/despawn barricades
-				despawnNpcs(getNpcs(219617), false);
+				// open barricades
+				despawnNpcs(instance.getNpcs(219617), false);
 				break;
 			case 219611:
 			case 286933:
@@ -148,35 +169,24 @@ public class TheHexwayInstance extends GeneralInstanceHandler {
 			case 219610:
 			case 219614:
 				// bridge bosses
-				for (int bridgeNumber = 0; bridgeNumber < bossIds.length; bridgeNumber++) {
-					int bossId = bossIds[bridgeNumber];
-					if (npc.getNpcId() == bossId) {
-						cancelBossDespawn(bridgeNumber);
-						spawnBossLoot(bridgeNumber + 1);
-						openDoor(treasureDoors[bridgeNumber]);
-						int killedCount = killedBossCount.incrementAndGet();
-						if (attackedBossCount.get() == 6) {
-							if (killedCount == 6) {
-								if (bonusChestSpawnAllowed.get()) {
-									if (bonusChestTask != null && !bonusChestTask.isDone() && !bonusChestTask.isCancelled()) {
-										bonusChestTask.cancel(false);
-									}
-									spawnBonusChest();
-									bonusChestSpawnAllowed.set(false);
-								}
-							} else {
-								currentCountdownIndex.set(-1);
-							}
-							removeTimer();
-						} else {
-							if (bonusChestSpawnAllowed.get()) {
-								if (currentCountdownIndex.get() == bridgeNumber + 1) {
-									currentCountdownIndex.set(0);
-									sendRemainingTimeToPlayer(null, 0, bonusChestTimeLimit, true);
-								}
-							} else {
-								currentCountdownIndex.set(-1);
-								removeTimer();
+				for (int bossIndex = 0; bossIndex < bossNpcIds.length; bossIndex++) {
+					int bossNpcId = bossNpcIds[bossIndex];
+					if (npc.getNpcId() == bossNpcId) {
+						cancelBossDespawn(bossIndex);
+						spawnBossChest(bossIndex);
+						openDoor(treasureDoorIds[bossIndex]);
+						// remove quest timers for affected players
+						synchronized (playerStageMapping) {
+							for (Player player : playerStageMapping.keySet())
+								if (playerStageMapping.get(player) == bossIndex)
+									PacketSendUtility.sendPacket(player, new SM_QUEST_ACTION(0, 0));
+						}
+						// spawn bonus loot chest if all 6 boss npc are killed in the given time limits
+						if (attackedBossCount.get() == 6 && killedBossCount.incrementAndGet() == 6) {
+							if (bonusChestSpawnAllowed.compareAndSet(true, false)) {
+								if (disableBonusChestSpawnTask != null && !disableBonusChestSpawnTask.isDone())
+									disableBonusChestSpawnTask.cancel(true);
+								spawnBonusChest();
 							}
 						}
 						break;
@@ -191,7 +201,7 @@ public class TheHexwayInstance extends GeneralInstanceHandler {
 					Npc secondNpc = getNpc(secondNpcId);
 					if (secondNpc != null) {
 						int currentHpPercentage = secondNpc.getLifeStats().getHpPercentage();
-						int bossId = bossIds[1];
+						int bossId = bossNpcIds[1];
 						Npc bossNpc = getNpc(bossId);
 						if (bossNpc != null) {
 							int percentageToDrop = 25 - currentHpPercentage;
@@ -211,49 +221,37 @@ public class TheHexwayInstance extends GeneralInstanceHandler {
 		}
 	}
 
-	private void sendTimerToPlayers(int time) {
-		sendPacketToOnlineInstancePlayers(new SM_QUEST_ACTION(0, time));
-	}
-
-	@Override
-	public void onEnterInstance(Player player) {
-		sendCurrentTimerToPlayer(player);
-	}
-
 	@Override
 	public boolean onReviveEvent(Player player) {
 		PlayerReviveService.revive(player, 25, 25, false, 0);
 		player.getGameStats().updateStatsAndSpeedVisually();
 		PacketSendUtility.sendPacket(player, SM_SYSTEM_MESSAGE.STR_REBIRTH_MASSAGE_ME());
 		TeleportService.teleportTo(player, mapId, 672.8343f, 606.2250f, 321.1900f, (byte) 0);
-		sendCurrentTimerToPlayer(player);
 		return true;
 	}
 
-	private void removeTimer() {
-		sendTimerToPlayers(-1);
+	@Override
+	public void handleUseItemFinish(Player player, Npc npc) {
+		if (npc.getNpcId() == 700455)
+			TeleportService.moveToInstanceExit(player, mapId, player.getRace());
 	}
 
-	private void sendRemainingTimeToPlayer(Player player, int timerIndex, long maxTime, boolean sendToInstancePlayers) {
-		long startTime = countdownTimer.get(timerIndex);
-		if (startTime > 0) {
-			long elapsedTime = System.currentTimeMillis() - startTime;
-			elapsedTime /= 1000;
-			if (elapsedTime < maxTime) {
-				int remainingTime = (int) (maxTime - elapsedTime);
-				if (sendToInstancePlayers) {
-					sendTimerToPlayers(remainingTime);
-				} else {
-					if (player != null) {
-						PacketSendUtility.sendPacket(player, new SM_QUEST_ACTION(0, remainingTime));
-					}
-				}
-			}
-		}
+	private void cancelBossDespawn(int bossIndex) {
+		Future<?> scheduledDespawn = scheduledBossDespawnTasks[bossIndex];
+		if (scheduledDespawn != null && !scheduledDespawn.isDone())
+			scheduledDespawn.cancel(true);
+		scheduledBossDespawnTasks[bossIndex] = null;
 	}
 
-	private List<Npc> getNpcs(int npcId) {
-		return instance.getNpcs(npcId);
+	private void scheduleBossDespawn(int bossIndex, int despawnDelayMillis) {
+		ScheduledFuture<?> despawnTask = ThreadPoolManager.getInstance().schedule(() -> {
+			int bossNpcId = bossNpcIds[bossIndex];
+			PacketSendUtility.broadcastToMap(instance,
+				SM_SYSTEM_MESSAGE.STR_MSG_HOUSING_OBJECT_DELETE_USE_COUNT_FINAL(DataManager.NPC_DATA.getNpcTemplate(bossNpcId).getL10n()));
+			despawnNpcs(instance.getNpcs(bossNpcId), true);
+			bonusChestSpawnAllowed.set(false);
+		}, despawnDelayMillis);
+		scheduledBossDespawnTasks[bossIndex] = despawnTask;
 	}
 
 	private void despawnNpcs(List<Npc> npcs, boolean ignoreDeadMobs) {
@@ -262,71 +260,61 @@ public class TheHexwayInstance extends GeneralInstanceHandler {
 	}
 
 	private void despawnNpc(Npc npc, boolean ignoreDead) {
-		if (npc != null)
-			if (!ignoreDead || npc.isDead())
-				NpcActions.delete(npc);
-	}
-
-	private void sendCurrentTimerToPlayer(Player player) {
-		int index = currentCountdownIndex.get();
-		if (index >= 0)
-			sendRemainingTimeToPlayer(player, index, index == 0 ? bonusChestTimeLimit : bossTimeLimits[index], false);
+		if (npc != null && (!ignoreDead || npc.isDead()))
+			NpcActions.delete(npc);
 	}
 
 	private void spawnBonusChest() {
-		sendPacketToOnlineInstancePlayers(SM_SYSTEM_MESSAGE.STR_MSG_IDAbRe_Core_NmdC_BoxSpawn());
+		PacketSendUtility.broadcastToMap(instance, SM_SYSTEM_MESSAGE.STR_MSG_IDAbRe_Core_NmdC_BoxSpawn());
 		spawn(701664, 485.59f, 585.42f, 357f, (byte) 0);
 	}
 
-	private void spawnBossLoot(int bridgeNumber) {
-		switch (bridgeNumber) {
-			case 1:
+	private void spawnBossChest(int bossIndex) {
+		switch (bossIndex) {
+			case 0:
 				spawn(701663, 223.6263f, 427.2576f, 364.6045f, (byte) 117);
-				if (Rnd.get(0, 100) <= 60) {
-					spawn(701662, 223.5121f, 409.0381f, 365.0105f, (byte) 25);
-				}
+				spawn(701662, 223.5121f, 409.0381f, 365.0105f, (byte) 25);
 				break;
-			case 2:
+			case 1:
 				spawn(701663, 195.7458f, 494.1713f, 365.0105f, (byte) 2);
 				spawn(701662, 193.7205f, 499.2636f, 365.0105f, (byte) 103);
 				break;
-			case 3:
+			case 2:
 				spawn(701663, 197.9107f, 566.2823f, 365.0105f, (byte) 91);
 				spawn(701662, 181.2328f, 537.9518f, 365.0105f, (byte) 17);
 				break;
-			case 4:
+			case 3:
 				spawn(701663, 185.4841f, 630.1168f, 365.0105f, (byte) 108);
 				spawn(701662, 201.0349f, 612.7051f, 364.6045f, (byte) 25);
-			case 5:
+			case 4:
 				spawn(701663, 192.4604f, 673.4794f, 365.0105f, (byte) 13);
 				spawn(701662, 213.7684f, 696.3926f, 365.0105f, (byte) 25);
 				break;
-			case 6:
+			case 5:
 				spawn(701663, 241.1405f, 754.4207f, 365.0105f, (byte) 96);
-				if (Rnd.get(0, 100) <= 60)
-					spawn(701662, 214.3266f, 743.3648f, 365.0105f, (byte) 25);
+				spawn(701662, 214.3266f, 743.3648f, 365.0105f, (byte) 25);
 				break;
 		}
 	}
 
 	private void initTimerTrigger() {
 		spawnBonusChestTimerTrigger();
-		spawnBossTimerTrigger(1, new Point3D(313.6324, 456.5294, 363.2561), new Point3D(310.5882, 462.1104, 365.7663),
-			new Point3D(307.6461, 468.3907, 368.2764));
-		spawnBossTimerTrigger(2, new Point3D(295.2951, 503.8784, 363.2561), new Point3D(293.2075, 510.0642, 365.7663),
-			new Point3D(290.7751, 516.5335, 368.2764));
-		spawnBossTimerTrigger(3, new Point3D(285.3754, 553.6730, 363.2561), new Point3D(284.4015, 560.1709, 365.7663),
-			new Point3D(283.3571, 566.7198, 368.2764));
-		spawnBossTimerTrigger(4, new Point3D(284.2865, 604.4503, 363.2561), new Point3D(284.3718, 611.0146, 365.7663),
-			new Point3D(284.3671, 617.4957, 368.2764));
-		spawnBossTimerTrigger(5, new Point3D(292.3535, 655.3181, 363.2561), new Point3D(293.0183, 661.3437, 365.7663),
-			new Point3D(294.3258, 667.3324, 368.2764));
-		spawnBossTimerTrigger(6, new Point3D(308.2845, 703.0124, 363.2561), new Point3D(310.6309, 709.1545, 365.7663),
-			new Point3D(312.7243, 714.9787, 368.2764));
+		spawnBossTimerTrigger(0, new Point3D(321.3199, 458.7944, 368.2764), new Point3D(317.8786, 465.5034, 361.7755),
+			new Point3D(314.5425, 472.0469, 361.7755));
+		spawnBossTimerTrigger(1, new Point3D(302.9791, 504.9413, 368.2764), new Point3D(300.9405, 511.9926, 361.7755),
+			new Point3D(298.7013, 519.5820, 361.7755));
+		spawnBossTimerTrigger(2, new Point3D(293.5730, 553.3477, 368.2764), new Point3D(292.3480, 560.7227, 361.7755),
+			new Point3D(291.3300, 568.1430, 361.7755));
+		spawnBossTimerTrigger(3, new Point3D(292.1579, 602.8574, 368.2764), new Point3D(292.4263, 610.3403, 361.7755),
+			new Point3D(292.6229, 617.7412, 361.7755));
+		spawnBossTimerTrigger(4, new Point3D(299.2268, 651.8597, 368.2764), new Point3D(300.9197, 659.2391, 361.7755),
+			new Point3D(302.4053, 666.5773, 361.7755));
+		spawnBossTimerTrigger(5, new Point3D(315.2073, 698.8103, 368.2764), new Point3D(317.9026, 705.6396, 361.7755),
+			new Point3D(320.6522, 712.8006, 361.7755));
 	}
 
 	private void spawnBonusChestTimerTrigger() {
-		FlyRing f1 = new FlyRing(new FlyRingTemplate("HEXWAY_WING_1", mapId, new Point3D(576.2102, 585.4146, 353.90677),
+		FlyRing f1 = new FlyRing(new FlyRingTemplate("HEXWAY_BONUSCHEST", mapId, new Point3D(576.2102, 585.4146, 353.90677),
 			new Point3D(576.2102, 585.4146, 359.90677), new Point3D(575.18384, 596.36664, 353.90677), 10), instanceId);
 		f1.spawn();
 	}
@@ -342,9 +330,4 @@ public class TheHexwayInstance extends GeneralInstanceHandler {
 			door.setOpen(true);
 	}
 
-	@Override
-	public void handleUseItemFinish(Player player, Npc npc) {
-		if (npc.getNpcId() == 700455)
-			TeleportService.moveToInstanceExit(player, mapId, player.getRace());
-	}
 }
