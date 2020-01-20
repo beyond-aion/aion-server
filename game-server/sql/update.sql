cce6fc87c80f8072d46892702a57dcec73f60061
Sykra <sykraaion@gmail.com>
2020-01-20 18:29:28 +0100

    Removed advent calendar stuff. This reverts commit #9070698f.

--- game-server/sql/update.sql
+++ game-server/sql/update.sql
@@ -1,10 +1,6 @@
 /*
- * DB changes since fe40449c (15.11.2019)
+ * DB changes since 230ce869 (13.12.2019)
  */
 
--- create advent calendar table
-CREATE TABLE `advent` (
-  `account_id` int(11) NOT NULL,
-  `last_day_received` tinyint(4) NOT NULL,
-  PRIMARY KEY (`account_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- drop advent calendar table
+DROP TABLE advent;
