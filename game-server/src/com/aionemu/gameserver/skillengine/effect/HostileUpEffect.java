2d90f039e8340e08a4be5b960d7605c99acc9ae6
Yeats <pooya_dialeh@hotmail.de>
2020-02-21 17:15:45 +0100

    Fixed #BA2431 https://beyond-aion.com/forum/thread/2431-aggro-des-%C3%A4thertechs-gladi/. Added missing temp hate values in skill_templates and also added hate boost calculation to hate value generated by auto attacks/skill damage

--- game-server/src/com/aionemu/gameserver/skillengine/effect/HostileUpEffect.java
+++ game-server/src/com/aionemu/gameserver/skillengine/effect/HostileUpEffect.java
@@ -2,11 +2,14 @@ package com.aionemu.gameserver.skillengine.effect;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlType;
 
 import com.aionemu.gameserver.model.gameobjects.Creature;
 import com.aionemu.gameserver.model.gameobjects.Npc;
 import com.aionemu.gameserver.skillengine.model.Effect;
+import com.aionemu.gameserver.utils.ThreadPoolManager;
+import com.aionemu.gameserver.utils.stats.StatFunctions;
 
 /**
  * @author ATracer
@@ -14,6 +17,15 @@ import com.aionemu.gameserver.skillengine.model.Effect;
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "HostileUpEffect")
 public class HostileUpEffect extends EffectTemplate {
+
+	@XmlAttribute(name = "temp_duration")
+	protected int tempDuration = 0;
+	@XmlAttribute(name = "temp_value")
+	protected int tempValue = 0;
+	@XmlAttribute(name = "temp_delta")
+	protected int tempDelta = 0;
+
+	private int tempHate = 0;
 
 	@Override
 	public void applyEffect(Effect effect) {
@@ -25,6 +37,15 @@ public class HostileUpEffect extends EffectTemplate {
 			if (effect.getSuccessEffects().size() == 1) // only this effect template is present, therefore we know regular hate will never broadcast
 				totalHate += effect.getEffectHate();
 			((Npc) effected).getAggroList().addHate(effect.getEffector(), totalHate);
+			if (tempHate > 0) {
+				effected.getAggroList().addHate(effect.getEffector(), tempHate);
+				ThreadPoolManager.getInstance().schedule(new Runnable() {
+					@Override
+					public void run() {
+						effected.getAggroList().addHate(effect.getEffector(), -1 * tempHate);
+					}
+				}, tempDuration);
+			}
 		}
 	}
 
@@ -33,5 +54,8 @@ public class HostileUpEffect extends EffectTemplate {
 		if (!super.calculate(effect, null, null))
 			return;
 		effect.setTauntHate(calculateBaseValue(effect));
+		tempHate = tempValue + tempDelta * effect.getSkillLevel();
+		if (tempHate > 0)
+			tempHate = StatFunctions.calculateHate(effect.getEffected(), tempHate);
 	}
 }
