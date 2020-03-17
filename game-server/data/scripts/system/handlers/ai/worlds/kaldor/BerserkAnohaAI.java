9fbcd3162c4e7467115d5e274620bea0f1944bd7
Yeats <pooya_dialeh@hotmail.de>
2020-03-17 02:07:45 +0100

    Fixed players who were not online did not get corresponding quests for berserk anoha. Now anyone who comes near berserk anoha receives the quest.

--- game-server/data/scripts/system/handlers/ai/worlds/kaldor/BerserkAnohaAI.java
+++ game-server/data/scripts/system/handlers/ai/worlds/kaldor/BerserkAnohaAI.java
@@ -4,10 +4,16 @@ import java.util.concurrent.TimeUnit;
 
 import com.aionemu.gameserver.ai.AIName;
 import com.aionemu.gameserver.ai.poll.AIQuestion;
+import com.aionemu.gameserver.model.Race;
 import com.aionemu.gameserver.model.TaskId;
+import com.aionemu.gameserver.model.gameobjects.Creature;
 import com.aionemu.gameserver.model.gameobjects.Npc;
+import com.aionemu.gameserver.model.gameobjects.player.Player;
 import com.aionemu.gameserver.model.siege.SiegeRace;
 import com.aionemu.gameserver.network.aion.serverpackets.SM_SYSTEM_MESSAGE;
+import com.aionemu.gameserver.questEngine.model.QuestEnv;
+import com.aionemu.gameserver.questEngine.model.QuestState;
+import com.aionemu.gameserver.services.QuestService;
 import com.aionemu.gameserver.services.SiegeService;
 import com.aionemu.gameserver.utils.PacketSendUtility;
 import com.aionemu.gameserver.utils.ThreadPoolManager;
@@ -23,6 +29,8 @@ import ai.AggressiveNpcAI;
 @AIName("berserk_anoha")
 public class BerserkAnohaAI extends AggressiveNpcAI {
 
+	private SiegeRace occupier;
+
 	public BerserkAnohaAI(Npc owner) {
 		super(owner);
 	}
@@ -31,6 +39,7 @@ public class BerserkAnohaAI extends AggressiveNpcAI {
 	protected void handleSpawned() {
 		super.handleSpawned();
 		scheduleDespawn();
+		occupier = SiegeService.getInstance().getFortress(7011).getRace();
 	}
 
 	private void scheduleDespawn() {
@@ -59,13 +68,24 @@ public class BerserkAnohaAI extends AggressiveNpcAI {
 	}
 
 	private void checkForFactionReward() {
-		SiegeRace occupier = SiegeService.getInstance().getFortress(7011).getRace();
 		Npc ca = (Npc) spawn(occupier == SiegeRace.ASMODIANS ? 804594 : 804595, 785.4833f, 458.4128f, 143.7177f, (byte) 30); // Commander Anoha
 		ca.getController().addTask(TaskId.DESPAWN, ThreadPoolManager.getInstance().schedule(() -> ca.getController().delete(), 60, TimeUnit.MINUTES));
 	}
 
 	private void broadcastAnnounce(SM_SYSTEM_MESSAGE msg) {
 		World.getInstance().forEachPlayer(player -> PacketSendUtility.sendPacket(player, msg));
+	}
+
+	@Override
+	protected void handleCreatureSee(Creature creature) {
+		super.handleCreatureSee(creature);
+		if (creature instanceof Player) {
+			if (occupier == SiegeRace.ASMODIANS) {
+				startQuest((Player) creature, creature.getRace() == Race.ELYOS ? 13818 : 23817);
+			} else if (occupier == SiegeRace.ELYOS) {
+				startQuest((Player) creature, creature.getRace() == Race.ELYOS ? 13817 : 23818);
+			}
+		}
 	}
 
 	@Override
@@ -77,5 +97,12 @@ public class BerserkAnohaAI extends AggressiveNpcAI {
 				return false;
 		}
 		return super.ask(question);
+	}
+
+	private void startQuest(Player player, int questId) {
+		final QuestState qs = player.getQuestStateList().getQuestState(questId);
+		QuestEnv env = new QuestEnv(null, player, questId);
+		if (qs == null || qs.isStartable())
+			QuestService.startQuest(env);
 	}
 }
