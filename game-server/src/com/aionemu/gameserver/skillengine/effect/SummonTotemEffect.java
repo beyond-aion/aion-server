b7f5e80a6e68cf38f98d74eeee6bf3f93720a7c6
Yeats <pooya_dialeh@hotmail.de>
2020-03-22 05:21:26 +0100

    Fixed spawned npcs of SummonTotemEffect would spawn behind obstacles.

--- game-server/src/com/aionemu/gameserver/skillengine/effect/SummonTotemEffect.java
+++ game-server/src/com/aionemu/gameserver/skillengine/effect/SummonTotemEffect.java
@@ -4,10 +4,14 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlType;
 
+import com.aionemu.gameserver.geoEngine.collision.CollisionIntention;
+import com.aionemu.gameserver.geoEngine.collision.IgnoreProperties;
+import com.aionemu.gameserver.geoEngine.math.Vector3f;
 import com.aionemu.gameserver.model.gameobjects.Creature;
 import com.aionemu.gameserver.model.gameobjects.NpcObjectType;
 import com.aionemu.gameserver.skillengine.model.Effect;
 import com.aionemu.gameserver.utils.PositionUtil;
+import com.aionemu.gameserver.world.geo.GeoService;
 
 /**
  * @author kecimis
@@ -25,9 +29,11 @@ public class SummonTotemEffect extends SummonServantEffect {
 		if (effect.getSkill().isFirstTargetSelf()) {
 			Creature effected = effect.getEffected();
 			double radian = Math.toRadians(PositionUtil.convertHeadingToAngle(effect.getEffector().getHeading()));
-			x = effected.getX() + (float) (Math.cos(radian) * 2);
-			y = effected.getY() + (float) (Math.sin(radian) * 2);
-			z = effected.getZ();
+			Vector3f pos = GeoService.getInstance().getClosestCollision(effector, effected.getX() + (float) (Math.cos(radian) * 2), effected.getY() + (float) (Math.sin(radian) * 2),
+					effected.getZ(), true, CollisionIntention.DEFAULT_COLLISIONS.getId(), IgnoreProperties.of(effector.getRace()));
+			x = pos.getX();
+			y = pos.getY();
+			z = pos.getZ();
 		} else if (effect.getSkill().isPointSkill()) { //fix for [657]Battle Banner
 			x = effect.getX();
 			y = effect.getY();
