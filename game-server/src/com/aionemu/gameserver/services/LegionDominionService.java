576b226d79ca1b6648abdccc16b98f5f61c7a2be
Sykra <sykraaion@gmail.com>
2020-03-26 23:08:14 +0100

    legion territory doesnt reset correctly for the occupying legion if the legion doesnt participate at the fight in the current period

--- game-server/src/com/aionemu/gameserver/services/LegionDominionService.java
+++ game-server/src/com/aionemu/gameserver/services/LegionDominionService.java
@@ -5,6 +5,8 @@ import java.util.Collection;
 import java.util.Map;
 import java.util.TreeMap;
 
+import org.slf4j.LoggerFactory;
+
 import com.aionemu.commons.database.dao.DAOManager;
 import com.aionemu.gameserver.dao.LegionDAO;
 import com.aionemu.gameserver.dao.LegionDominionDAO;
@@ -75,37 +77,62 @@ public class LegionDominionService {
 		}
 	}
 
+	private void storeAndUpdateDominionStatusForLegion(Legion legion, int locationId) {
+		DAOManager.getDAO(LegionDAO.class).storeLegion(legion);
+		PacketSendUtility.broadcastToLegion(legion, new SM_LEGION_DOMINION_RANK(locationId));
+		PacketSendUtility.broadcastToLegion(legion, new SM_LEGION_INFO(legion));
+	}
+
 	public void startWeeklyCalculation() {
 		for (LegionDominionLocation loc : legionDominionLocations.values()) {
-			// find winner and change locations legionId to winners Id
+			// reset current occupying legion
+			int previousOccupyingLegionId = loc.getLegionId();
+			if (previousOccupyingLegionId != 0) {
+				Legion legion = LegionService.getInstance().getLegion(previousOccupyingLegionId);
+				if (legion != null) {
+					legion.setOccupiedLegionDominion(0);
+					legion.setLastLegionDominion(loc.getLocationId());
+					legion.setCurrentLegionDominion(0);
+					storeAndUpdateDominionStatusForLegion(legion, loc.getLocationId());
+				}
+			}
+
+			// find winner of stonespear reach challenge
+			int newOccupyingLegionId = 0;
 			LegionDominionParticipantInfo winner = loc.getWinner();
 			if (winner != null) {
 				Legion winningLegion = LegionService.getInstance().getLegion(winner.getLegionId());
-				if (winningLegion != null && !winningLegion.isDisbanding()) {
-					winningLegion.setOccupiedLegionDominion(loc.getLocationId());
-					loc.setLegionId(winningLegion.getLegionId());
-				} else {
-					loc.setLegionId(0);
+				if (winningLegion != null) {
+					if (!winningLegion.isDisbanding()) {
+						newOccupyingLegionId = winningLegion.getLegionId();
+					} else {
+						LoggerFactory.getLogger(LegionDominionService.class).warn(
+							"[Legion dominion] Skipped occupy of location {} for legion [id={}, name={}] due to disbanding", loc.getLocationId(),
+							winningLegion.getLegionId(), winningLegion.getName());
+					}
 				}
-			} else {
-				loc.setLegionId(0);
 			}
+
+			loc.setLegionId(newOccupyingLegionId);
 			loc.setOccupiedDate(new Timestamp(System.currentTimeMillis()));
 
-			// reset all participated legions & store them to db
+			// update all participated legions & store them to db
 			for (LegionDominionParticipantInfo info : loc.getParticipantInfo().values()) {
+				// skip previous legion since its already updated and didnt reoccupy it
+				if (info.getLegionId() == previousOccupyingLegionId && info.getLegionId() != newOccupyingLegionId)
+					continue;
 				Legion legion = LegionService.getInstance().getLegion(info.getLegionId());
 				if (legion != null) {
-					if (winner != null && info.getLegionId() != winner.getLegionId()) {
+					if (winner != null && legion.getLegionId() == newOccupyingLegionId) {
+						legion.setOccupiedLegionDominion(loc.getLocationId());
+					} else {
 						legion.setOccupiedLegionDominion(0);
 					}
 					legion.setLastLegionDominion(loc.getLocationId());
 					legion.setCurrentLegionDominion(0);
-					DAOManager.getDAO(LegionDAO.class).storeLegion(legion);
-					DAOManager.getDAO(LegionDominionDAO.class).delete(info);
-					PacketSendUtility.broadcastToLegion(legion, new SM_LEGION_DOMINION_RANK(loc.getLocationId()));
-					PacketSendUtility.broadcastToLegion(legion, new SM_LEGION_INFO(legion));
+					storeAndUpdateDominionStatusForLegion(legion, loc.getLocationId());
 				}
+				DAOManager.getDAO(LegionDominionDAO.class).delete(info);
 			}
 			// reset locations participant info and update this location
 			loc.reset();
