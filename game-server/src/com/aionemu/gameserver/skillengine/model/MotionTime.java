fd2996defa359149f53e9f10b18086705862b211
Yeats <pooya_dialeh@hotmail.de>
2020-03-27 01:26:26 +0100

    Replaced map.putIfAbsent() with map.computeIfAbsent() in MotionTime.java

--- game-server/src/com/aionemu/gameserver/skillengine/model/MotionTime.java
+++ game-server/src/com/aionemu/gameserver/skillengine/model/MotionTime.java
@@ -116,81 +116,57 @@ public class MotionTime {
 			switch (t.getWeapon()) {
 				case "1hand":
 					wrapper = new WeaponTypeWrapper(ItemGroup.SWORD, null);
-					map.putIfAbsent(wrapper, new HashMap<>());
 					break;
 				case "2hand":
 					wrapper = new WeaponTypeWrapper(ItemGroup.GREATSWORD, null);
-					map.putIfAbsent(wrapper, new HashMap<>());
 					break;
 				case "keyblade":
 					wrapper = new WeaponTypeWrapper(ItemGroup.KEYBLADE, null);
-					map.putIfAbsent(wrapper, new HashMap<>());
 					break;
 				case "polearm":
 					wrapper = new WeaponTypeWrapper(ItemGroup.POLEARM, null);
-					map.putIfAbsent(wrapper, new HashMap<>());
 					break;
 				case "dagger":
 					wrapper = new WeaponTypeWrapper(ItemGroup.DAGGER, null);
-					map.putIfAbsent(wrapper, new HashMap<>());
 					break;
 				case "mace":
 					wrapper = new WeaponTypeWrapper(ItemGroup.MACE, null);
-					map.putIfAbsent(wrapper, new HashMap<>());
 					break;
 				case "staff":
 					wrapper = new WeaponTypeWrapper(ItemGroup.STAFF, null);
-					map.putIfAbsent(wrapper, new HashMap<>());
 					break;
 				case "2weapon":
 					wrapper = new WeaponTypeWrapper(ItemGroup.DAGGER, ItemGroup.DAGGER);
-					WeaponTypeWrapper wrapper2 = new WeaponTypeWrapper(ItemGroup.DAGGER, ItemGroup.SWORD);
-					WeaponTypeWrapper wrapper3 = new WeaponTypeWrapper(ItemGroup.SWORD, ItemGroup.DAGGER);
-					WeaponTypeWrapper wrapper4 = new WeaponTypeWrapper(ItemGroup.SWORD, ItemGroup.SWORD);
-					map.putIfAbsent(wrapper, new HashMap<>());
-					map.putIfAbsent(wrapper2, new HashMap<>());
-					map.putIfAbsent(wrapper3, new HashMap<>());
-					map.putIfAbsent(wrapper4, new HashMap<>());
-					map.get(wrapper2).put(t.getId(), t);
-					map.get(wrapper3).put(t.getId(), t);
-					map.get(wrapper4).put(t.getId(), t);
+					map.computeIfAbsent(new WeaponTypeWrapper(ItemGroup.DAGGER, ItemGroup.SWORD), k -> new HashMap<>()).put(t.getId(), t);
+					map.computeIfAbsent(new WeaponTypeWrapper(ItemGroup.SWORD, ItemGroup.DAGGER), k -> new HashMap<>()).put(t.getId(), t);
+					map.computeIfAbsent(new WeaponTypeWrapper(ItemGroup.SWORD, ItemGroup.SWORD), k -> new HashMap<>()).put(t.getId(), t);
 					break;
 				case "noweapon":
 					wrapper = new WeaponTypeWrapper(null, null);
-					map.putIfAbsent(wrapper, new HashMap<>());
 					break;
 				case "book":
 					wrapper = new WeaponTypeWrapper(ItemGroup.SPELLBOOK, null);
-					map.putIfAbsent(wrapper, new HashMap<>());
 					break;
 				case "orb":
 					wrapper = new WeaponTypeWrapper(ItemGroup.ORB, null);
-					map.putIfAbsent(wrapper, new HashMap<>());
 					break;
 				case "1gun":
 					wrapper = new WeaponTypeWrapper(ItemGroup.GUN, null);
-					map.putIfAbsent(wrapper, new HashMap<>());
 					break;
 				case "2gun":
 					wrapper = new WeaponTypeWrapper(ItemGroup.GUN, ItemGroup.GUN);
-					map.putIfAbsent(wrapper, new HashMap<>());
 					break;
 				case "cannon":
 					wrapper = new WeaponTypeWrapper(ItemGroup.CANNON, null);
-					map.putIfAbsent(wrapper, new HashMap<>());
 					break;
 				case "bow":
 					wrapper = new WeaponTypeWrapper(ItemGroup.BOW, null);
-					map.putIfAbsent(wrapper, new HashMap<>());
 					break;
 				case "harp":
 					wrapper = new WeaponTypeWrapper(ItemGroup.HARP, null);
-					map.putIfAbsent(wrapper, new HashMap<>());
 					break;
 			}
-			if (wrapper != null) {
-				map.get(wrapper).put(t.getId(), t);
-			}
+			map.computeIfAbsent(wrapper, k -> new HashMap<>()).put(t.getId(), t);
 		}
 	}
 }
