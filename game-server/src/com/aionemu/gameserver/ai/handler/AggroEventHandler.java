d445ec758cb2d6f7088a349323ba253ccf29f9a8
Yeats <pooya_dialeh@hotmail.de>
2020-04-06 02:50:50 +0200

    renamed variable PUSHED_RANGE_VALUE to SUPPORT_RANGE_OFFSET

--- game-server/src/com/aionemu/gameserver/ai/handler/AggroEventHandler.java
+++ game-server/src/com/aionemu/gameserver/ai/handler/AggroEventHandler.java
@@ -28,7 +28,7 @@ import com.aionemu.gameserver.world.geo.GeoService;
  */
 public class AggroEventHandler {
 
-	private static final int PUSHED_RANGE_VALUE = 2;
+	private static final int SUPPORT_RANGE_OFFSET = 2; // might be <pushed_range> in client data
 	/**
 	 * @param npcAI
 	 * @param myTarget
@@ -50,7 +50,7 @@ public class AggroEventHandler {
 
 	public static boolean onCreatureNeedsSupport(NpcAI npcAI, Creature creatureAskingForSupport) {
 		Npc owner = npcAI.getOwner();
-		if (TribeRelationService.canHelpCreature(creatureAskingForSupport, owner) && PositionUtil.isInRange(owner, creatureAskingForSupport, owner.getAggroRange() + PUSHED_RANGE_VALUE, false)
+		if (TribeRelationService.canHelpCreature(creatureAskingForSupport, owner) && PositionUtil.isInRange(owner, creatureAskingForSupport, owner.getAggroRange() + SUPPORT_RANGE_OFFSET, false)
 			&& GeoService.getInstance().canSee(owner, creatureAskingForSupport)) {
 			VisibleObject attacker = creatureAskingForSupport.getTarget();
 			if (attacker instanceof Creature) {
@@ -73,7 +73,7 @@ public class AggroEventHandler {
 		VisibleObject target = attacker.getTarget();
 		if (target instanceof Player) {
 			Player playerTarget = (Player) target;
-			if (!owner.isEnemy(playerTarget) && owner.isEnemy(attacker) && PositionUtil.isInRange(owner, playerTarget, owner.getAggroRange() + PUSHED_RANGE_VALUE, false)
+			if (!owner.isEnemy(playerTarget) && owner.isEnemy(attacker) && PositionUtil.isInRange(owner, playerTarget, owner.getAggroRange() + SUPPORT_RANGE_OFFSET, false)
 				&& GeoService.getInstance().canSee(owner, attacker)) {
 				owner.getAggroList().addHate(attacker, 1);
 				return true;
