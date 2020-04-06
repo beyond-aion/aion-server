ec327b1c0f15c2407a155ec69be5aa5850fd78ab
Yeats <pooya_dialeh@hotmail.de>
2020-04-06 12:54:49 +0200

    Updated comment in CM_HEADING_UPDATE to make it easier to understand

--- game-server/src/com/aionemu/gameserver/network/aion/clientpackets/CM_HEADING_UPDATE.java
+++ game-server/src/com/aionemu/gameserver/network/aion/clientpackets/CM_HEADING_UPDATE.java
@@ -7,14 +7,15 @@ import com.aionemu.gameserver.network.aion.AionConnection.State;
 
 public class CM_HEADING_UPDATE extends AionClientPacket {
 
-	// TODO: Client sends this packet after spin effect. Find out other states and what to answer
+	// Client sends this packet after spin effect to update the heading (in that case we already set the heading before we receive this packet)
+	// TODO: Find out when else this packet is sent and what or even if we have to answer
 	public CM_HEADING_UPDATE(int opcode, Set<State> validStates) {
 		super(opcode, validStates);
 	}
 
 	@Override
 	protected void readImpl() {
-		readC(); // heading, which is already set before this packet is received
+		readC(); // heading
 	}
 
 	@Override
