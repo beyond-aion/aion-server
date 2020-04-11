33b81f9cfd62b042d2d70dcf2ac39b6bc12e82e2
Yeats <pooya_dialeh@hotmail.de>
2020-04-11 05:45:41 +0200

    Removed unused imports and static List in Nomorph.java

--- game-server/data/scripts/system/handlers/playercommands/Nomorph.java
+++ game-server/data/scripts/system/handlers/playercommands/Nomorph.java
@@ -1,24 +1,13 @@
 package playercommands;
 
-import com.aionemu.gameserver.model.gameobjects.TransformModel;
-import com.aionemu.gameserver.model.gameobjects.player.Player;
-import com.aionemu.gameserver.network.aion.serverpackets.SM_TRANSFORM;
-import com.aionemu.gameserver.skillengine.effect.EffectTemplate;
-import com.aionemu.gameserver.skillengine.effect.TransformEffect;
-import com.aionemu.gameserver.skillengine.model.Effect;
-import com.aionemu.gameserver.skillengine.model.TransformType;
-import com.aionemu.gameserver.utils.ChatUtil;
-import com.aionemu.gameserver.utils.PacketSendUtility;
-import com.aionemu.gameserver.utils.chathandlers.PlayerCommand;
-
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.List;
+
+import com.aionemu.gameserver.model.gameobjects.player.Player;
+import com.aionemu.gameserver.utils.ChatUtil;
+import com.aionemu.gameserver.utils.chathandlers.PlayerCommand;
 
 
 public class Nomorph extends PlayerCommand {
-
-	private static List<Integer> demorphedPlayers = new ArrayList<>();
 
 	public Nomorph() {
 		super("nomorph", "Enables/disables your current transformation appearance.");
