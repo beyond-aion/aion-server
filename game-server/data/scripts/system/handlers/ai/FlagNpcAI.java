361637f789af43a76f7f827d62241b0745227068
Sykra <sykraaion@gmail.com>
2020-04-16 23:30:39 +0200

    fixed a bug where worldraid/base flags could be killed using some kind of aoe

--- game-server/data/scripts/system/handlers/ai/FlagNpcAI.java
+++ game-server/data/scripts/system/handlers/ai/FlagNpcAI.java
@@ -2,10 +2,12 @@ package ai;
 
 import com.aionemu.gameserver.ai.AIName;
 import com.aionemu.gameserver.ai.NpcAI;
+import com.aionemu.gameserver.model.gameobjects.Creature;
 import com.aionemu.gameserver.model.gameobjects.Npc;
+import com.aionemu.gameserver.skillengine.model.Effect;
 
 /**
- * @author Cheatkiller
+ * @author Cheatkiller, Sykra
  */
 @AIName("flag")
 public class FlagNpcAI extends NpcAI {
@@ -13,4 +15,15 @@ public class FlagNpcAI extends NpcAI {
 	public FlagNpcAI(Npc owner) {
 		super(owner);
 	}
+
+	@Override
+	public int modifyDamage(Creature attacker, int damage, Effect effect) {
+		return 0;
+	}
+
+	@Override
+	public int modifyOwnerDamage(int damage, Creature effected, Effect effect) {
+		return 0;
+	}
+
 }
