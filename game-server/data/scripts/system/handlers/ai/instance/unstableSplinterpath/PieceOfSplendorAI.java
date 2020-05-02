c6860b83413c21e04a89381869435137e614b8e3
Neon <neon@beyond-aion.com>
2020-05-02 17:40:53 +0200

    fixed npe in PieceOfSplendorAI

--- game-server/data/scripts/system/handlers/ai/instance/unstableSplinterpath/PieceOfSplendorAI.java
+++ game-server/data/scripts/system/handlers/ai/instance/unstableSplinterpath/PieceOfSplendorAI.java
@@ -1,7 +1,6 @@
 package ai.instance.unstableSplinterpath;
 
 import com.aionemu.gameserver.ai.AIName;
-import com.aionemu.gameserver.ai.NpcAI;
 import com.aionemu.gameserver.model.gameobjects.Creature;
 import com.aionemu.gameserver.model.gameobjects.Npc;
 import com.aionemu.gameserver.utils.PositionUtil;
@@ -20,20 +19,20 @@ public class PieceOfSplendorAI extends AggressiveNpcAI {
 
 	@Override
 	protected void handleCreatureSee(Creature creature) {
-		checkDistance(this, creature);
+		checkDistance(creature);
 	}
 
 	@Override
 	protected void handleCreatureMoved(Creature creature) {
-		checkDistance(this, creature);
+		checkDistance(creature);
 	}
 
-	private void checkDistance(NpcAI ai, Creature creature) {
-		Npc rukril = getPosition().getWorldMapInstance().getNpc(219551);
+	private void checkDistance(Creature creature) {
 		Npc ebonsoul = getPosition().getWorldMapInstance().getNpc(219552);
-		if (creature instanceof Npc) {
+		if (creature instanceof Npc && ebonsoul != null) {
 			if (PositionUtil.isInRange(getOwner(), ebonsoul, 5) && ebonsoul.getEffectController().hasAbnormalEffect(19159)) {
 				ebonsoul.getEffectController().removeEffect(19159);
+				Npc rukril = getPosition().getWorldMapInstance().getNpc(219551);
 				if (rukril != null && rukril.getEffectController().hasAbnormalEffect(19266))
 					rukril.getEffectController().removeEffect(19266);
 			}
