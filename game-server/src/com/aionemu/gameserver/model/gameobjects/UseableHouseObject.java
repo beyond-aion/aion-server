ee75d083cb5e42e185cc6d4b752f7f5f0bcc713a
Sykra <sykraaion@gmail.com>
2020-08-31 11:59:05 +0200

    replaced UseableHouseObject workarounds with proper fix utilizing its knownlist to unlock itself if a player disconnects without sending CM_RELEASE_OBJECT

--- game-server/src/com/aionemu/gameserver/model/gameobjects/UseableHouseObject.java
+++ game-server/src/com/aionemu/gameserver/model/gameobjects/UseableHouseObject.java
@@ -14,8 +14,6 @@ public abstract class UseableHouseObject<T extends PlaceableHouseObject> extends
 
 	private final AtomicInteger usingPlayer = new AtomicInteger();
 
-	private volatile long autoReleaseTimeMs = 0;
-
 	public UseableHouseObject(HouseRegistry registry, int objId, int templateId) {
 		super(registry, objId, templateId);
 	}
@@ -29,34 +27,14 @@ public abstract class UseableHouseObject<T extends PlaceableHouseObject> extends
 	 * @return True if a player currently uses this object and no one else should access it.
 	 */
 	public boolean isOccupied() {
-		return usingPlayer.get() != 0 && autoReleaseTimeMs >= System.currentTimeMillis();
+		return usingPlayer.get() != 0;
 	}
 
 	/**
-	 * @return False if the occupant could not be set (another player currently uses the object).
+	 * @return True if the player could successful occupy the object or is already occupying it
 	 */
 	public final boolean setOccupant(Player player) {
-		return setOccupant(player, 0);
-	}
-
-	/**
-	 * @return False if the occupant could not be set (another player currently uses the object).
-	 */
-	public final boolean setOccupant(Player player, int reuseDelayMs) {
-		long now = System.currentTimeMillis();
-		if (usingPlayer.compareAndSet(0, player.getObjectId()) || usingPlayer.get() == player.getObjectId()) {
-			autoReleaseTimeMs = now + reuseDelayMs;
-			return true;
-		} else if (World.getInstance().findPlayer(usingPlayer.get()) == null) {
-			usingPlayer.set(player.getObjectId());
-			autoReleaseTimeMs = now + reuseDelayMs;
-			return true;
-		} else if (reuseDelayMs > 0 && autoReleaseTimeMs < now) {
-			usingPlayer.set(player.getObjectId());
-			autoReleaseTimeMs = now + reuseDelayMs;
-			return true;
-		}
-		return false;
+		return usingPlayer.compareAndSet(0, player.getObjectId()) || usingPlayer.get() == player.getObjectId();
 	}
 
 	/**
@@ -64,11 +42,7 @@ public abstract class UseableHouseObject<T extends PlaceableHouseObject> extends
 	 * @return True if the using player was released and the object is not occupied anymore.
 	 */
 	public final boolean releaseOccupant(Player player) {
-		if (usingPlayer.compareAndSet(player.getObjectId(), 0)) {
-			autoReleaseTimeMs = 0;
-			return true;
-		}
-		return false;
+		return usingPlayer.compareAndSet(player.getObjectId(), 0);
 	}
 
 	/**
@@ -76,6 +50,5 @@ public abstract class UseableHouseObject<T extends PlaceableHouseObject> extends
 	 */
 	protected final void releaseOccupant() {
 		usingPlayer.set(0);
-		autoReleaseTimeMs = 0;
 	}
 }
