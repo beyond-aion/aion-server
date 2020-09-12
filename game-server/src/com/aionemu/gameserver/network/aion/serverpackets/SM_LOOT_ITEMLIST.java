fcaa7b1bd437e13806fc2d2def3345059de4858c
Sykra <sykraaion@gmail.com>
2020-09-12 20:34:10 +0200

    changed loot confirmation requirement to behave more retail like. from now on a confirmation is not needed when a player... - ...is not in a team - ...is in a team but no team members are nearby - ...is the owner of the item - this should fix most concerns mentioned in #BA2436 (https://beyond-aion.com/forum/thread/2436-die-lootbestätigung-abschaffen/) - removed superfluous javadoc

--- game-server/src/com/aionemu/gameserver/network/aion/serverpackets/SM_LOOT_ITEMLIST.java
+++ game-server/src/com/aionemu/gameserver/network/aion/serverpackets/SM_LOOT_ITEMLIST.java
@@ -1,37 +1,43 @@
 package com.aionemu.gameserver.network.aion.serverpackets;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 
 import com.aionemu.gameserver.dataholders.DataManager;
 import com.aionemu.gameserver.model.drop.Drop;
 import com.aionemu.gameserver.model.drop.DropItem;
+import com.aionemu.gameserver.model.gameobjects.DropNpc;
 import com.aionemu.gameserver.model.gameobjects.player.Player;
 import com.aionemu.gameserver.model.templates.item.ItemTemplate;
-import com.aionemu.gameserver.model.templates.item.enums.ItemGroup;
 import com.aionemu.gameserver.network.aion.AionConnection;
 import com.aionemu.gameserver.network.aion.AionServerPacket;
 
 /**
- * @author alexa026, Avol, Corrected by Metos modified by ATracer, KID
+ * @author alexa026, Avol, Metos, ATracer, KID, Sykra
  */
 public class SM_LOOT_ITEMLIST extends AionServerPacket {
 
-	private int targetObjectId;
-	private List<DropItem> dropItems;
+	private final int targetObjectId;
+	private final boolean teamMembersNearby;
+	private final List<DropItem> dropItems;
 
-	public SM_LOOT_ITEMLIST(int targetObjectId, Set<DropItem> setItems, Player player) {
-		this.targetObjectId = targetObjectId;
+	public SM_LOOT_ITEMLIST(DropNpc dropNpc, Set<DropItem> setItems, Player player) {
+		this.targetObjectId = dropNpc.getObjectId();
+		Collection<Player> playersInRange = dropNpc.getInRangePlayers();
+		this.teamMembersNearby = playersInRange.size() > 1 && playersInRange.contains(player);
 		this.dropItems = new ArrayList<>();
-		for (DropItem item : setItems) {
+		for (DropItem item : setItems)
 			if (item.canViewDropItem(player.getObjectId()))
 				dropItems.add(item);
-		}
 	}
 
 	@Override
 	protected void writeImpl(AionConnection con) {
+		Player activePlayer = con.getActivePlayer();
+		if (activePlayer == null)
+			return;
 		writeD(targetObjectId);
 		writeC(dropItems.size());
 
@@ -44,7 +50,10 @@ public class SM_LOOT_ITEMLIST extends AionServerPacket {
 			writeC(0);
 			writeC(0); // 3.5
 			ItemTemplate template = DataManager.ITEM_DATA.getItemTemplate(drop.getItemId());
-			writeC(template.getItemGroup() != ItemGroup.QUEST && !template.isTradeable() ? 1 : 0);
+			boolean showLootConfirmation = !template.isTradeable();
+			if (dropItem.isOnlyPossibleLooter(activePlayer) || !teamMembersNearby)
+				showLootConfirmation = false;
+			writeC(showLootConfirmation ? 1 : 0);
 		}
 
 	}
