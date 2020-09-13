ecba10acbc945eae9316b1fd8ee121c86c0f4cf6
Yeats <pooya_dialeh@hotmail.de>
2020-09-14 00:43:14 +0200

    Fixed #BA2500 (https://beyond-aion.com/forum/thread/2500-beschw%C3%B6rung-heilende-energie%E2%80%8B%E2%80%8B-kleriker%E2%80%8B-stigma-gibt-kein-healboost-von-der-heil/)

--- game-server/src/com/aionemu/gameserver/model/stats/container/ServantGameStats.java
+++ game-server/src/com/aionemu/gameserver/model/stats/container/ServantGameStats.java
@@ -9,11 +9,16 @@ import com.aionemu.gameserver.model.stats.calc.Stat2;
 public class ServantGameStats extends SummonedObjectGameStats {
 
 	private int fixedMBoost;
+	private int fixedHealBoost;
 
 	public ServantGameStats(Npc owner) {
 		super(owner);
 	}
 
+	@Override
+	public Stat2 getStat(StatEnum statEnum, int base) {
+		return super.getStat(statEnum, statEnum == StatEnum.HEAL_BOOST ? fixedHealBoost : base);
+	}
 
 	@Override
 	public Stat2 getMBoost() {
@@ -23,5 +28,11 @@ public class ServantGameStats extends SummonedObjectGameStats {
 	// TODO: there might be more stats which are set only at spawn
 	public void setUpStats() {
 		fixedMBoost = owner.getMaster().getGameStats().getMBoost().getBonus();
+		Stat2 healBoostStat = super.getStat(StatEnum.HEAL_BOOST, 0);
+		healBoostStat.setBonusRate(0.5f);
+		fixedHealBoost = owner.getMaster().getGameStats().getItemStatBoost(StatEnum.HEAL_BOOST, healBoostStat).getCurrent();
+		if (fixedHealBoost > 500) {
+			fixedHealBoost = 500;
+		}
 	}
 }
