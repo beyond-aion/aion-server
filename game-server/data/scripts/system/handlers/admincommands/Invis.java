74c46553c51161736485895b09567a86fd67172b
Neon <neon@beyond-aion.com>
2020-10-01 03:42:09 +0200

    fixed client prevented opening a private store after removing invis state which was set at login

--- game-server/data/scripts/system/handlers/admincommands/Invis.java
+++ game-server/data/scripts/system/handlers/admincommands/Invis.java
@@ -1,7 +1,10 @@
 package admincommands;
 
+import java.util.Collections;
+
 import com.aionemu.gameserver.model.gameobjects.player.Player;
 import com.aionemu.gameserver.model.gameobjects.state.CreatureVisualState;
+import com.aionemu.gameserver.network.aion.serverpackets.SM_ABNORMAL_STATE;
 import com.aionemu.gameserver.network.aion.serverpackets.SM_PLAYER_STATE;
 import com.aionemu.gameserver.network.aion.serverpackets.SM_SYSTEM_MESSAGE;
 import com.aionemu.gameserver.skillengine.effect.AbnormalState;
@@ -32,5 +35,7 @@ public class Invis extends AdminCommand {
 			PacketSendUtility.sendPacket(player, SM_SYSTEM_MESSAGE.STR_SKILL_EFFECT_INVISIBLE_END());
 		}
 		PacketSendUtility.broadcastPacket(player, new SM_PLAYER_STATE(player), true);
+		// required because without a skill this isn't sent automatically (outdated abnormals can cause issues when opening a private store for example)
+		PacketSendUtility.sendPacket(player, new SM_ABNORMAL_STATE(Collections.emptyList(), player.getEffectController().getAbnormals(), 0));
 	}
 }
--- game-server/data/scripts/system/handlers/consolecommands/Invisible.java
+++ game-server/data/scripts/system/handlers/consolecommands/Invisible.java
@@ -1,7 +1,10 @@
 package consolecommands;
 
+import java.util.Collections;
+
 import com.aionemu.gameserver.model.gameobjects.player.Player;
 import com.aionemu.gameserver.model.gameobjects.state.CreatureVisualState;
+import com.aionemu.gameserver.network.aion.serverpackets.SM_ABNORMAL_STATE;
 import com.aionemu.gameserver.network.aion.serverpackets.SM_PLAYER_STATE;
 import com.aionemu.gameserver.network.aion.serverpackets.SM_SYSTEM_MESSAGE;
 import com.aionemu.gameserver.skillengine.effect.AbnormalState;
@@ -27,5 +30,7 @@ public class Invisible extends ConsoleCommand {
 			PacketSendUtility.broadcastPacket(player, new SM_PLAYER_STATE(player), true);
 		}
 		PacketSendUtility.sendPacket(player, SM_SYSTEM_MESSAGE.STR_SKILL_EFFECT_INVISIBLE_BEGIN());
+		// required because without a skill this isn't sent automatically (outdated abnormals can cause issues when opening a private store for example)
+		PacketSendUtility.sendPacket(player, new SM_ABNORMAL_STATE(Collections.emptyList(), player.getEffectController().getAbnormals(), 0));
 	}
 }
