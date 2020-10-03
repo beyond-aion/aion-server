2c7581c5ff86baacac2bc93c201aeab424d33eac
Yeats <pooya_dialeh@hotmail.de>
2020-10-04 00:29:35 +0200

    Added missing XMLType and XMLEnum tags to HostileType.java

--- game-server/src/com/aionemu/gameserver/skillengine/model/HostileType.java
+++ game-server/src/com/aionemu/gameserver/skillengine/model/HostileType.java
@@ -1,5 +1,10 @@
 package com.aionemu.gameserver.skillengine.model;
 
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlType(name = "HostileType")
+@XmlEnum
 public enum HostileType {
     NONE,
     DIRECT,
