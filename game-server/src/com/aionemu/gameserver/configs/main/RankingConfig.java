ad53d37d5db562186fd35d9ad537ef224447a9bc
Neon <neon@beyond-aion.com>
2020-10-04 10:46:55 +0200

    removed hardcoded ranking configs, now that we have properties. added retail values in ranking.properties. our custom values are set in mygs.properties as usual. gp loss cap config got removed since it's superfluous now

--- game-server/src/com/aionemu/gameserver/configs/main/RankingConfig.java
+++ game-server/src/com/aionemu/gameserver/configs/main/RankingConfig.java
@@ -1,12 +1,12 @@
 package com.aionemu.gameserver.configs.main;
 
-import com.aionemu.commons.configuration.Properties;
+import java.util.Map;
+
 import org.quartz.CronExpression;
 
+import com.aionemu.commons.configuration.Properties;
 import com.aionemu.commons.configuration.Property;
 import com.aionemu.gameserver.utils.stats.AbyssRankEnum;
-
-import java.util.Map;
 
 /**
  * @author Sarynth
@@ -18,9 +18,6 @@ public class RankingConfig {
 
 	@Property(key = "gameserver.topranking.daily.gploss.time", defaultValue = "0 0 12 ? * *")
 	public static CronExpression TOP_RANKING_DAILY_GP_LOSS_TIME;
-
-	@Property(key = "gameserver.topranking.gp_loss_cap", defaultValue = "-1")
-	public static int TOP_RANKING_GP_LOSS_CAP;
 
 	@Property(key = "gameserver.topranking.small.cache", defaultValue = "false")
 	public static boolean TOP_RANKING_SMALL_CACHE;
--- game-server/src/com/aionemu/gameserver/utils/stats/AbyssRankEnum.java
+++ game-server/src/com/aionemu/gameserver/utils/stats/AbyssRankEnum.java
@@ -14,48 +14,37 @@ import com.aionemu.gameserver.utils.ChatUtil;
  */
 @XmlEnum
 public enum AbyssRankEnum {
-	GRADE9_SOLDIER(1, 300, 90, 0, 0, 0, 0),
-	GRADE8_SOLDIER(2, 345, 103, 1200, 0, 0, 0),
-	GRADE7_SOLDIER(3, 396, 118, 4220, 0, 0, 0),
-	GRADE6_SOLDIER(4, 455, 136, 10990, 0, 0, 0),
-	GRADE5_SOLDIER(5, 523, 156, 23500, 0, 0, 0),
-	GRADE4_SOLDIER(6, 601, 180, 42780, 0, 0, 0),
-	GRADE3_SOLDIER(7, 721, 216, 69700, 0, 0, 0),
-	GRADE2_SOLDIER(8, 865, 259, 105600, 0, 0, 0),
-	GRADE1_SOLDIER(9, 1038, 311, 150800, 0, 0, 0),
-	STAR1_OFFICER(10, 1557, 467, 0, 1000, 1244, 14),
-	STAR2_OFFICER(11, 1868, 560, 0, 700, 1368, 29),
-	STAR3_OFFICER(12, 2148, 644, 0, 500, 1915, 57),
-	STAR4_OFFICER(13, 2470, 741, 0, 300, 3064, 107),
-	STAR5_OFFICER(14, 3705, 1482, 0, 100, 5210, 179),
-	GENERAL(15, 4075, 1630, 0, 30, 8335, 357),
-	GREAT_GENERAL(16, 4482, 1792, 0, 10, 10002, 464),
-	COMMANDER(17, 4930, 1972, 0, 3, 11503, 571),
-	SUPREME_COMMANDER(18, 5916, 2366, 0, 1, 12437, 714);
+	GRADE9_SOLDIER(1, 300, 90, 0, 0),
+	GRADE8_SOLDIER(2, 345, 103, 1200, 0),
+	GRADE7_SOLDIER(3, 396, 118, 4220, 0),
+	GRADE6_SOLDIER(4, 455, 136, 10990, 0),
+	GRADE5_SOLDIER(5, 523, 156, 23500, 0),
+	GRADE4_SOLDIER(6, 601, 180, 42780, 0),
+	GRADE3_SOLDIER(7, 721, 216, 69700, 0),
+	GRADE2_SOLDIER(8, 865, 259, 105600, 0),
+	GRADE1_SOLDIER(9, 1038, 311, 150800, 0),
+	STAR1_OFFICER(10, 1557, 467, 0, 1244),
+	STAR2_OFFICER(11, 1868, 560, 0, 1368),
+	STAR3_OFFICER(12, 2148, 644, 0, 1915),
+	STAR4_OFFICER(13, 2470, 741, 0, 3064),
+	STAR5_OFFICER(14, 3705, 1482, 0, 5210),
+	GENERAL(15, 4075, 1630, 0, 8335),
+	GREAT_GENERAL(16, 4482, 1792, 0, 10002),
+	COMMANDER(17, 4930, 1972, 0, 11503),
+	SUPREME_COMMANDER(18, 5916, 2366, 0, 12437);
 
 	private final int id;
 	private final int pointsGained;
 	private final int pointsLost;
 	private final int requiredAP;
-	private final int quota;
 	private final int requiredGP;
-	private final int gpLossPerDay;
 
-	/**
-	 * @param id
-	 * @param pointsGained
-	 * @param pointsLost
-	 * @param required
-	 * @param quota
-	 */
-	private AbyssRankEnum(int id, int pointsGained, int pointsLost, int required, int quota, int gloryPointsRequired, int gpLossPerDay) {
+	AbyssRankEnum(int id, int pointsGained, int pointsLost, int required, int gloryPointsRequired) {
 		this.id = id;
 		this.pointsGained = pointsGained;
 		this.pointsLost = pointsLost;
 		this.requiredAP = required;
-		this.quota = quota;
 		this.requiredGP = gloryPointsRequired;
-		this.gpLossPerDay = gpLossPerDay;
 	}
 
 	/**
@@ -91,16 +80,14 @@ public enum AbyssRankEnum {
 	}
 
 	public int getGpLossPerDay() {
-		int gpLossCap = RankingConfig.TOP_RANKING_GP_LOSS_CAP;
-		int dailyGpLoss = RankingConfig.TOP_RANKING_GP_LOSS.getOrDefault(this, gpLossPerDay);
-		return gpLossCap > -1 && dailyGpLoss > gpLossCap ? gpLossCap : dailyGpLoss;
+		return RankingConfig.TOP_RANKING_GP_LOSS.getOrDefault(this, 0);
 	}
 
 	/**
 	 * @return The quota is the maximum number of allowed player to have the rank
 	 */
 	public int getQuota() {
-		return RankingConfig.TOP_RANKING_QUOTA.getOrDefault(this, quota);
+		return RankingConfig.TOP_RANKING_QUOTA.getOrDefault(this, 0);
 	}
 
 	public static String getRankL10n(Player player) {
