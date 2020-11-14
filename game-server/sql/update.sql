57886e057bf0a1cb77bdc6c696054102c2befa50
Farlon <farlon@beyond-aion.com>
2020-11-14 09:22:14 +0100

    Advent calendar 2020 (this reverts commit cce6fc87)

--- game-server/sql/update.sql
+++ game-server/sql/update.sql
@@ -1,18 +1,10 @@
 /*
- * DB changes since 02a4e09 (13.11.2020)
+ * DB changes since c979c007 (14.11.2020)
  */
 
-ALTER TABLE `abyss_rank`
-    CHANGE COLUMN `rank` `rank` TINYINT NOT NULL DEFAULT 1 AFTER `ap`,
-    CHANGE COLUMN `max_rank` `max_rank` TINYINT NOT NULL DEFAULT 1 AFTER `rank`,
-    CHANGE COLUMN `rank_pos` `rank_pos` SMALLINT NOT NULL DEFAULT 0 AFTER `max_rank`,
-    CHANGE COLUMN `old_rank_pos` `old_rank_pos` SMALLINT NOT NULL DEFAULT 0 AFTER `rank_pos`,
-    DROP COLUMN `top_ranking`,
-    ADD INDEX `rank` (`rank`),
-    ADD INDEX `rank_pos` (`rank_pos`),
-    ADD INDEX `gp` (`gp`);
-
-ALTER TABLE `legions`
-    CHANGE COLUMN `rank_pos` `rank_pos` SMALLINT NOT NULL DEFAULT 0 AFTER `disband_time`,
-    CHANGE COLUMN `old_rank_pos` `old_rank_pos` SMALLINT NOT NULL DEFAULT 0 AFTER `rank_pos`,
-    ADD INDEX `rank_pos` (`rank_pos`);
+-- create advent calendar table
+CREATE TABLE `advent` (
+  `account_id` int(11) NOT NULL,
+  `last_day_received` tinyint(4) NOT NULL,
+  PRIMARY KEY (`account_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
