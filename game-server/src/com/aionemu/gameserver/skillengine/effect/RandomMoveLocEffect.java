88ecdd5b4b8f378c20ed3e1305fd17728954e40d
Yeats <pooya_dialeh@hotmail.de>
2021-02-17 18:17:39 +0100

    Fixed some skills with RandomMoveLocEffect did not output any messages because SkillMoveType was set to DODE -> changed to SkillMoveType.MOVEBEHIND. (partial fix for Survival Instinct(2106) #BA3105 https://beyond-aion.com/forum/thread/3105-gunner-skills-die-keine-chat-anzeige-haben/)

--- game-server/src/com/aionemu/gameserver/skillengine/effect/RandomMoveLocEffect.java
+++ game-server/src/com/aionemu/gameserver/skillengine/effect/RandomMoveLocEffect.java
@@ -39,15 +39,14 @@ public class RandomMoveLocEffect extends EffectTemplate {
 	public void calculate(Effect effect) {
 		effect.addSuccessEffect(this);
 		DashStatus ds = reserved5 == 1 ? DashStatus.RANDOMMOVELOC_NEW : DashStatus.RANDOMMOVELOC;
-		SkillMoveType mt = direction == 1 ? SkillMoveType.MOVEBEHIND : SkillMoveType.DODGE;
-		effect.setSkillMoveType(mt);
+		effect.setSkillMoveType(SkillMoveType.MOVEBEHIND);
 		effect.setDashStatus(ds);
 
 		Creature effector = effect.getEffector();
 		// Move Effector backwards direction=1 or frontwards direction=0
 		if (distance != 0) {
 			float dir = PositionUtil.convertHeadingToAngle(effector.getHeading());
-			Vector3f closestCollision = GeoService.getInstance().findMovementCollision(effector, mt == SkillMoveType.MOVEBEHIND ? dir + 180 : dir, distance);
+			Vector3f closestCollision = GeoService.getInstance().findMovementCollision(effector, direction == 1 ? dir + 180 : dir, distance);
 			effect.getSkill().setTargetPosition(closestCollision.getX(), closestCollision.getY(), closestCollision.getZ(), effector.getHeading());
 		} else
 			effect.getSkill().setTargetPosition(effector.getX(), effector.getY(), effector.getZ(), effector.getHeading());
