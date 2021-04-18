fffdd035ea73580e19ad9f96a018d311a16f2f43
Sykra <sykraaion@gmail.com>
2021-04-18 11:04:52 +0200

    fixed format fail in PlayerStatCalculator

--- game-server/src/com/aionemu/gameserver/model/stats/calc/PlayerStatCalculator.java
+++ game-server/src/com/aionemu/gameserver/model/stats/calc/PlayerStatCalculator.java
@@ -14,26 +14,26 @@ public class PlayerStatCalculator {
 		return (int) (base + level * mod1 + level * level * mod2);
 	}
 
-		public static int calculateMaxMp(PlayerClass playerClass, int level) {
-			float base = playerClass.getWillMultiplier() * 0.35f;
-			float mod1 = level * base / 2f;
-			float mod2 = level * level * playerClass.getWillMultiplier() * 0.125f / 10000;
-    return (int) (base + mod1 + mod2);
-  }
+	public static int calculateMaxMp(PlayerClass playerClass, int level) {
+		float base = playerClass.getWillMultiplier() * 0.35f;
+		float mod1 = level * base / 2f;
+		float mod2 = level * level * playerClass.getWillMultiplier() * 0.125f / 10000;
+		return (int) (base + mod1 + mod2);
+	}
 
-		public static int calculateBlockEvasionOrParry(int level) {
-			return (int) (62 + 12.4f * level);
-		}
+	public static int calculateBlockEvasionOrParry(int level) {
+		return (int) (62 + 12.4f * level);
+	}
 
-		public static int calculateMagicalAccuracy(int level) {
-			return (int) (14.26f * level);
-		}
+	public static int calculateMagicalAccuracy(int level) {
+		return (int) (14.26f * level);
+	}
 
-		public static int calculatePhysicalAccuracy(int level) {
-			return 190 + 8 * level;
-		}
+	public static int calculatePhysicalAccuracy(int level) {
+		return 190 + 8 * level;
+	}
 
-		public static int calculateStrikeResist(int level) {
-			return level > 50 ? 6 * (level - 50) : 0;
-		}
+	public static int calculateStrikeResist(int level) {
+		return level > 50 ? 6 * (level - 50) : 0;
+	}
 }
