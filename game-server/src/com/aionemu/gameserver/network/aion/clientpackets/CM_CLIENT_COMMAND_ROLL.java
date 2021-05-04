d1cc3aaf21c9be6c868d50e792e840b0c6e9c9cc
Neon <neon@beyond-aion.com>
2021-05-04 04:02:37 +0200

    fixed invalid Rnd.get parameters from `/roll` command and added some info in comments

--- game-server/src/com/aionemu/gameserver/network/aion/clientpackets/CM_CLIENT_COMMAND_ROLL.java
+++ game-server/src/com/aionemu/gameserver/network/aion/clientpackets/CM_CLIENT_COMMAND_ROLL.java
@@ -10,12 +10,13 @@ import com.aionemu.gameserver.network.aion.serverpackets.SM_SYSTEM_MESSAGE;
 import com.aionemu.gameserver.utils.PacketSendUtility;
 
 /**
+ * Received when a player types /roll in chat. {@link #maxRoll} is an optional numeric parameter which defaults to 100 on client side.
+ * 
  * @author Rhys2002
  */
 public class CM_CLIENT_COMMAND_ROLL extends AionClientPacket {
 
 	private int maxRoll;
-	private int roll;
 
 	public CM_CLIENT_COMMAND_ROLL(int opcode, Set<State> validStates) {
 		super(opcode, validStates);
@@ -29,8 +30,9 @@ public class CM_CLIENT_COMMAND_ROLL extends AionClientPacket {
 	@Override
 	protected void runImpl() {
 		Player player = getConnection().getActivePlayer();
-
-		roll = Rnd.get(1, maxRoll);
+		if (maxRoll <= 0) // client sends 100 on /roll 0 but negative numbers are passed through for whatever reason
+			maxRoll = 100;
+		int roll = Rnd.get(1, maxRoll);
 		PacketSendUtility.sendPacket(player, SM_SYSTEM_MESSAGE.STR_MSG_DICE_CUSTOM_ME(roll, maxRoll));
 		PacketSendUtility.broadcastPacket(player, SM_SYSTEM_MESSAGE.STR_MSG_DICE_CUSTOM_OTHER(player.getName(), roll, maxRoll));
 	}
