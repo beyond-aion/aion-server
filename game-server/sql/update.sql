cd812b61ffc528e73f58f9634cd3fc9143b0cbb5
Farlon <farlon@beyond-aion.com>
2021-07-04 20:05:04 +0200

    SQL DELETE for old event items

--- game-server/sql/update.sql
+++ game-server/sql/update.sql
@@ -1,6 +1,6 @@
 /*
- * DB changes since db3cab66 (18.04.2021)
+ * DB changes since a5bfa744 (03.07.2021)
  */
 
 -- remove old event items
-DELETE FROM inventory WHERE item_id IN (185000221, 185000222);
+DELETE FROM inventory WHERE item_id IN (186000111, 188051090, 188051091, 188051092, 188051093, 188052625, 188052627, 188100124, 188100125);
