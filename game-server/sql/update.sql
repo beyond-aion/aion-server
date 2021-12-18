0c9bb1daef273c3b0f02f22fad98fb968d2b2130
Farlon <farlon@beyond-aion.com>
2021-12-18 03:11:05 +0100

    SQL DELETE for old event items

--- game-server/sql/update.sql
+++ game-server/sql/update.sql
@@ -1,10 +1,6 @@
 /*
- * DB changes since ec7c95de (14.11.2021)
+ * DB changes since 993bf90f (27.11.2021)
  */
 
--- create advent calendar table
-CREATE TABLE `advent` (
-  `account_id` int(11) NOT NULL,
-  `last_day_received` tinyint(4) NOT NULL,
-  PRIMARY KEY (`account_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- remove old event items
+DELETE FROM inventory WHERE item_id IN (188052986, 188052987, 188052988, 188052989, 188052990, 188052991, 188052992, 188052993, 188052994, 188052995, 188052996, 188052997, 188052998, 188052999, 188100148, 188100149, 182007148);
