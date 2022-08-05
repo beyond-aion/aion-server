e5a5460409cf2213819cc7202bdfde221c9c4e70
Neon <neon@beyond-aion.com>
2022-08-05 21:18:05 +0200

    Some more localized chat messages

--- game-server/data/handlers/admincommands/Invul.java
+++ game-server/data/handlers/admincommands/Invul.java
@@ -2,12 +2,12 @@ package admincommands;
 
 import com.aionemu.gameserver.model.gameobjects.player.CustomPlayerState;
 import com.aionemu.gameserver.model.gameobjects.player.Player;
+import com.aionemu.gameserver.utils.ChatUtil;
 import com.aionemu.gameserver.utils.PacketSendUtility;
 import com.aionemu.gameserver.utils.chathandlers.AdminCommand;
 
 /**
- * @author Andy
- * @author Divinity - update
+ * @author Andy, Divinity
  */
 public class Invul extends AdminCommand {
 
@@ -22,7 +22,7 @@ public class Invul extends AdminCommand {
 			PacketSendUtility.sendMessage(player, "You are now mortal.");
 		} else {
 			player.setCustomState(CustomPlayerState.INVULNERABLE);
-			PacketSendUtility.sendMessage(player, "You are now immortal.");
+			sendInfo(player, ChatUtil.l10n(293440)); // Immune to all damage.
 		}
 	}
 }
--- game-server/data/handlers/admincommands/See.java
+++ game-server/data/handlers/admincommands/See.java
@@ -3,6 +3,7 @@ package admincommands;
 import com.aionemu.gameserver.model.gameobjects.player.Player;
 import com.aionemu.gameserver.model.gameobjects.state.CreatureSeeState;
 import com.aionemu.gameserver.network.aion.serverpackets.SM_PLAYER_STATE;
+import com.aionemu.gameserver.utils.ChatUtil;
 import com.aionemu.gameserver.utils.PacketSendUtility;
 import com.aionemu.gameserver.utils.chathandlers.AdminCommand;
 
@@ -19,7 +20,7 @@ public class See extends AdminCommand {
 	public void execute(Player admin, String... params) {
 		if (admin.getSeeState() < 2) {
 			admin.setSeeState(CreatureSeeState.SEARCH20);
-			sendInfo(admin, "You got vision.");
+			sendInfo(admin, ChatUtil.l10n(288645)); // Can see targets in advanced hide states.
 		} else {
 			admin.unsetSeeState(CreatureSeeState.SEARCH20);
 			sendInfo(admin, "You lost vision.");
