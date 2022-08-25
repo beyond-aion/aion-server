e76390998ee6264f4583c80a787f51c0a4ec33e7
Estrayl <galawaynkrueger@yahoo.de>
2022-08-25 21:49:13 +0200

    Fixed #BA3500 faulty id/item https://beyond-aion.com/forum/thread/3500-belohnungstruhe-der-gro%C3%9Finvasion-stufe-3/

--- game-server/sql/update.sql
+++ game-server/sql/update.sql
@@ -1,16 +1,6 @@
 /*
- * DB changes since d1b22999 (02.07.2022)
+ * DB changes since d1b22999 (25.08.2022)
  */
 
--- drop advent calendar table
-drop table advent;
-
--- add 2 new columns for custom instance
-ALTER TABLE `custom_instance`
-ADD `max_rank` int(11) NOT NULL,
-ADD `dps` int(11) NOT NULL;
-
-UPDATE `custom_instance` SET `max_rank` = `rank`;
-
--- remove old event items
-DELETE FROM inventory WHERE item_id IN (186000111, 188051090, 188051091, 188051092, 188051093, 188051676, 188052625, 188052626, 188052627, 188100057, 188100058, 188100059, 188100060, 188100124, 188100125);
+-- replace incorrect items
+UPDATE inventory SET item_id=188053183 AND item_skin=188053183 WHERE item_id=188053179;
