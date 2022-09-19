f743ba78e9732eeb379d4fb1803d072a0346c4f3
Estrayl <galawaynkrueger@yahoo.de>
2022-09-19 22:21:54 +0200

    Fixed #BA3514 Assassins could not use hide if they got aggro but did not get damaged. Removed SM_ATTACK packet on aggro event. I didn't find any bytes or case in SM_ATTACK where this would not happen. So, removing the packet does not change anything it seems obsolete.
    https://beyond-aion.com/forum/thread/3514-kein-hide-m%C3%B6glich-sobald-man-aggro-zieht-als-assa/

--- game-server/src/com/aionemu/gameserver/ai/handler/AggroEventHandler.java
+++ game-server/src/com/aionemu/gameserver/ai/handler/AggroEventHandler.java
@@ -1,24 +1,15 @@
 package com.aionemu.gameserver.ai.handler;
 
-import java.util.Collections;
-import java.util.function.Consumer;
-
 import com.aionemu.gameserver.ai.NpcAI;
 import com.aionemu.gameserver.ai.event.AIEventType;
-import com.aionemu.gameserver.controllers.attack.AttackResult;
-import com.aionemu.gameserver.controllers.attack.AttackStatus;
 import com.aionemu.gameserver.model.TribeClass;
-import com.aionemu.gameserver.model.animations.AttackHandAnimation;
-import com.aionemu.gameserver.model.animations.AttackTypeAnimation;
 import com.aionemu.gameserver.model.gameobjects.Creature;
 import com.aionemu.gameserver.model.gameobjects.Npc;
 import com.aionemu.gameserver.model.gameobjects.VisibleObject;
 import com.aionemu.gameserver.model.gameobjects.player.CustomPlayerState;
 import com.aionemu.gameserver.model.gameobjects.player.Player;
 import com.aionemu.gameserver.model.templates.npc.NpcTemplateType;
-import com.aionemu.gameserver.network.aion.serverpackets.SM_ATTACK;
 import com.aionemu.gameserver.services.TribeRelationService;
-import com.aionemu.gameserver.utils.PacketSendUtility;
 import com.aionemu.gameserver.utils.PositionUtil;
 import com.aionemu.gameserver.utils.ThreadPoolManager;
 import com.aionemu.gameserver.world.geo.GeoService;
@@ -29,10 +20,7 @@ import com.aionemu.gameserver.world.geo.GeoService;
 public class AggroEventHandler {
 
 	private static final int SUPPORT_RANGE_OFFSET = 2; // might be <pushed_range> in client data
-	/**
-	 * @param npcAI
-	 * @param myTarget
-	 */
+
 	public static void onAggro(NpcAI npcAI, final Creature myTarget) {
 		final Npc owner = npcAI.getOwner();
 		// TODO move out?
@@ -41,22 +29,17 @@ public class AggroEventHandler {
 				return;
 		} else if (TribeRelationService.isFriend(owner, myTarget) || myTarget.isFlag())
 			return;
-		PacketSendUtility.broadcastPacket(owner,
-			new SM_ATTACK(owner, myTarget, 0, 633, AttackTypeAnimation.MELEE, AttackHandAnimation.MAIN_HAND, Collections.singletonList(new AttackResult(0, AttackStatus.NORMALHIT))));
-
 		ThreadPoolManager.getInstance().schedule(new AggroNotifier(owner, myTarget, true), 500);
 		owner.getPosition().getWorldMapInstance().getInstanceHandler().onAggro(owner);
 	}
 
 	public static boolean onCreatureNeedsSupport(NpcAI npcAI, Creature creatureAskingForSupport) {
 		Npc owner = npcAI.getOwner();
-		if (TribeRelationService.canHelpCreature(creatureAskingForSupport, owner) && PositionUtil.isInRange(owner, creatureAskingForSupport, owner.getAggroRange() + SUPPORT_RANGE_OFFSET, false)
+		if (TribeRelationService.canHelpCreature(creatureAskingForSupport, owner)
+			&& PositionUtil.isInRange(owner, creatureAskingForSupport, owner.getAggroRange() + SUPPORT_RANGE_OFFSET, false)
 			&& GeoService.getInstance().canSee(owner, creatureAskingForSupport)) {
 			VisibleObject attacker = creatureAskingForSupport.getTarget();
-			if (attacker instanceof Creature) {
-				Creature targetCreature = (Creature) attacker;
-				PacketSendUtility.broadcastPacket(owner,
-					new SM_ATTACK(owner, targetCreature, 0, 633, AttackTypeAnimation.MELEE, AttackHandAnimation.MAIN_HAND, Collections.singletonList(new AttackResult(0, AttackStatus.NORMALHIT))));
+			if (attacker instanceof Creature targetCreature) {
 				ThreadPoolManager.getInstance().schedule(new AggroNotifier(owner, targetCreature, false), 500);
 				return true;
 			}
@@ -71,9 +54,9 @@ public class AggroEventHandler {
 			return false;
 		}
 		VisibleObject target = attacker.getTarget();
-		if (target instanceof Player) {
-			Player playerTarget = (Player) target;
-			if (!owner.isEnemy(playerTarget) && owner.isEnemy(attacker) && PositionUtil.isInRange(owner, playerTarget, owner.getAggroRange() + SUPPORT_RANGE_OFFSET, false)
+		if (target instanceof Player playerTarget) {
+			if (!owner.isEnemy(playerTarget) && owner.isEnemy(attacker)
+				&& PositionUtil.isInRange(owner, playerTarget, owner.getAggroRange() + SUPPORT_RANGE_OFFSET, false)
 				&& GeoService.getInstance().canSee(owner, attacker)) {
 				owner.getAggroList().addHate(attacker, 1);
 				return true;
@@ -84,9 +67,9 @@ public class AggroEventHandler {
 
 	private static final class AggroNotifier implements Runnable {
 
+		private final boolean broadcast;
 		private Npc aggressive;
 		private Creature target;
-		private boolean broadcast;
 
 		AggroNotifier(Npc aggressive, Creature target, boolean broadcast) {
 			this.aggressive = aggressive;
@@ -97,15 +80,9 @@ public class AggroEventHandler {
 		@Override
 		public void run() {
 			aggressive.getAggroList().addHate(target, 1);
-			if (broadcast) {
-				aggressive.getKnownList().forEachNpc(new Consumer<Npc>() {
+			if (broadcast)
+				aggressive.getKnownList().forEachNpc(object -> object.getAi().onCreatureEvent(AIEventType.CREATURE_NEEDS_SUPPORT, aggressive));
 
-					@Override
-					public void accept(Npc object) {
-						object.getAi().onCreatureEvent(AIEventType.CREATURE_NEEDS_SUPPORT, aggressive);
-					}
-				});
-			}
 			aggressive = null;
 			target = null;
 		}
