581e6db6455f2c50af473b130301f3b217e3f0a1
Estrayl <galawaynkrueger@yahoo.de>
2022-11-15 00:01:27 +0100

    Fake cake will now despawn, so it behaves more like a honeypot.

--- game-server/data/handlers/ai/events/FakeCakeAI.java
+++ game-server/data/handlers/ai/events/FakeCakeAI.java
@@ -1,11 +1,8 @@
 package ai.events;
 
 import com.aionemu.gameserver.ai.AIName;
-import com.aionemu.gameserver.model.ChatType;
 import com.aionemu.gameserver.model.gameobjects.Npc;
 import com.aionemu.gameserver.model.gameobjects.player.Player;
-import com.aionemu.gameserver.utils.PacketSendUtility;
-import com.aionemu.gameserver.utils.ThreadPoolManager;
 import com.aionemu.gameserver.utils.audit.AuditLogger;
 
 import ai.ChestAI;
@@ -27,7 +24,6 @@ public class FakeCakeAI extends ChestAI {
 
 	private void punishment(Player player) {
 		AuditLogger.log(player, String.format("%s used the fake cake at %s.", player, player.getPosition()));
-		player.getController().onAttack(getOwner(), player.getLifeStats().getMaxHp(), null);
-		ThreadPoolManager.getInstance().schedule(() -> PacketSendUtility.sendMessage(player, "WASTED!", ChatType.BRIGHT_YELLOW_CENTER), 6000);
+		getOwner().getController().die();
 	}
 }
