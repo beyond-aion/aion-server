10acee0fb62dd1269da297639a8e253399a31223
Farlon <farlon@beyond-aion.com>
2023-02-10 17:40:10 +0100

    Valentine's Day Event

--- game-server/sql/update.sql
+++ game-server/sql/update.sql
@@ -1,6 +1,9 @@
 /*
- * DB changes since f45d8b5c (12.01.2023)
+ * DB changes since 4bcc2bbf (12.01.2023)
  */
 
 -- drop advent calendar table
 drop table advent;
+
+-- remove old event items
+DELETE FROM inventory WHERE item_id IN (188052318, 188100091, 188100092, 188100093, 188100094);
