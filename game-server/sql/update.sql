5df24625db7c493b3df89d392c4293e860062aa9
Farlon <farlon@beyond-aion.com>
2023-08-06 05:41:28 +0200

    SQL DELETE for old event items

--- game-server/sql/update.sql
+++ game-server/sql/update.sql
@@ -1,9 +1,9 @@
 /*
- * DB changes since bf5cd5b0 (12.05.2023)
+ * DB changes since 2947cbf0 (05.08.2023)
  */
 
 -- drop advent calendar table
 drop table advent;
 
 -- remove old event items
-DELETE FROM inventory WHERE item_id IN (185000221, 185000222, 188600311);
+DELETE FROM inventory WHERE item_id IN (186000111, 188051090, 188051091, 188052625, 188052626, 188052627, 188100124, 188100125);
