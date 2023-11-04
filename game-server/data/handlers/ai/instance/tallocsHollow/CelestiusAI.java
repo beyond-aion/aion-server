0061070e6dc7d58dda4e259e6c614170c889e90c
Estrayl <galawaynkrueger@yahoo.de>
2023-11-04 01:56:14 +0100

    #BA3464 Possibly fixed spam spawns at Taloc's Hollow endboss
    https://beyond-aion.com/forum/thread/3464-taloc-s-hollow/

--- game-server/data/handlers/ai/instance/tallocsHollow/CelestiusAI.java
+++ game-server/data/handlers/ai/instance/tallocsHollow/CelestiusAI.java
@@ -1,6 +1,7 @@
 package ai.instance.tallocsHollow;
 
 import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import com.aionemu.gameserver.ai.AIName;
 import com.aionemu.gameserver.ai.NpcAI;
@@ -8,6 +9,8 @@ import com.aionemu.gameserver.ai.manager.WalkManager;
 import com.aionemu.gameserver.model.EmotionType;
 import com.aionemu.gameserver.model.gameobjects.Creature;
 import com.aionemu.gameserver.model.gameobjects.Npc;
+import com.aionemu.gameserver.model.gameobjects.Summon;
+import com.aionemu.gameserver.model.gameobjects.player.Player;
 import com.aionemu.gameserver.model.gameobjects.state.CreatureState;
 import com.aionemu.gameserver.network.aion.serverpackets.SM_EMOTION;
 import com.aionemu.gameserver.skillengine.SkillEngine;
@@ -22,6 +25,8 @@ import ai.AggressiveNpcAI;
 @AIName("celestius")
 public class CelestiusAI extends AggressiveNpcAI {
 
+	private static final int SUMMONS_ID = 281514;
+	private final AtomicBoolean isSpawnTaskStarted = new AtomicBoolean();
 	private Future<?> helpersTask;
 
 	public CelestiusAI(Npc owner) {
@@ -31,61 +36,58 @@ public class CelestiusAI extends AggressiveNpcAI {
 	@Override
 	protected void handleAttack(Creature creature) {
 		super.handleAttack(creature);
-		if (helpersTask == null)
+		if ((creature instanceof Player || creature instanceof Summon) && isSpawnTaskStarted.compareAndSet(false, true))
 			startHelpersCall();
 	}
 
-	private synchronized void cancelHelpersTask() {
-		if (helpersTask != null && !helpersTask.isDone()) {
-			helpersTask.cancel(true);
-			helpersTask = null;
-		}
-	}
-
-	private synchronized void startHelpersCall() {
-		if (helpersTask != null)
-			return;
+	private void startHelpersCall() {
 		helpersTask = ThreadPoolManager.getInstance().scheduleAtFixedRate(() -> {
 			if (!isDead()) {
 				SkillEngine.getInstance().getSkill(getOwner(), 18981, 44, getOwner()).useNoAnimationSkill();
-				startRun((Npc) spawn(281514, 518, 813, 1378, (byte) 0), "3001900001");
-				startRun((Npc) spawn(281514, 551, 795, 1376, (byte) 0), "3001900002");
-				startRun((Npc) spawn(281514, 574, 854, 1375, (byte) 0), "3001900003");
+				startWalker((Npc) spawn(SUMMONS_ID, 518, 813, 1378, (byte) 0), "3001900001");
+				startWalker((Npc) spawn(SUMMONS_ID, 551, 795, 1376, (byte) 0), "3001900002");
+				startWalker((Npc) spawn(SUMMONS_ID, 574, 854, 1375, (byte) 0), "3001900003");
 			}
 		}, 1000, 25000);
 	}
 
-	private void startRun(Npc npc, String walkId) {
+	private void startWalker(Npc npc, String walkId) {
 		npc.getSpawn().setWalkerId(walkId);
 		WalkManager.startWalking((NpcAI) npc.getAi());
 		npc.setState(CreatureState.ACTIVE, true);
 		PacketSendUtility.broadcastPacket(npc, new SM_EMOTION(npc, EmotionType.CHANGE_SPEED, 0, npc.getObjectId()));
 	}
 
-	private void deleteHelpers() {
-		for (Npc npc : getPosition().getWorldMapInstance().getNpcs(281514)) {
-			npc.getController().delete();
+	private void cleanUp() {
+		cancelTask();
+		deleteSummons();
+	}
+
+	private void cancelTask() {
+		if (helpersTask != null && !helpersTask.isDone()) {
+			helpersTask.cancel(true);
 		}
+	}
+
+	private void deleteSummons() {
+		getPosition().getWorldMapInstance().getNpcs(SUMMONS_ID).forEach(npc -> npc.getController().delete());
 	}
 
 	@Override
 	protected void handleBackHome() {
-		cancelHelpersTask();
-		deleteHelpers();
+		cleanUp();
 		super.handleBackHome();
 	}
 
 	@Override
 	protected void handleDespawned() {
-		cancelHelpersTask();
-		deleteHelpers();
+		cleanUp();
 		super.handleDespawned();
 	}
 
 	@Override
 	protected void handleDied() {
-		cancelHelpersTask();
-		deleteHelpers();
+		cleanUp();
 		super.handleDied();
 	}
 
