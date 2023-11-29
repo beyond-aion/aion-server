25f1a3fad0cd0f93891a263938f81ee83aca7e8a
Farlon <farlon@beyond-aion.com>
2023-11-29 03:34:38 +0100

    Advent calendar 2023 (this reverts commit d0bbac56)

--- game-server/sql/update.sql
+++ game-server/sql/update.sql
@@ -1,9 +1,16 @@
 /*
- * DB changes since 2947cbf0 (05.08.2023)
+ * DB changes since a70a0380 (24.11.2023)
  */
 
 -- drop advent calendar table
 drop table advent;
 
+-- create advent calendar table
+CREATE TABLE `advent` (
+  `account_id` int(11) NOT NULL,
+  `last_day_received` tinyint(4) NOT NULL,
+  PRIMARY KEY (`account_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 -- remove old event items
-DELETE FROM inventory WHERE item_id IN (186000111, 188051090, 188051091, 188052625, 188052626, 188052627, 188100124, 188100125);
+DELETE FROM inventory WHERE item_id IN (182007148, 186000177, 186000381, 188052986, 188052987, 188052988, 188052989, 188052990, 188052991, 188052992, 188052993, 188052994, 188052995, 188052996, 188052997, 188052998, 188052999, 188100148, 188100149);
