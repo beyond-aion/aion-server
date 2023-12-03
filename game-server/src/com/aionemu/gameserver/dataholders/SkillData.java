beyond-aion/aion-server

commit 44c4bd79a610ff2644eed0a92bdc86275f0bf9c9

fixed skill learn msg spam/incorrect level values and some other stuff

game-server/src/com/aionemu/gameserver/dataholders/SkillData.java
@@ -35,7 +35,10 @@ public class SkillData {
 	private TIntObjectHashMap<SkillTemplate> skillData = new TIntObjectHashMap<>();
 
 	@XmlTransient
-	private Map<String, List<SkillTemplate>> skillTemplatesByGroups = new FastMap<>();
+	private Map<String, List<SkillTemplate>> skillTemplatesByGroup = new FastMap<>();
+
+	@XmlTransient
+	private Map<String, List<SkillTemplate>> skillTemplatesByStack = new FastMap<>();
 
 	/**
 	 * Map that contains cooldownId - skillId List
@@ -45,7 +48,8 @@ public class SkillData {
 
 	void afterUnmarshal(Unmarshaller u, Object parent) {
 		skillData.clear();
-		skillTemplatesByGroups.clear();
+		skillTemplatesByGroup.clear();
+		skillTemplatesByStack.clear();
 		cooldownGroups.clear();
 		for (SkillTemplate skillTemplate : skillTemplates) {
 			int skillId = skillTemplate.getSkillId();
@@ -56,9 +60,14 @@ public class SkillData {
 			cooldownGroups.get(cooldownId).add(skillId);
 
 			if (skillTemplate.getGroup() != null) {
-				if (!skillTemplatesByGroups.containsKey(skillTemplate.getGroup()))
-					skillTemplatesByGroups.put(skillTemplate.getGroup(), new FastTable<>());
-				skillTemplatesByGroups.get(skillTemplate.getGroup()).add(skillTemplate);
+				if (!skillTemplatesByGroup.containsKey(skillTemplate.getGroup()))
+					skillTemplatesByGroup.put(skillTemplate.getGroup(), new FastTable<>());
+				skillTemplatesByGroup.get(skillTemplate.getGroup()).add(skillTemplate);
+			}
+			if (skillTemplate.getStack() != null) {
+				if (!skillTemplatesByStack.containsKey(skillTemplate.getGroup()))
+					skillTemplatesByStack.put(skillTemplate.getGroup(), new FastTable<>());
+				skillTemplatesByStack.get(skillTemplate.getGroup()).add(skillTemplate);
 			}
 		}
 	}
@@ -71,8 +80,22 @@ public class SkillData {
 		return skillData.get(skillId);
 	}
 
-	public List<SkillTemplate> getSkillTemplate(String skillGroup) {
-		return skillTemplatesByGroups.get(skillGroup);
+	/**
+	 * @param skillGroup
+	 * @return All skill templates of this group. A group is less precise and may be null.
+	 */
+	public List<SkillTemplate> getSkillTemplatesByGroup(String skillGroup) {
+		return skillTemplatesByGroup.get(skillGroup);
+	}
+
+	/**
+	 * @param skillStack
+	 * @return All skill templates of this stack. A stack is more precise than a group.
+	 *         For example: Charge skills have their own stack per charging level. Also, skills that are similar for both factions have the same group,
+	 *         but different stacks.
+	 */
+	public List<SkillTemplate> getSkillTemplatesByStack(String skillStack) {
+		return skillTemplatesByStack.get(skillStack);
 	}
 
 	/**

game-server/src/com/aionemu/gameserver/dataholders/SkillTreeData.java
@@ -15,6 +15,7 @@ import javolution.util.FastTable;
 import com.aionemu.gameserver.model.PlayerClass;
 import com.aionemu.gameserver.model.Race;
 import com.aionemu.gameserver.skillengine.model.SkillLearnTemplate;
+import com.aionemu.gameserver.skillengine.model.SkillTemplate;
 
 /**
  * @author ATracer
@@ -69,7 +70,7 @@ public class SkillTreeData {
 	 * @param race
 	 * @return SkillLearnTemplate[]
 	 */
-	public SkillLearnTemplate[] getTemplatesFor(PlayerClass playerClass, int level, Race race) {
+	public List<SkillLearnTemplate> getTemplatesFor(PlayerClass playerClass, int level, Race race) {
 		List<SkillLearnTemplate> newSkills = new FastTable<>();
 
 		List<SkillLearnTemplate> classRaceSpecificTemplates = templates.get(makeHash(playerClass.ordinal(), race.ordinal(), level));
@@ -86,79 +87,86 @@ public class SkillTreeData {
 		if (generalTemplates != null)
 			newSkills.addAll(generalTemplates);
 
-		return newSkills.toArray(new SkillLearnTemplate[newSkills.size()]);
+		return newSkills;
 	}
 
-	public SkillLearnTemplate[] getTemplatesForSkill(int skillId) {
-		List<SkillLearnTemplate> searchSkills = new FastTable<>();
-
-		List<SkillLearnTemplate> byId = templatesById.get(skillId);
-		if (byId != null)
-			searchSkills.addAll(byId);
-
-		return searchSkills.toArray(new SkillLearnTemplate[searchSkills.size()]);
-	}
-	
-	public SkillLearnTemplate[] getStigmaSkillsForSkill(int skillId, PlayerClass playerClass, int level, Race race) {
-		List<SkillLearnTemplate> addList = new FastTable<>();
-		try {
-			List<SkillLearnTemplate> byId = templatesById.get(skillId);
-			if (!playerClass.isStartingClass()) {
-				SkillLearnTemplate topTemplate = null;
-				for (SkillLearnTemplate template : byId) {
-					if (!template.isStigma())
-						continue;
-					if (template.getRace() != race && template.getRace() != Race.PC_ALL)
-						continue;
-					if (template.getClassId() != playerClass && template.getClassId() != PlayerClass.ALL)
-						continue;
-					topTemplate = template;
-					break;
-				}
-				if (topTemplate != null)
-					createSkillTree(topTemplate, addList, true);
-			}
-			for (int i = addList.size() - 1; i >= 0; i--) {
-				SkillLearnTemplate treeEntry = addList.get(i);
-				if (treeEntry.getMinLevel() > level)
-					addList.remove(i);
-			}
+	/**
+	 * @param skillId
+	 * @param playerClass
+	 * @param race
+	 * @param maxLevel
+	 * @return All skills that are of the same skill stack for this skill ID. Class and race are necessary but playerLevel can be set to -1 if you want
+	 *         the full skill list. It's needed since every level of a certain skill has its own ID since 4.8. If you would only add the current skill
+	 *         ID, you wouldn't be able to use the lower level versions of a skill.
+	 */
+	public List<SkillLearnTemplate> getSkillsForSkill(int skillId, PlayerClass playerClass, Race race, int playerLevel) {
+		List<SkillLearnTemplate> skillTree = new FastTable<>();
+		for (SkillLearnTemplate learnTemplate : getTemplatesForSkill(getHighestSkill(skillId), playerClass, race)) {
+			createSkillTree(learnTemplate, skillTree);
+			break;
 		}
-		catch(Exception e) {
-			//log.error("Error while getting learn templates for skill "+skillId+", skillLevel");
-			//FIXME: NEED TO REPARSE skill_tree.xml
-		}
-		return addList.toArray(new SkillLearnTemplate[addList.size()]);
+		if (playerLevel > -1)
+			skillTree.removeIf(template -> template.getMinLevel() > playerLevel);
+		return skillTree;
 	}
 
 	/**
 	 * Creates skill tree list in ascending player level order recursively
-	 * @param topSkill - max skill for the stigma specified in item templates
-	 * @param addList - initially an empty list to which skills are added
-	 * @param isStigma - creating stigma skill tree ?
+	 * 
+	 * @param topSkill
+	 *          - max/best skill version of the stack
+	 * @param addList
+	 *          - initially an empty list to which skills are added
+	 * @param isStigma
+	 *          - creating stigma skill tree ?
 	 */
-	private void createSkillTree(SkillLearnTemplate topSkill, List<SkillLearnTemplate> addList, boolean isStigma) {
-		if (isStigma != topSkill.isStigma())
+	private void createSkillTree(SkillLearnTemplate topSkill, List<SkillLearnTemplate> addList) {
+		if (topSkill == null)
 			return;
 		addList.add(0, topSkill);
 		if (topSkill.getLearnSkill() == null)
 			return;
 
-		PlayerClass playerClass = topSkill.getClassId();
-		Race race = topSkill.getRace();
-
-		List<SkillLearnTemplate> byId = templatesById.get(topSkill.getLearnSkill());
-		for (SkillLearnTemplate template : byId) {
-			if (isStigma != template.isStigma())
+		for (SkillLearnTemplate template : getTemplatesForSkill(topSkill.getLearnSkill(), topSkill.getClassId(), topSkill.getRace())) {
+			if (topSkill.isStigma() != template.isStigma())
 				continue;
-			if (template.getRace() != race && template.getRace() != Race.PC_ALL)
-				continue;
-			// TODO: if needed, it should check starting class as well. Then add another argument to the method
-			if (template.getClassId() != playerClass && template.getClassId() != PlayerClass.ALL)
-				continue;
-			createSkillTree(template, addList, isStigma);
+			createSkillTree(template, addList);
 			break;
 		}
+	}
+
+	/**
+	 * @param skillId
+	 * @param playerClass
+	 * @param race
+	 * @return All skill learn templates with the specified skill ID, that match the players class and race. Should return 1 template max. If more, most
+	 *         probably skill_tree.xml is parsed incorrectly.
+	 */
+	public List<SkillLearnTemplate> getTemplatesForSkill(int skillId, PlayerClass playerClass, Race race) {
+		List<SkillLearnTemplate> searchSkills = new FastTable<>();
+		List<SkillLearnTemplate> byId = templatesById.get(skillId);
+		if (byId != null) {
+			for (SkillLearnTemplate template : byId)
+				if ((template.getClassId() == PlayerClass.ALL || template.getClassId() == playerClass)
+					&& (template.getRace() == Race.PC_ALL || template.getRace() == race))
+					searchSkills.add(template);
+		}
+		return searchSkills;
+	}
+
+	/**
+	 * @param skillId
+	 * @return The skill id with the highest skill level of this skills' stack.
+	 */
+	private int getHighestSkill(int skillId) {
+		SkillTemplate baseTemplate = DataManager.SKILL_DATA.getSkillTemplate(skillId);
+		if (baseTemplate == null)
+			return skillId;
+		List<SkillTemplate> templates = DataManager.SKILL_DATA.getSkillTemplatesByStack(baseTemplate.getStack());
+		if (templates == null)
+			return skillId;
+		SkillTemplate highestSkill = templates.stream().max((template1, template2) -> template1.getLvl() - template2.getLvl()).orElse(baseTemplate);
+		return highestSkill.getSkillId();
 	}
 
 	public boolean isLearnedSkill(int skillId) {

game-server/src/com/aionemu/gameserver/model/skill/PlayerSkillEntry.java
@@ -1,7 +1,8 @@
 package com.aionemu.gameserver.model.skill;
 
+import java.util.List;
+
 import com.aionemu.gameserver.dataholders.DataManager;
-import com.aionemu.gameserver.model.PlayerClass;
 import com.aionemu.gameserver.model.gameobjects.PersistentState;
 import com.aionemu.gameserver.model.gameobjects.player.Player;
 import com.aionemu.gameserver.network.aion.serverpackets.SM_SKILL_REMOVE;
@@ -19,13 +20,12 @@ public class PlayerSkillEntry extends SkillEntry {
 
 	public PlayerSkillEntry(Player player, int skillId, int skillLvl, PersistentState persistentState) {
 		this(skillId, skillLvl, 0, persistentState);
-		SkillLearnTemplate[] learnTemplates = DataManager.SKILL_TREE_DATA.getTemplatesForSkill(skillId);
-		if (learnTemplates.length == 0)
+		List<SkillLearnTemplate> learnTemplates = DataManager.SKILL_TREE_DATA.getTemplatesForSkill(skillId, player.getPlayerClass(), player.getRace());
+		if (learnTemplates.size() == 0)
 			skillType = DataManager.SKILL_DATA.getSkillTemplate(skillId).getStigmaType() == StigmaType.NONE ? 0 : 1; // no way to tell if linked stigma
 		else {
 			for (SkillLearnTemplate template : learnTemplates) {
-				if (template.isStigma() && template.getRace() != player.getOppositeRace()
-					&& (template.getClassId() == player.getPlayerClass() || template.getClassId() == PlayerClass.ALL)) {
+				if (template.isStigma()) {
 					skillType = template.isLinkedStigma() ? 3 : 1;
 					break;
 				}

game-server/src/com/aionemu/gameserver/model/skill/PlayerSkillList.java
@@ -7,9 +7,11 @@ import javolution.util.FastMap;
 import javolution.util.FastTable;
 
 import com.aionemu.gameserver.configs.main.CraftConfig;
+import com.aionemu.gameserver.dataholders.DataManager;
 import com.aionemu.gameserver.model.gameobjects.PersistentState;
 import com.aionemu.gameserver.model.gameobjects.player.Player;
 import com.aionemu.gameserver.services.SkillLearnService;
+import com.aionemu.gameserver.skillengine.model.SkillLearnTemplate;
 
 /**
  * @author IceReaper, orfeo087, Avol, AEJTester
@@ -62,14 +64,21 @@ public final class PlayerSkillList implements SkillList<Player> {
 
 	private synchronized boolean addSkill(Player player, int skillId, int skillLevel, boolean isTemporary) {
 		PlayerSkillEntry existingSkill = skills.get(skillId);
-		boolean isNew = false;
+		boolean isNew = true;
 		if (existingSkill != null) {
 			if (skillLevel <= existingSkill.getSkillLevel())
 				return false;
 			existingSkill.setSkillLvl(skillLevel);
+			isNew = false;
 		} else {
 			skills.put(skillId, new PlayerSkillEntry(player, skillId, skillLevel, isTemporary ? PersistentState.NOACTION : PersistentState.NEW));
-			isNew = true;
+			List<SkillLearnTemplate> learnTemplates = DataManager.SKILL_TREE_DATA.getSkillsForSkill(skillId, player.getPlayerClass(), player.getRace(), player.getLevel());
+			for (SkillLearnTemplate learnTemplate : learnTemplates) {
+				if (learnTemplate.getLearnSkill() != null && skills.get(learnTemplate.getLearnSkill()) != null) {
+					isNew = false;
+					break;
+				}
+			}
 		}
 		SkillLearnService.onLearnSkill(player, skillId, skillLevel, isNew);
 		return true;

game-server/src/com/aionemu/gameserver/model/templates/item/Stigma.java
@@ -47,7 +47,7 @@ public class Stigma {
 
 	public List<SkillTemplate> getGainSkillsByGroup(int groupNo) {
 		if (groupNo > 0 && groupNo <= gainSkillGroups.length)
-			return DataManager.SKILL_DATA.getSkillTemplate(gainSkillGroups[groupNo - 1]);
+			return DataManager.SKILL_DATA.getSkillTemplatesByGroup(gainSkillGroups[groupNo - 1]);
 		else
 			return null;
 	}

game-server/src/com/aionemu/gameserver/model/templates/item/StigmaSkill.java
@@ -1,26 +1,0 @@
-package com.aionemu.gameserver.model.templates.item;
-
-public class StigmaSkill {
-
-	private int skillId;
-	private int skillLvl;
-	private boolean isLinkedStigma;
-
-	public StigmaSkill(int skillLvl, int skillId, boolean isLinkedStigma) {
-		this.skillId = skillId;
-		this.skillLvl = skillLvl;
-		this.isLinkedStigma = isLinkedStigma;
-	}
-
-	public int getSkillLvl() {
-		return this.skillLvl;
-	}
-
-	public int getSkillId() {
-		return this.skillId;
-	}
-
-	public boolean isLinkedStigma() {
-		return this.isLinkedStigma;
-	}
-}

game-server/src/com/aionemu/gameserver/network/aion/serverpackets/SM_SKILL_LIST.java
@@ -37,7 +37,7 @@ public class SM_SKILL_LIST extends AionServerPacket {
 		this.skillList = FastTable.of(skill);
 		this.messageId = messageId;
 		this.skillNameId = DataManager.SKILL_DATA.getSkillTemplate(skill.getSkillId()).getNameId();
-		this.skillLvl = String.valueOf(skill.isStigmaSkill() ? skill.getSkillTemplate().getLvl() : skill.getSkillLevel());
+		this.skillLvl = String.valueOf(skill.getSkillLevel());
 		this.isNew = isNew;
 	}
 
@@ -47,7 +47,7 @@ public class SM_SKILL_LIST extends AionServerPacket {
 		writeC(isNew ? 0 : 1); // 1 all learned skills, 0 newly updated skills
 		for (PlayerSkillEntry entry : skillList) {
 			writeH(entry.getSkillId());// id
-			writeH(entry.getSkillLevel());// lvl
+			writeH(entry.isNormalSkill() ? 1 : entry.getSkillLevel()); // don't ask me, it's retail like...
 			writeC(0x00);
 			writeC(entry.getProfessionSkillBarSize());
 			writeD(entry.isProfessionSkill() ? entry.getProfessionFlag() : entry.getFlag(isNew));

game-server/src/com/aionemu/gameserver/services/SkillLearnService.java
@@ -33,7 +33,8 @@ public class SkillLearnService {
 		if (skill.isProfessionSkill())
 			PacketSendUtility.sendPacket(player, new SM_SKILL_LIST(skill, isNew ? 1330061 : 1330005, false));
 		else if (isNew)
-			PacketSendUtility.sendPacket(player, new SM_SKILL_LIST(skill, skill.isStigmaSkill() ? skill.isLinkedStigmaSkill() ? 1402891 : 1300401 : 1300050, true));
+			PacketSendUtility.sendPacket(player,
+				new SM_SKILL_LIST(skill, skill.isStigmaSkill() ? skill.isLinkedStigmaSkill() ? 1402891 : 1300401 : 1300050, true));
 		else
 			PacketSendUtility.sendPacket(player, new SM_SKILL_LIST(skill, 0, false));
 	}
@@ -82,13 +83,9 @@ public class SkillLearnService {
 	}
 
 	public static void learnSkillBook(Player player, int skillId) {
-		for (int i = player.getLevel(); i > 0; i--) {
-			for (SkillLearnTemplate skill : DataManager.SKILL_TREE_DATA.getTemplatesFor(player.getPlayerClass(), i, player.getRace()))
-				if (skillId == skill.getSkillId()) {
-					player.getSkillList().addSkill(player, skillId, skill.getSkillLevel());
-					break;
-				}
-		}
+		for (SkillLearnTemplate skill : DataManager.SKILL_TREE_DATA.getSkillsForSkill(skillId, player.getPlayerClass(), player.getRace(),
+			player.getLevel()))
+			player.getSkillList().addSkill(player, skillId, skill.getSkillLevel());
 	}
 
 	public static boolean removeSkill(Player player, int skillId) {
@@ -102,12 +99,13 @@ public class SkillLearnService {
 		return false;
 	}
 
-	public static int getSkillMinLevel(int skillId, int playerLevel, int wantedSkillLevel) {
+	public static int getSkillMinLevel(Player player, int skillId, int wantedSkillLevel) {
 		int nearestMinLevel = 0;
-		for (SkillLearnTemplate template : DataManager.SKILL_TREE_DATA.getTemplatesForSkill(skillId)) {
-			if (template.getMinLevel() <= playerLevel && template.getSkillLevel() <= wantedSkillLevel)
+		for (SkillLearnTemplate template : DataManager.SKILL_TREE_DATA.getSkillsForSkill(skillId, player.getPlayerClass(), player.getRace(),
+			player.getLevel())) {
+			if (template.getSkillLevel() <= wantedSkillLevel)
 				nearestMinLevel = Math.max(template.getMinLevel(), nearestMinLevel);
 		}
-		return nearestMinLevel > 0 ? nearestMinLevel : playerLevel;
+		return nearestMinLevel == 0 ? player.getLevel() : nearestMinLevel;
 	}
 }

commit 655c674e72cf07b4120584482e00f843d82bd33e

Fixed wrong keys for skillTemplatesByStack introduced by 44c4bd7

game-server/src/com/aionemu/gameserver/dataholders/SkillData.java
@@ -21,11 +21,8 @@ public class SkillData {
 	@XmlElement(name = "skill_template")
 	private List<SkillTemplate> skillTemplates;
 
-	/**
-	 * Map that contains skillId - SkillTemplate key-value pair
-	 */
 	@XmlTransient
-	private final Map<Integer, SkillTemplate> skillData = new HashMap<>();
+	private final Map<Integer, SkillTemplate> skillTemplateById = new HashMap<>();
 
 	@XmlTransient
 	private final Map<String, List<SkillTemplate>> skillTemplatesByGroup = new LinkedHashMap<>();
@@ -33,49 +30,31 @@ public class SkillData {
 	@XmlTransient
 	private final Map<String, List<SkillTemplate>> skillTemplatesByStack = new LinkedHashMap<>();
 
-	/**
-	 * Map that contains cooldownId - skillId List
-	 */
 	@XmlTransient
-	private Map<Integer, List<Integer>> cooldownGroups = new HashMap<>();
-
+	private final Map<Integer, List<Integer>> skillIdsByCooldownId = new HashMap<>();
 	void afterUnmarshal(Unmarshaller u, Object parent) {
-		skillData.clear();
+		skillTemplateById.clear();
 		skillTemplatesByGroup.clear();
 		skillTemplatesByStack.clear();
-		cooldownGroups.clear();
+		skillIdsByCooldownId.clear();
 		for (SkillTemplate skillTemplate : skillTemplates) {
 			int skillId = skillTemplate.getSkillId();
 			int cooldownId = skillTemplate.getCooldownId();
-			skillData.put(skillId, skillTemplate);
-			if (!cooldownGroups.containsKey(cooldownId))
-				cooldownGroups.put(cooldownId, new ArrayList<>());
-			cooldownGroups.get(cooldownId).add(skillId);
-
-			if (skillTemplate.getGroup() != null) {
-				if (!skillTemplatesByGroup.containsKey(skillTemplate.getGroup()))
-					skillTemplatesByGroup.put(skillTemplate.getGroup(), new ArrayList<>());
-				skillTemplatesByGroup.get(skillTemplate.getGroup()).add(skillTemplate);
-			}
-			if (skillTemplate.getStack() != null) {
-				if (!skillTemplatesByStack.containsKey(skillTemplate.getGroup()))
-					skillTemplatesByStack.put(skillTemplate.getGroup(), new ArrayList<>());
-				skillTemplatesByStack.get(skillTemplate.getGroup()).add(skillTemplate);
-			}
+			skillTemplateById.put(skillId, skillTemplate);
+			skillIdsByCooldownId.computeIfAbsent(cooldownId, k -> new ArrayList<>()).add(skillId);
+			if (skillTemplate.getGroup() != null)
+				skillTemplatesByGroup.computeIfAbsent(skillTemplate.getGroup(), k -> new ArrayList<>()).add(skillTemplate);
+			if (skillTemplate.getStack() != null)
+				skillTemplatesByStack.computeIfAbsent(skillTemplate.getStack(), k -> new ArrayList<>()).add(skillTemplate);
 		}
 		skillTemplates = null;
 	}
 
-	/**
-	 * @param skillId
-	 * @return SkillTemplate
-	 */
 	public SkillTemplate getSkillTemplate(int skillId) {
-		return skillData.get(skillId);
+		return skillTemplateById.get(skillId);
 	}
 
 	/**
-	 * @param skillGroup
 	 * @return All skill templates of this group. A group is less precise and may be null.
 	 */
 	public List<SkillTemplate> getSkillTemplatesByGroup(String skillGroup) {
@@ -83,7 +62,6 @@ public class SkillData {
 	}
 
 	/**
-	 * @param skillStack
 	 * @return All skill templates of this stack. A stack is more precise than a group.
 	 *         For example: Charge skills have their own stack per charging level. Also, skills that are similar for both factions have the same group,
 	 *         but different stacks.
@@ -92,28 +70,16 @@ public class SkillData {
 		return skillTemplatesByStack.get(skillStack);
 	}
 
-	/**
-	 * @return skillData.size()
-	 */
 	public int size() {
-		return skillData.size();
+		return skillTemplateById.size();
 	}
 
-	/**
-	 * @return the skillTemplates
-	 */
 	public Collection<SkillTemplate> getSkillTemplates() {
-		return skillData.values();
+		return skillTemplateById.values();
 	}
 
-	/**
-	 * This method is used to get all skills assigned to a specific cooldownId
-	 * 
-	 * @param cooldownId
-	 * @return List including all skills for asked cooldownId
-	 */
 	public List<Integer> getSkillsForCooldownId(int cooldownId) {
-		return cooldownGroups.get(cooldownId);
+		return skillIdsByCooldownId.get(cooldownId);
 	}
 
 	public void validateMotions() {