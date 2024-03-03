4b57e07ba1e00c6ac9d3c8e3859ae54898c10c38
Estrayl <galawaynkrueger@yahoo.de>
2024-03-03 20:32:35 +0100

    Upgrade Arcade Event
    https://beyond-aion.com/forum/thread/1132-upgrade-arcade-event/?postID=15355#post15355

--- game-server/sql/update.sql
+++ game-server/sql/update.sql
@@ -1,9 +1,6 @@
 /*
- * DB changes since feb36dd6 (09.02.2024)
+ * DB changes since feb36dd6 (04.03.2024)
  */
 
--- drop advent calendar table
-drop table advent;
-
 -- remove old event items
-DELETE FROM inventory WHERE item_id IN (188052318, 188100091, 188100092, 188100093, 188100094);
+DELETE FROM inventory WHERE item_id IN (186000389);
