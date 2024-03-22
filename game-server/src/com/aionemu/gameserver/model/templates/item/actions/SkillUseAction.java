37ae0c91a4554ed430cd3fbfff9d07cb4f082a89
Neon <neon@beyond-aion.com>
2024-03-23 00:15:16 +0100

    Fixed heal items could not be used on other creatures if the casting player was fully healed

--- game-server/src/com/aionemu/gameserver/model/templates/item/actions/SkillUseAction.java
+++ game-server/src/com/aionemu/gameserver/model/templates/item/actions/SkillUseAction.java
@@ -7,9 +7,9 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlType;
 
+import com.aionemu.gameserver.model.gameobjects.Creature;
 import com.aionemu.gameserver.model.gameobjects.Item;
 import com.aionemu.gameserver.model.gameobjects.player.Player;
-import com.aionemu.gameserver.model.stats.container.PlayerLifeStats;
 import com.aionemu.gameserver.network.aion.serverpackets.SM_SYSTEM_MESSAGE;
 import com.aionemu.gameserver.skillengine.SkillEngine;
 import com.aionemu.gameserver.skillengine.effect.*;
@@ -64,24 +64,31 @@ public class SkillUseAction extends AbstractItemAction {
 					return false;
 				}
 			}
-			PlayerLifeStats lifeStats = player.getLifeStats();
-			if (lifeStats.isFullyRestoredHp() || lifeStats.isFullyRestoredMp()) { // convenience feature: block using ineffective heals
-				int hpHealEffects = 0, mpHealEffects = 0;
-				for (EffectTemplate template : effects) {
-					if (template instanceof HealEffect || template instanceof HealInstantEffect || template instanceof ProcHealInstantEffect) {
-						hpHealEffects++;
-					} else if (template instanceof MPHealEffect || template instanceof MPHealInstantEffect || template instanceof ProcMPHealInstantEffect) {
-						mpHealEffects++;
-					}
-				}
-				if (hpHealEffects + mpHealEffects == effects.size() && !(!lifeStats.isFullyRestoredHp() && hpHealEffects > 0)
-					&& !(!lifeStats.isFullyRestoredMp() && mpHealEffects > 0)) {
-					PacketSendUtility.sendPacket(player, SM_SYSTEM_MESSAGE.STR_NOTHING_HAPPEN());
-					return false;
-				}
+		}
+		if (!skill.canUseSkill(CastState.CAST_START)) // also initializes effectedList for isIneffectiveHealSkill check down below
+			return false;
+		if (isIneffectiveHealSkill(effects, skill.getEffectedList())) { // convenience feature: block using ineffective heals
+			PacketSendUtility.sendPacket(player, SM_SYSTEM_MESSAGE.STR_NOTHING_HAPPEN());
+			return false;
+		}
+		return true;
+	}
+
+	private static boolean isIneffectiveHealSkill(List<EffectTemplate> effects, List<Creature> effectedList) {
+		int hpHealEffects = 0, mpHealEffects = 0;
+		for (EffectTemplate template : effects) {
+			if (template.getValue() < 0) // negative heal value means damage
+				return false;
+			if (template instanceof HealEffect || template instanceof HealInstantEffect || template instanceof ProcHealInstantEffect) {
+				hpHealEffects++;
+			} else if (template instanceof MPHealEffect || template instanceof MPHealInstantEffect || template instanceof ProcMPHealInstantEffect) {
+				mpHealEffects++;
+			} else {
+				return false;
 			}
 		}
-		return skill.canUseSkill(CastState.CAST_START);
+		return (hpHealEffects == 0 || effectedList.stream().allMatch(effected -> effected.getLifeStats().isFullyRestoredHp()))
+			&& (mpHealEffects == 0 || effectedList.stream().allMatch(effected -> effected.getLifeStats().isFullyRestoredMp()));
 	}
 
 	@Override
