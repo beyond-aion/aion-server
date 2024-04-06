e92a773fd992272d63826095794ff94b9da773f8
Farlon <farlon@beyond-aion.com>
2024-04-06 04:33:35 +0200

    Alchemy Event

--- game-server/sql/update.sql
+++ game-server/sql/update.sql
@@ -1,6 +1,6 @@
 /*
- * DB changes since feb36dd6 (04.03.2024)
+ * DB changes since a66b0738 (06.04.2024)
  */
 
 -- remove old event items
-DELETE FROM inventory WHERE item_id IN (186000389);
+DELETE FROM inventory WHERE item_id IN (185000185, 185000186, 188052640, 188052642, 188100126, 188100127);
