9a49f5b5a1e1d31c274c558c0f07d5721e64a315
crlfrancis <cftp_14@hotmail.com>
2025-01-25 20:40:44 -0400

    Fixed players cannot search npcs from another map

--- game-server/src/com/aionemu/gameserver/network/aion/clientpackets/CM_OBJECT_SEARCH.java
+++ game-server/src/com/aionemu/gameserver/network/aion/clientpackets/CM_OBJECT_SEARCH.java
@@ -37,10 +37,10 @@ public class CM_OBJECT_SEARCH extends AionClientPacket {
 		if (activePlayer == null) {
 			return;
 		}
-		SpawnSearchResult searchResult = DataManager.SPAWNS_DATA.getNearestSpawnByNpcId(activePlayer, npcId, activePlayer.getWorldId());
+		SpawnSearchResult searchResult = DataManager.SPAWNS_DATA.getFirstSpawnByNpcId(0, npcId);
 		if (searchResult != null)
-			sendPacket(new SM_SHOW_NPC_ON_MAP(activePlayer, npcId, searchResult.getWorldId(), searchResult.getSpot().getX(), searchResult.getSpot().getY(),
-				searchResult.getSpot().getZ()));
+			sendPacket(new SM_SHOW_NPC_ON_MAP(activePlayer, npcId, searchResult.getWorldId(), searchResult.getSpot().getX(),
+				searchResult.getSpot().getY(), searchResult.getSpot().getZ()));
 		else
 			sendPacket(SM_SYSTEM_MESSAGE.STR_FIND_POS_UNKNOWN_NAME());
 	}
