2be4b89a3bcd7186d45c00195fc5d0f0ca739030
Neon <neon@beyond-aion.com>
2025-02-03 04:40:44 +0100

    Adjustments based on retail info

--- game-server/data/handlers/quest/theobomos/_3058StoneofMabolo.java
+++ game-server/data/handlers/quest/theobomos/_3058StoneofMabolo.java
@@ -2,17 +2,11 @@ package quest.theobomos;
 
 import static com.aionemu.gameserver.model.DialogAction.*;
 
-import com.aionemu.gameserver.model.gameobjects.Item;
 import com.aionemu.gameserver.model.gameobjects.Npc;
 import com.aionemu.gameserver.model.gameobjects.player.Player;
-import com.aionemu.gameserver.network.aion.serverpackets.SM_ITEM_USAGE_ANIMATION;
 import com.aionemu.gameserver.questEngine.handlers.AbstractQuestHandler;
-import com.aionemu.gameserver.questEngine.handlers.HandlerResult;
 import com.aionemu.gameserver.questEngine.model.QuestEnv;
 import com.aionemu.gameserver.questEngine.model.QuestState;
-import com.aionemu.gameserver.questEngine.model.QuestStatus;
-import com.aionemu.gameserver.utils.PacketSendUtility;
-import com.aionemu.gameserver.utils.ThreadPoolManager;
 
 /**
  * @author Leunam
@@ -25,7 +19,6 @@ public class _3058StoneofMabolo extends AbstractQuestHandler {
 
 	@Override
 	public void register() {
-		qe.registerQuestItem(182208041, questId);
 		qe.registerQuestNpc(798189).addOnTalkEvent(questId);
 		qe.registerQuestNpc(203701).addOnTalkEvent(questId);
 		qe.registerQuestNpc(798213).addOnTalkEvent(questId);
@@ -51,57 +44,29 @@ public class _3058StoneofMabolo extends AbstractQuestHandler {
 		if (qs == null)
 			return false;
 
-		int var = qs.getQuestVarById(0);
-		if (qs.getStatus() == QuestStatus.REWARD) {
-			if (targetId == 798213) {
-				if (env.getDialogActionId() == USE_OBJECT)
-					return sendQuestDialog(env, 2375);
-				else if (env.getDialogActionId() == SELECT_QUEST_REWARD)
-					return sendQuestDialog(env, 5);
-				else
-					return sendQuestEndDialog(env);
-			}
-		} else if (qs.getStatus() != QuestStatus.START) {
-			return false;
-		}
 		if (targetId == 798189) {
 			switch (env.getDialogActionId()) {
 				case QUEST_SELECT:
-					if (var == 0)
-						return sendQuestDialog(env, 1352);
-					return false;
+					return sendQuestDialog(env, 1352);
 				case SETPRO1:
-						return defaultCloseDialog(env, 0, 1);
+					return defaultCloseDialog(env, 0, 1);
 			}
 		} else if (targetId == 203701) {
 			switch (env.getDialogActionId()) {
 				case QUEST_SELECT:
-					if (var == 1)
-						return sendQuestDialog(env, 1693);
-					return false;
+					return sendQuestDialog(env, 1693);
 				case SETPRO2:
-						return defaultCloseDialog(env, 1, 2, true, false);
+					return defaultCloseDialog(env, 1, 2);
 			}
+		} else if (targetId == 798213) {
+			switch (env.getDialogActionId()) {
+				case QUEST_SELECT:
+					return sendQuestDialog(env, 2375);
+				case SELECT_QUEST_REWARD:
+					changeQuestStep(env, 2, 2, true);
+			}
+			return sendQuestEndDialog(env);
 		}
 		return false;
-	}
-
-	@Override
-	public HandlerResult onItemUseEvent(final QuestEnv env, Item item) {
-		final Player player = env.getPlayer();
-		final int id = item.getItemTemplate().getTemplateId();
-		final int itemObjId = item.getObjectId();
-
-		if (id != 182208041)
-			return HandlerResult.UNKNOWN;
-		PacketSendUtility.broadcastPacket(player, new SM_ITEM_USAGE_ANIMATION(player.getObjectId(), itemObjId, id, 20, 0, 0), true);
-		ThreadPoolManager.getInstance().schedule(new Runnable() {
-
-			@Override
-			public void run() {
-				PacketSendUtility.broadcastPacket(player, new SM_ITEM_USAGE_ANIMATION(player.getObjectId(), itemObjId, id, 0, 1, 0), true);
-			}
-		}, 20);
-		return HandlerResult.SUCCESS;
 	}
 }
--- game-server/src/com/aionemu/gameserver/model/templates/item/actions/QuestStartAction.java
+++ game-server/src/com/aionemu/gameserver/model/templates/item/actions/QuestStartAction.java
@@ -9,6 +9,7 @@ import com.aionemu.gameserver.dataholders.DataManager;
 import com.aionemu.gameserver.model.DialogAction;
 import com.aionemu.gameserver.model.gameobjects.Item;
 import com.aionemu.gameserver.model.gameobjects.player.Player;
+import com.aionemu.gameserver.network.aion.serverpackets.SM_ITEM_USAGE_ANIMATION;
 import com.aionemu.gameserver.network.aion.serverpackets.SM_SYSTEM_MESSAGE;
 import com.aionemu.gameserver.questEngine.QuestEngine;
 import com.aionemu.gameserver.questEngine.model.QuestEnv;
@@ -43,6 +44,7 @@ public class QuestStartAction extends AbstractItemAction {
 
 	@Override
 	public void act(Player player, Item parentItem, Item targetItem, Object... params) {
+		PacketSendUtility.broadcastPacketAndReceive(player, new SM_ITEM_USAGE_ANIMATION(player.getObjectId(), parentItem.getObjectId(), parentItem.getItemId()));
 		QuestEngine.getInstance().onDialog(new QuestEnv(null, player, questid, DialogAction.ASK_QUEST_ACCEPT));
 	}
 }
