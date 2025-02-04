3dc2167fc4a2aa6a9b0ce7df4e895fe252f1758b
sky1683589933 <1683589933@qq.com>
2025-02-05 03:20:02 +0800

    Fixed Quest 30255 and 30355 unable to complete properly
    Modify the Quest usage object to the correct one and add the correct usage object to the Abyss instance.

--- game-server/data/handlers/instance/abyss/AbyssalSplinterInstance.java
+++ game-server/data/handlers/instance/abyss/AbyssalSplinterInstance.java
@@ -147,11 +147,21 @@ public class AbyssalSplinterInstance extends GeneralInstanceHandler {
 			case 701593: // Artifact of Protection (Hard Mode)
 				sendMsg(SM_SYSTEM_MESSAGE.STR_MSG_IDAbRe_Core_NmdDH_Wakeup());
 				spawn(216960, 329.70886f, 733.8744f, 197.60938f, (byte) 0);
+				if (player.getRace() == Race.ELYOS) {
+					spawn(700857, 326.1821f, 766.9640f, 202.1832f, (byte) 100, 79); // artifact of protection(elyos) for quest 30255
+				} else if (player.getRace() == Race.ASMODIANS) {
+					spawn(700858, 326.1821f, 766.9640f, 202.1832f, (byte) 100, 79); // artifact of protection(asmodians) for quest 30355
+				}
 				npc.getController().die();
 				break;
 			case 700856: // Artifact of Protection (Easy Mode)
 				sendMsg(SM_SYSTEM_MESSAGE.STR_MSG_IDAbRe_Core_NmdD_Wakeup());
 				spawn(216952, 329.70886f, 733.8744f, 197.60938f, (byte) 0);
+				if (player.getRace() == Race.ELYOS) {
+					spawn(700857, 326.1821f, 766.9640f, 202.1832f, (byte) 100, 79); // artifact of protection(elyos) for quest 30255
+				} else if (player.getRace() == Race.ASMODIANS) {
+					spawn(700858, 326.1821f, 766.9640f, 202.1832f, (byte) 100, 79); // artifact of protection(asmodians) for quest 30355
+				}
 				npc.getController().die();
 				break;
 		}
