0e0874b0120d4bbe18b0cbe94a6896c7d34e1c30
sky1683589933 <1683589933@qq.com>
2025-02-07 09:59:02 +0800

    Add three new quest execution scripts
    Add quest execution script 3908 to fix the issue of not being able to complete quest properly, add quest execution script 3972 to fix empty dialog, and add quest execution script 3973 to fix the issue of completing quest without work items and encountering empty dialog.

---
+++ game-server/data/handlers/quest/sanctum/_3972TheGoBetween.java
@@ -0,0 +1,63 @@
+package quest.sanctum;
+
+import static com.aionemu.gameserver.model.DialogAction.*;
+
+import com.aionemu.gameserver.model.gameobjects.player.Player;
+import com.aionemu.gameserver.questEngine.handlers.AbstractQuestHandler;
+import com.aionemu.gameserver.questEngine.model.QuestEnv;
+import com.aionemu.gameserver.questEngine.model.QuestState;
+import com.aionemu.gameserver.questEngine.model.QuestStatus;
+
+public class _3972TheGoBetween extends AbstractQuestHandler {
+
+	private final static int questId = 3972;
+
+	public _3972TheGoBetween() {
+		super(questId);
+	}
+
+	public void register() {
+		qe.registerQuestNpc(203893).addOnQuestStart(questId);
+		qe.registerQuestNpc(203893).addOnTalkEvent(questId);
+		qe.registerQuestNpc(798949).addOnTalkEvent(questId);
+	}
+
+	@Override
+	public boolean onDialogEvent(QuestEnv env) {
+		Player player = env.getPlayer();
+		QuestState qs = player.getQuestStateList().getQuestState(questId);
+		int dialogActionId = env.getDialogActionId();
+		int targetId = env.getTargetId();
+		
+		if (qs == null || qs.isStartable()) {
+			if (targetId == 203893) {
+				if (dialogActionId == QUEST_SELECT)
+					return sendQuestDialog(env, 1011);
+				else
+					return sendQuestStartDialog(env);
+			}
+		} else if (qs.getStatus() == QuestStatus.START){
+			int var = qs.getQuestVarById(0);
+			if (targetId == 798949) {
+				switch (dialogActionId) {
+					case QUEST_SELECT:
+						if (var == 0)
+							return sendQuestDialog(env, 1352);
+					case SETPRO1:
+						return defaultCloseDialog(env, 0, 1);
+				}
+			} else if (targetId == 203893) {
+				switch (dialogActionId) {
+					case QUEST_SELECT:
+						if (var == 1)
+							return sendQuestDialog(env, 2375);
+					case SELECT_QUEST_REWARD:
+						return defaultCloseDialog(env, 1, 1, true, true);
+				}
+			}
+		} else if (qs.getStatus() == QuestStatus.REWARD) {
+			return sendQuestEndDialog(env);
+		}
+		return false;
+	}
+}
