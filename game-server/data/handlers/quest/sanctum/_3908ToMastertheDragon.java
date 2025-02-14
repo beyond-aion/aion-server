1de458bf0e801351d73eed8f2a5e080a4f8a0fc4
sky1683589933 <161305276+sky1683589933@users.noreply.github.com>
2025-02-14 10:16:40 +0800

    Update game-server/data/handlers/quest/sanctum/_3908ToMastertheDragon.java
    Co-authored-by: Neon <neon@beyond-aion.com>

--- game-server/data/handlers/quest/sanctum/_3908ToMastertheDragon.java
+++ game-server/data/handlers/quest/sanctum/_3908ToMastertheDragon.java
@@ -8,6 +8,9 @@ import com.aionemu.gameserver.questEngine.model.QuestEnv;
 import com.aionemu.gameserver.questEngine.model.QuestState;
 import com.aionemu.gameserver.questEngine.model.QuestStatus;
 
+/**
+ * @author sky123
+ */
 public class _3908ToMastertheDragon extends AbstractQuestHandler {
 
 	public _3908ToMastertheDragon() {
