103e61bb4717aa9ce72b87865e26fd6ade9acdcd
Neon <neon@beyond-aion.com>
2025-08-20 01:46:19 +0200

    Validate entering character edit mode

--- game-server/src/com/aionemu/gameserver/network/aion/clientpackets/CM_QUIT.java
+++ game-server/src/com/aionemu/gameserver/network/aion/clientpackets/CM_QUIT.java
@@ -3,13 +3,18 @@ package com.aionemu.gameserver.network.aion.clientpackets;
 import java.util.Set;
 
 import com.aionemu.gameserver.dao.PlayerPunishmentsDAO;
+import com.aionemu.gameserver.model.DialogAction;
 import com.aionemu.gameserver.model.account.PlayerAccountData;
+import com.aionemu.gameserver.model.gameobjects.Npc;
+import com.aionemu.gameserver.model.gameobjects.VisibleObject;
 import com.aionemu.gameserver.model.gameobjects.player.Player;
 import com.aionemu.gameserver.network.aion.AionClientPacket;
 import com.aionemu.gameserver.network.aion.AionConnection;
 import com.aionemu.gameserver.network.aion.AionConnection.State;
 import com.aionemu.gameserver.network.aion.serverpackets.SM_QUIT_RESPONSE;
 import com.aionemu.gameserver.services.player.PlayerLeaveWorldService;
+import com.aionemu.gameserver.utils.PositionUtil;
+import com.aionemu.gameserver.utils.audit.AuditLogger;
 
 /**
  * @author -Nemesiss-, Neon
@@ -37,12 +42,19 @@ public class CM_QUIT extends AionClientPacket {
 		boolean charEditScreen = false;
 
 		if (player != null) {
+			charEditScreen = player.getCommonData().isInEditMode();
+			if (charEditScreen) {
+				VisibleObject target = player.getTarget();
+				if (!(target instanceof Npc npc) || (!npc.getObjectTemplate().supportsAction(DialogAction.EDIT_CHARACTER_ALL) && !npc.getObjectTemplate().supportsAction(DialogAction.EDIT_CHARACTER_GENDER)) || !PositionUtil.isInTalkRange(player, npc)) {
+					AuditLogger.log(player, "tried to enter the plastic surgery screen without targeting the respective npc within talk distance");
+					return;
+				}
+			}
 			if (stayConnected) { // update char selection info
 				player.getAccountData().setVisibleItems(player.getEquipment().getEquippedForAppearance());
 				for (PlayerAccountData plAccData : con.getAccount().getPlayerAccDataList())
 					plAccData.setCharBanInfo(PlayerPunishmentsDAO.getCharBanInfo(plAccData.getPlayerCommonData().getPlayerObjId()));
 			}
-			charEditScreen = player.getCommonData().isInEditMode();
 			PlayerLeaveWorldService.leaveWorld(player);
 		}
 
