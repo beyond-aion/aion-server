beyond-aion/aion-server

commit 010cc8f9a03f109aaed0d32c0dae459daa8fe8f5

Fixed #32
Also removed obsolete portal handling and added logging for other suspicious code which hopefully can be removed soon

game-server/src/com/aionemu/gameserver/controllers/NpcController.java
@@ -262,10 +262,14 @@ public class NpcController extends CreatureController<Npc> {
 	}
 
 	@Override
-	public void onDialogSelect(int dialogActionId, int prevDialogId, final Player player, int questId, int extendedRewardIndex) {
-		QuestEnv env = new QuestEnv(getOwner(), player, questId, dialogActionId);
-		if (!PositionUtil.isInTalkRange(player, getOwner()) && !QuestEngine.getInstance().onDialog(env))
-			return;
+	public void onDialogSelect(int dialogActionId, int prevDialogId, Player player, int questId, int extendedRewardIndex) {
+		if (!PositionUtil.isInTalkRange(player, getOwner())) {
+			// TODO remove this call and return unconditionally once logging has clarified wtf it is trying to solve 
+			if (QuestEngine.getInstance().onDialog(new QuestEnv(getOwner(), player, questId, dialogActionId)))
+				log.warn(player + " talked to " + getOwner() + " out of range, but QuestEngine.onDialog returned true");
+			else
+				return;
+		}
 		if (!getOwner().getAi().onDialogSelect(player, dialogActionId, questId, extendedRewardIndex)) {
 			DialogService.onDialogSelect(dialogActionId, player, getOwner(), questId, extendedRewardIndex);
 		}

game-server/src/com/aionemu/gameserver/services/DialogService.java
@@ -12,7 +12,6 @@ import com.aionemu.gameserver.configs.main.AutoGroupConfig;
 import com.aionemu.gameserver.dataholders.DataManager;
 import com.aionemu.gameserver.model.DialogAction;
 import com.aionemu.gameserver.model.DialogPage;
-import com.aionemu.gameserver.model.animations.TeleportAnimation;
 import com.aionemu.gameserver.model.autogroup.AutoGroupType;
 import com.aionemu.gameserver.model.gameobjects.Npc;
 import com.aionemu.gameserver.model.gameobjects.PetAction;
@@ -24,9 +23,6 @@ import com.aionemu.gameserver.model.siege.FortressLocation;
 import com.aionemu.gameserver.model.team.alliance.PlayerAlliance;
 import com.aionemu.gameserver.model.templates.goods.GoodsList;
 import com.aionemu.gameserver.model.templates.npc.TalkInfo;
-import com.aionemu.gameserver.model.templates.portal.PortalPath;
-import com.aionemu.gameserver.model.templates.teleport.TeleportLocation;
-import com.aionemu.gameserver.model.templates.teleport.TeleporterTemplate;
 import com.aionemu.gameserver.model.templates.tradelist.TradeListTemplate;
 import com.aionemu.gameserver.model.templates.tradelist.TradeListTemplate.TradeTab;
 import com.aionemu.gameserver.model.templates.zone.ZoneClassName;
@@ -40,7 +36,6 @@ import com.aionemu.gameserver.services.instance.PeriodicInstanceManager;
 import com.aionemu.gameserver.services.item.ItemChargeService;
 import com.aionemu.gameserver.services.item.ItemPacketService.ItemUpdateType;
 import com.aionemu.gameserver.services.player.PlayerMailboxState;
-import com.aionemu.gameserver.services.teleport.PortalService;
 import com.aionemu.gameserver.services.teleport.TeleportService;
 import com.aionemu.gameserver.services.trade.PricesService;
 import com.aionemu.gameserver.skillengine.model.DispelSlotType;
@@ -70,9 +65,7 @@ public class DialogService {
 			mailbox.mailBoxState = PlayerMailboxState.CLOSED;
 	}
 
-	public static void onDialogSelect(int dialogActionId, final Player player, Npc npc, int questId, int extendedRewardIndex) {
-		QuestEnv env = new QuestEnv(npc, player, questId, dialogActionId);
-		env.setExtendedRewardIndex(extendedRewardIndex);
+	public static void onDialogSelect(int dialogActionId, Player player, Npc npc, int questId, int extendedRewardIndex) {
 		int targetObjectId = npc.getObjectId();
 
 		if (questId == 0) {
@@ -277,34 +270,14 @@ public class DialogService {
 				case HOUSING_RECREATE_PERSONAL_INS: // recreate personal house instance (studio)
 					HousingService.getInstance().recreatePlayerStudio(player);
 					break;
-				case SETPRO1:
-				case SETPRO2:
-				case SETPRO3:
-				case TELEPORT_SIMPLE:
-					if (QuestEngine.getInstance().onDialog(env)) { // remove this shit after assigning AI portal_dialog
-						return;
-					}
-					PortalPath portalPath = DataManager.PORTAL2_DATA.getPortalDialogPath(npc.getNpcId(), dialogActionId, player);
-					if (portalPath != null) {
-						PortalService.port(portalPath, player, npc);
-					} else {
-						TeleporterTemplate template = DataManager.TELEPORTER_DATA.getTeleporterTemplateByNpcId(npc.getNpcId());
-						if (template != null) {
-							TeleportLocation loc = template.getTeleLocIdData().getTelelocations().get(0);
-							if (loc != null)
-								TeleportService.teleport(template, loc.getLocId(), player, npc,
-									npc.getAi().getName().equals("general") ? TeleportAnimation.JUMP_IN : TeleportAnimation.FADE_OUT_BEAM);
-						}
-					}
-					break;
 				default:
-					if (QuestEngine.getInstance().onDialog(env))
-						return;
 					// action id = next page id
 					PacketSendUtility.sendPacket(player, new SM_DIALOG_WINDOW(targetObjectId, dialogActionId));
 					break;
 			}
 		} else {
+			QuestEnv env = new QuestEnv(npc, player, questId, dialogActionId);
+			env.setExtendedRewardIndex(extendedRewardIndex);
 			if (QuestEngine.getInstance().onDialog(env))
 				return;
 			// action id = next page id

commit c7453844d4c7109b52ef24a2d332f03365d65f63

Fixed relic npcs that broke with 010cc8f (#82)

game-server/src/com/aionemu/gameserver/controllers/NpcController.java
@@ -263,13 +263,8 @@ public class NpcController extends CreatureController<Npc> {
 
 	@Override
 	public void onDialogSelect(int dialogActionId, int prevDialogId, Player player, int questId, int extendedRewardIndex) {
-		if (!PositionUtil.isInTalkRange(player, getOwner())) {
-			// TODO remove this call and return unconditionally once logging has clarified wtf it is trying to solve 
-			if (QuestEngine.getInstance().onDialog(new QuestEnv(getOwner(), player, questId, dialogActionId)))
-				log.warn(player + " talked to " + getOwner() + " out of range, but QuestEngine.onDialog returned true");
-			else
-				return;
-		}
+		if (!PositionUtil.isInTalkRange(player, getOwner()))
+			return;
 		if (!getOwner().getAi().onDialogSelect(player, dialogActionId, questId, extendedRewardIndex)) {
 			DialogService.onDialogSelect(dialogActionId, player, getOwner(), questId, extendedRewardIndex);
 		}

game-server/src/com/aionemu/gameserver/questEngine/handlers/AbstractQuestHandler.java
@@ -91,14 +91,7 @@ public abstract class AbstractQuestHandler {
 	public abstract void register();
 
 	public boolean onDialogEvent(QuestEnv env) {
-		int dialogActionId = env.getDialogActionId();
-		if (dialogActionId >= SELECT1 && dialogActionId <= SELECT15_4_4_4_4) {
-			// simple "next page" event (action ID = next dialog page ID), but there are some quests where this default behavior does not apply (e.g.
-			// 4074)
-			sendDialogPacket(env, dialogActionId, questId);
-			return true;
-		}
-		switch (dialogActionId) {
+		switch (env.getDialogActionId()) {
 			case ASK_QUEST_ACCEPT: // show quest accept dialog (coming from pre-conversation)
 				sendDialogPacket(env, DialogPage.ASK_QUEST_ACCEPT_WINDOW.id(), questId);
 				return true;

game-server/src/com/aionemu/gameserver/services/DialogService.java
@@ -271,18 +271,21 @@ public class DialogService {
 					HousingService.getInstance().recreatePlayerStudio(player);
 					break;
 				default:
-					// action id = next page id
-					PacketSendUtility.sendPacket(player, new SM_DIALOG_WINDOW(targetObjectId, dialogActionId));
+					handleQuestDialogueOrSendNextPage(dialogActionId, player, npc, questId, extendedRewardIndex);
 					break;
 			}
 		} else {
-			QuestEnv env = new QuestEnv(npc, player, questId, dialogActionId);
-			env.setExtendedRewardIndex(extendedRewardIndex);
-			if (QuestEngine.getInstance().onDialog(env))
-				return;
-			// action id = next page id
-			PacketSendUtility.sendPacket(player, new SM_DIALOG_WINDOW(targetObjectId, dialogActionId, questId));
+			handleQuestDialogueOrSendNextPage(dialogActionId, player, npc, questId, extendedRewardIndex);
 		}
+	}
+
+	private static void handleQuestDialogueOrSendNextPage(int dialogActionId, Player player, Npc npc, int questId, int extendedRewardIndex) {
+		QuestEnv env = new QuestEnv(npc, player, questId, dialogActionId);
+		env.setExtendedRewardIndex(extendedRewardIndex);
+		if (QuestEngine.getInstance().onDialog(env))
+			return;
+		// action id = next page id
+		PacketSendUtility.sendPacket(player, new SM_DIALOG_WINDOW(npc.getObjectId(), dialogActionId, questId));
 	}
 
 	private static void sendDialogWindow(int dialogActionId, final Player player, Npc npc) {