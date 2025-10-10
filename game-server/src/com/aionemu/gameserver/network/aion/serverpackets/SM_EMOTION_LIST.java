c2bfc12cf1c2c98343509939eaf40ba4b9437360
Neon <neon@beyond-aion.com>
2025-10-10 17:12:48 +0200

    Fixed missing emotions with MembershipConfig.EMOTIONS_ALL

--- game-server/src/com/aionemu/gameserver/network/aion/serverpackets/SM_EMOTION_LIST.java
+++ game-server/src/com/aionemu/gameserver/network/aion/serverpackets/SM_EMOTION_LIST.java
@@ -2,19 +2,15 @@ package com.aionemu.gameserver.network.aion.serverpackets;
 
 import java.util.Collection;
 
-import com.aionemu.gameserver.configs.main.MembershipConfig;
 import com.aionemu.gameserver.model.gameobjects.player.emotion.Emotion;
 import com.aionemu.gameserver.network.aion.AionConnection;
 import com.aionemu.gameserver.network.aion.AionServerPacket;
 
 public class SM_EMOTION_LIST extends AionServerPacket {
 
-	byte action;
-	Collection<Emotion> emotions;
+	private final byte action;
+	private final Collection<Emotion> emotions;
 
-	/**
-	 * @param action
-	 */
 	public SM_EMOTION_LIST(byte action, Collection<Emotion> emotions) {
 		this.action = action;
 		this.emotions = emotions;
@@ -23,18 +19,10 @@ public class SM_EMOTION_LIST extends AionServerPacket {
 	@Override
 	protected void writeImpl(AionConnection con) {
 		writeC(action);
-		if (con.getActivePlayer().hasPermission(MembershipConfig.EMOTIONS_ALL)) {
-			writeH(86);
-			for (int i = 0; i < 86; i++) {
-				writeD(64 + i);
-				writeH(0x00);
-			}
-		} else {
-			writeH(emotions.size());
-			for (Emotion emotion : emotions) {
-				writeD(emotion.getId());
-				writeH(emotion.secondsUntilExpiration());
-			}
+		writeH(emotions.size());
+		for (Emotion emotion : emotions) {
+			writeD(emotion.getId());
+			writeH(emotion.secondsUntilExpiration());
 		}
 	}
 }
