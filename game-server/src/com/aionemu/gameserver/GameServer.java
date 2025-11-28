937026f5be02d7ad6371e40757023d5ac041631d
Neon <neon@beyond-aion.com>
2025-11-28 22:44:47 +0100

    Refactored logging setup

--- game-server/src/com/aionemu/gameserver/GameServer.java
+++ game-server/src/com/aionemu/gameserver/GameServer.java
@@ -1,27 +1,16 @@
 package com.aionemu.gameserver;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.lang.management.ManagementFactory;
-import java.nio.file.*;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.TimeZone;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.Stream;
-import java.util.zip.Deflater;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.aionemu.commons.configuration.transformers.PropertyTransformers;
 import com.aionemu.commons.database.DatabaseFactory;
+import com.aionemu.commons.logging.Logging;
 import com.aionemu.commons.network.NioServer;
 import com.aionemu.commons.network.ServerCfg;
 import com.aionemu.commons.utils.concurrent.UncaughtExceptionHandler;
@@ -72,8 +61,6 @@ import com.aionemu.gameserver.world.World;
 import com.aionemu.gameserver.world.geo.GeoService;
 import com.aionemu.gameserver.world.zone.ZoneService;
 
-import ch.qos.logback.classic.ClassicConstants;
-
 /**
  * <tt>GameServer</tt> is the main class of the application and represents the whole game server.<br>
  * This class is also an entry point with main() method.
@@ -83,8 +70,7 @@ import ch.qos.logback.classic.ClassicConstants;
 public class GameServer {
 
 	static {
-		System.setProperty(ClassicConstants.CONFIG_FILE_PROPERTY, "config/logback.xml"); // must be set before instantiating any logger
-		archiveLogs(); // must also run before instantiating any logger
+		Logging.init(); // must run before instantiating any logger
 	}
 	private static final Logger log = LoggerFactory.getLogger(GameServer.class);
 
@@ -103,60 +89,7 @@ public class GameServer {
 	private GameServer() {
 	}
 
-	private static void archiveLogs() {
-		try {
-			Path logFolder = Paths.get("./log");
-			Path oldLogsFolder = Paths.get(logFolder + "/archived");
-			List<File> files = new ArrayList<>();
-			File serverStartTimeFile = new File("./log/[server_start_marker]");
-			long serverStartTime;
-			long[] serverEndTime = { 0 }; // for mutability within a stream (file walker), we need to use an array here
-
-			Files.createDirectories(serverStartTimeFile.toPath().getParent());
-			serverStartTimeFile.createNewFile(); // creates the file only if it does not exists
-			serverStartTime = serverStartTimeFile.lastModified();
-			serverStartTimeFile.setLastModified(ManagementFactory.getRuntimeMXBean().getStartTime()); // update with new server start time
-
-			Files.createDirectories(logFolder);
-			Files.walkFileTree(logFolder, new SimpleFileVisitor<>() {
-
-				@Override
-				public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
-					if (!attrs.isDirectory() && file.toString().toLowerCase().endsWith(".log")) {
-						files.add(file.toFile());
-						if (serverEndTime[0] < attrs.lastModifiedTime().toMillis())
-							serverEndTime[0] = attrs.lastModifiedTime().toMillis();
-					}
-					return FileVisitResult.CONTINUE;
-				}
-			});
-
-			if (!files.isEmpty()) {
-				Files.createDirectories(oldLogsFolder);
-				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH.mm");
-				String outFilename = (serverStartTime < serverEndTime[0] ? sdf.format(serverStartTime) : "Unknown") + " to " + sdf.format(serverEndTime[0]) + ".zip";
-				try (ZipOutputStream out = new ZipOutputStream(new FileOutputStream(oldLogsFolder + "/" + outFilename))) {
-					out.setMethod(ZipOutputStream.DEFLATED);
-					out.setLevel(Deflater.BEST_COMPRESSION);
-					for (File logFile : files) {
-						try (FileInputStream in = new FileInputStream(logFile)) {
-							out.putNextEntry(new ZipEntry(logFolder.relativize(logFile.toPath()).toString()));
-							in.transferTo(out);
-							out.closeEntry();
-						}
-					}
-				}
-				for (File logFile : files) { // remove files after successful archiving
-					logFile.delete();
-					logFile.getParentFile().delete(); // attempt to delete the parent directory (only succeeds if empty)
-				}
-			}
-		} catch (IOException | SecurityException e) {
-			throw new RuntimeException("Error gathering and archiving old logs, shutting down...", e);
-		}
-	}
-
-	public static void main(String[] args) {
+	static void main() {
 		JAXBUtil.preLoadContextAsync(StaticData.class); // do this early so DataManager doesn't need to wait as long
 		initUtilityServicesAndConfig();
 
