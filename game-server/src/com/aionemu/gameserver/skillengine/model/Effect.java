30ff90dd0a100053f9828b00e5cb98453691f54e
crlfrancis <cftp_14@hotmail.com>
2025-12-15 23:32:02 -0400

    Fixed Shield of Vengeance reflected buffs and heals (#89)
    Co-authored-by: Neon <neon@beyond-aion.com>

--- game-server/src/com/aionemu/gameserver/skillengine/model/Effect.java
+++ game-server/src/com/aionemu/gameserver/skillengine/model/Effect.java
@@ -351,6 +351,8 @@ public class Effect implements StatOwner {
 	}
 
 	public void setShieldDefense(int shieldDefense) {
+		if ((shieldDefense & ShieldType.SKILL_REFLECTOR.getId()) != 0 && getSkillSubType() != SkillSubType.ATTACK && getSkillSubType() != SkillSubType.DEBUFF)
+			shieldDefense &= ~ShieldType.SKILL_REFLECTOR.getId(); // disable SKILL_REFLECTOR bit (only attack type effects can reflect whole effects)
 		this.shieldDefense = shieldDefense;
 	}
 
