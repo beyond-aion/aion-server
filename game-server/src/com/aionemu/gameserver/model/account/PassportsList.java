f0626d8e0596b8788e86bb08b0451539886e286d
SVDNESS <167009133+SVDNESS@users.noreply.github.com>
2025-12-26 06:20:53 +0300

    Atreian Passport System Fixed
    Atreian Passport System turned out to be incomplete, and there were several critical bugs related to receiving rewards (double or infinite), as well as problems with synchronizing the time record in the database.
    There was no implementation of attributes such as "reward_permit_level" and "reward_item_expire_time", as well as no implementation for the "ANNIVERSARY" type.
    Fixed all the errors and implemented the system to the end, as it should work for retail.

--- game-server/src/com/aionemu/gameserver/model/account/PassportsList.java
+++ game-server/src/com/aionemu/gameserver/model/account/PassportsList.java
@@ -1,43 +1,62 @@
 package com.aionemu.gameserver.model.account;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * @author ViAl
+ * @author SVDNESS
+ * @version 4.8 [JDK 25]
  */
+
 public class PassportsList {
+    private final List<Passport> passports;
 
-	private final List<Passport> passports;
+    public PassportsList() {
+        passports = new ArrayList<>();
+    }
 
-	public PassportsList() {
-		passports = new ArrayList<>();
-	}
+    public void addPassport(Passport passport) {
+        passports.add(passport);
+    }
 
-	public void addPassport(Passport passport) {
-		passports.add(passport);
-	}
+    public void removePassport(Passport passport) {
+        passports.remove(passport);
+    }
 
-	public void removePassport(Passport passport) {
-		passports.remove(passport);
-	}
+    public Passport getPassport(int passportId, int timestamp) {
+        for (var passport : this.passports) {
+            if (passport.getId() == passportId && passport.getArriveDate().getTime() / 1000 == timestamp) {
+                return passport;
+            }
+        }
+        return null;
+    }
 
-	public Passport getPassport(int passportId, int timestamp) {
-		for (Passport passport : this.passports)
-			if (passport.getId() == passportId && passport.getArriveDate().getTime() / 1000 == timestamp)
-				return passport;
-		return null;
-	}
+    public boolean isPassportPresent(int passportId) {
+        for (var pp : this.passports) {
+            if (pp.getId() == passportId) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	public boolean isPassportPresent(int passportId) {
-		for (Passport pp : this.passports) {
-			if (pp.getId() == passportId)
-				return true;
-		}
-		return false;
-	}
+    public List<Passport> getAllPassports() {
+        return passports;
+    }
 
-	public List<Passport> getAllPassports() {
-		return passports;
-	}
+    public boolean hasPassportForDay(int passportId, LocalDate attendDay) {
+        for (var pp : passports) {
+            if (pp.getId() != passportId) {
+                continue;
+            }
+            var ppDay = pp.getArriveDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+            if (ppDay.equals(attendDay)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
