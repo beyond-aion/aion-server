e344ce4f9cd30c0d2f92014e37769a71a55ca91f
Maximus <daddycaddy@gmail.com>
2025-12-29 09:48:48 +0800

    Quest Fixes

--- game-server/src/com/aionemu/gameserver/model/templates/item/actions/ExpExtractAction.java
+++ game-server/src/com/aionemu/gameserver/model/templates/item/actions/ExpExtractAction.java
@@ -3,11 +3,12 @@ package com.aionemu.gameserver.model.templates.item.actions;
 import javax.xml.bind.annotation.XmlAttribute;
 
 import com.aionemu.gameserver.controllers.observer.ItemUseObserver;
+import com.aionemu.gameserver.dataholders.DataManager;
 import com.aionemu.gameserver.model.TaskId;
 import com.aionemu.gameserver.model.gameobjects.Item;
 import com.aionemu.gameserver.model.gameobjects.player.Player;
+import com.aionemu.gameserver.model.gameobjects.player.PlayerCommonData;
 import com.aionemu.gameserver.network.aion.serverpackets.SM_ITEM_USAGE_ANIMATION;
-import com.aionemu.gameserver.network.aion.serverpackets.SM_STATUPDATE_EXP;
 import com.aionemu.gameserver.network.aion.serverpackets.SM_SYSTEM_MESSAGE;
 import com.aionemu.gameserver.services.item.ItemService;
 import com.aionemu.gameserver.utils.PacketSendUtility;
@@ -15,6 +16,7 @@ import com.aionemu.gameserver.utils.ThreadPoolManager;
 
 /**
  * @author Rolandas
+ * Fix by daddycaddy
  */
 public class ExpExtractAction extends AbstractItemAction {
 
@@ -27,22 +29,36 @@ public class ExpExtractAction extends AbstractItemAction {
 
 	@Override
 	public boolean canAct(Player player, Item parentItem, Item targetItem, Object... params) {
-		if (player.getCommonData().getExp() == 0) {
-			return false;
-		}
 		if (player.getInventory().isFull()) {
 			return false;
 		}
-		return true;
+
+		PlayerCommonData cd = player.getCommonData();
+
+		long expShown = cd.getExpShown();
+
+		int required;
+		if (isPercent) {
+			required = (int) ((long) cd.getExpNeed() * cost / 100L);
+		} else {
+			required = cost;
+		}
+
+		if (required <= 0) {
+			return false;
+		}
+
+		return expShown >= required;
 	}
 
 	@Override
 	public void act(final Player player, final Item parentItem, Item targetItem, Object... params) {
 		PacketSendUtility.sendPacket(player,
 			new SM_ITEM_USAGE_ANIMATION(player.getObjectId(), parentItem.getObjectId(), parentItem.getItemTemplate().getTemplateId(), 5000, 0, 0));
-		player.getController().cancelTask(TaskId.ITEM_USE);
-		final ItemUseObserver observer = new ItemUseObserver() {
 
+		player.getController().cancelTask(TaskId.ITEM_USE);
+
+		final ItemUseObserver observer = new ItemUseObserver() {
 			@Override
 			public void abort() {
 				player.getController().cancelTask(TaskId.ITEM_USE);
@@ -52,28 +68,49 @@ public class ExpExtractAction extends AbstractItemAction {
 				player.getObserveController().removeObserver(this);
 			}
 		};
+
 		player.getObserveController().attach(observer);
+
 		player.getController().addTask(TaskId.ITEM_USE, ThreadPoolManager.getInstance().schedule(new Runnable() {
 
 			@Override
 			public void run() {
 				player.getObserveController().removeObserver(observer);
-				int toDecrease = 0;
+
+				int toDecrease;
 				if (isPercent) {
-					toDecrease = (int) (player.getCommonData().getExpNeed() / 100) * cost;
+					toDecrease = (int) ((long) player.getCommonData().getExpNeed() * cost / 100L);
 				} else {
 					toDecrease = cost;
 				}
-				player.getCommonData().setExp(player.getCommonData().getExp() - toDecrease);
+
+				PlayerCommonData cd = player.getCommonData();
+				long currentExp = cd.getExp();
+				long levelStartExp = DataManager.PLAYER_EXPERIENCE_TABLE.getStartExpForLevel(cd.getLevel());
+
+				long newExp = currentExp - toDecrease;
+				if (newExp < levelStartExp) {
+					newExp = levelStartExp;
+				}
+
+				if (newExp == currentExp) {
+					player.getController().cancelTask(TaskId.ITEM_USE);
+					PacketSendUtility.sendPacket(player, SM_SYSTEM_MESSAGE.STR_DECOMPOSE_ITEM_INVALID_STANCE(parentItem.getL10n()));
+					PacketSendUtility.sendPacket(player,
+						new SM_ITEM_USAGE_ANIMATION(player.getObjectId(), parentItem.getObjectId(),
+							parentItem.getItemTemplate().getTemplateId(), 0, 2, 0));
+					return;
+				}
+
+				cd.setExp(newExp);
+
 				ItemService.addItem(player, itemId, 1);
 				player.getInventory().decreaseByItemId(parentItem.getItemId(), 1);
+
 				PacketSendUtility.sendPacket(player,
 					new SM_ITEM_USAGE_ANIMATION(player.getObjectId(), parentItem.getObjectId(), parentItem.getItemTemplate().getTemplateId(), 0, 1, 0));
 			}
 		}, 5000));
-		PacketSendUtility.sendPacket(player, new SM_STATUPDATE_EXP(player.getCommonData().getExpShown(), player.getCommonData().getExpRecoverable(),
-			player.getCommonData().getExpNeed(), player.getCommonData().getCurrentReposeEnergy(), player.getCommonData().getMaxReposeEnergy()));
 
 	}
-
 }
