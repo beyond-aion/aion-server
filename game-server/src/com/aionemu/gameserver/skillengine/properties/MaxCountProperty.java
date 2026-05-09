beyond-aion/aion-server

commit 256abae49338f641b3880a0857cfa86c6543a4cf

fixed #BA2236 some area skills affected one target less than they should https://beyond-aion.com/forum/thread/2236-gunner-aoes-1-target-zu-wenig/

game-server/src/com/aionemu/gameserver/skillengine/properties/MaxCountProperty.java
@@ -15,9 +15,11 @@ import com.aionemu.gameserver.utils.PositionUtil;
  */
 public class MaxCountProperty {
 
-	public static boolean set(final Skill skill, Properties properties) {
+	public static boolean set(Skill skill, Properties properties) {
 		TargetRangeAttribute value = properties.getTargetType();
 		int maxCount = properties.getTargetMaxCount();
+		if (properties.getFirstTarget() == FirstTargetAttribute.TARGET && value == TargetRangeAttribute.AREA) // firstTarget doesn't count on AREA skills (see skill 1245 or 16689)
+			maxCount += 1;
 		if (maxCount == 0 || skill.getEffectedList().size() <= maxCount)
 			return true;
 

commit 765b731f56c013f5f6dfb95c1cb9a484ea9a28c1

Fixed max target count of target-based area skills
This reverts commit 256abae4, which caused wrong target counts with skills like Illusion Chains or the ones originally reported. Verified on 4.6 PTS.

game-server/src/com/aionemu/gameserver/skillengine/properties/MaxCountProperty.java
@@ -16,8 +16,6 @@ public class MaxCountProperty {
 	public static boolean set(Properties properties, Properties.ValidationResult result) {
 		TargetRangeAttribute value = properties.getTargetType();
 		int maxCount = properties.getTargetMaxCount();
-		if (properties.getFirstTarget() == FirstTargetAttribute.TARGET && value == TargetRangeAttribute.AREA) // firstTarget doesn't count on AREA skills (see skill 1245 or 16689)
-			maxCount += 1;
 		if (maxCount == 0 || result.getTargets().size() <= maxCount)
 			return true;
 