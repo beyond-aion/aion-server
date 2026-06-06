7bd02e9d2ce8ecc56ecf8f42a4a6e5b74414983b
Peter Kozlovsky <birthright5050@gmail.com>
2026-06-06 05:21:38 +0300

    Always resist nofly/fall effects with INVULNERABLE_WING (#141)

--- game-server/src/com/aionemu/gameserver/skillengine/effect/FallEffect.java
+++ game-server/src/com/aionemu/gameserver/skillengine/effect/FallEffect.java
@@ -5,6 +5,7 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlType;
 
 import com.aionemu.gameserver.model.gameobjects.player.Player;
+import com.aionemu.gameserver.model.stats.container.StatEnum;
 import com.aionemu.gameserver.skillengine.model.Effect;
 
 /**
@@ -15,15 +16,17 @@ import com.aionemu.gameserver.skillengine.model.Effect;
 public class FallEffect extends EffectTemplate {
 
 	@Override
-	public void calculate(Effect effect) {
-		// Affects only players (for now as we dont have flying Npc's)
-		if (effect.getEffected() instanceof Player)
-			super.calculate(effect, null, null);
+	protected boolean isDodgedOrResisted(Effect effect, StatEnum statEnum) {
+		if (effect.getEffected().getEffectController().isInAnyAbnormalState(AbnormalState.INVULNERABLE_WING)) {
+			return true;
+		}
+		return super.isDodgedOrResisted(effect, statEnum);
 	}
 
 	@Override
 	public void applyEffect(Effect effect) {
-		if (!effect.getEffected().getEffectController().isInAnyAbnormalState(AbnormalState.INVULNERABLE_WING))
-			((Player) effect.getEffected()).getFlyController().endFly(true);
+		if (effect.getEffected() instanceof Player player) {
+			player.getFlyController().endFly(true);
+		}
 	}
 }
