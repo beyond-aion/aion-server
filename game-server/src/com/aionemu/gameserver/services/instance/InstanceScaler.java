c7918e016b3de3b2dad334ea6368d314713fb11b
Neon <neon@beyond-aion.com>
2026-08-03 01:59:32 +0200

    Added config options to InstaceScaler and set some default restrictions

--- game-server/src/com/aionemu/gameserver/services/instance/InstanceScaler.java
+++ game-server/src/com/aionemu/gameserver/services/instance/InstanceScaler.java
@@ -4,12 +4,15 @@ import static com.aionemu.gameserver.configs.main.InstanceConfig.*;
 
 import java.util.*;
 
+import com.aionemu.gameserver.dataholders.DataManager;
+import com.aionemu.gameserver.model.Race;
 import com.aionemu.gameserver.model.gameobjects.Npc;
 import com.aionemu.gameserver.model.gameobjects.player.Player;
 import com.aionemu.gameserver.model.stats.calc.Stat2;
 import com.aionemu.gameserver.model.stats.calc.StatOwner;
 import com.aionemu.gameserver.model.stats.calc.functions.StatFunction;
 import com.aionemu.gameserver.model.stats.container.StatEnum;
+import com.aionemu.gameserver.model.templates.InstanceCooltime;
 import com.aionemu.gameserver.utils.stats.CalculationType;
 import com.aionemu.gameserver.world.WorldMapInstance;
 
@@ -59,7 +62,7 @@ public class InstanceScaler implements StatOwner {
 	}
 
 	private static boolean shouldScale(Npc npc, WorldMapInstance instance) {
-		return !npc.isDead() && instance.getPlayersInside().stream().filter(p -> !p.isStaff()).findFirst().map(npc::isEnemyFrom).orElse(false);
+		return npc.getRating().ordinal() >= INSTANCE_SCALING_NPC_MIN_RATING.ordinal() && !npc.isDead() && instance.getPlayersInside().stream().filter(p -> !p.isStaff()).findFirst().map(npc::isEnemyFrom).orElse(false);
 	}
 
 	private static void scaleNpc(Npc npc, Scaling scaling) {
@@ -68,8 +71,10 @@ public class InstanceScaler implements StatOwner {
 			npc.getGameStats().addEffect(INSTANCE, scaling.statFunctions);
 	}
 
-	public static float calculateMultiplier(WorldMapInstance instance, float floor, int playerCount) {
-		return Math.max(floor, (float) Math.min(playerCount, instance.getMaxPlayers()) / instance.getMaxPlayers());
+	public static float calculateMultiplier(WorldMapInstance instance, float floor, float scaleFactor, int playerCount) {
+		float multi = (float) Math.min(playerCount, instance.getMaxPlayers()) / instance.getMaxPlayers();
+		multi = 1 - (1 - multi) * scaleFactor;
+		return Math.max(floor, multi);
 	}
 
 	static class Scaling {
@@ -78,7 +83,10 @@ public class InstanceScaler implements StatOwner {
 		private List<InstanceScalerStatFunction> statFunctions = Collections.emptyList();
 
 		boolean update(WorldMapInstance instance) {
-			int playerCount = (int) instance.getPlayersInside().stream().filter(p -> !p.isStaff()).count();
+			List<Player> players = instance.getPlayersInside().stream().filter(p -> !p.isStaff()).toList();
+			int playerCount = players.size();
+			if (playerCount < instance.getMaxPlayers() && isLowLevelInstanceWithHighLevelPlayers(instance, players))
+				playerCount = instance.getMaxPlayers(); // disable scaling
 			if (this.playerCount >= playerCount)
 				return false;
 			this.playerCount = playerCount;
@@ -86,10 +94,22 @@ public class InstanceScaler implements StatOwner {
 			return true;
 		}
 
+		private boolean isLowLevelInstanceWithHighLevelPlayers(WorldMapInstance instance, List<Player> players) {
+			if (players.isEmpty())
+				return false;
+			int maxAllowedLevel = getInstanceEnterMinLevel(instance, players) + INSTANCE_SCALING_MAX_LEVEL_DIFF;
+			return players.stream().mapToInt(Player::getLevel).max().orElseThrow() > maxAllowedLevel;
+		}
+
+		private int getInstanceEnterMinLevel(WorldMapInstance instance, List<Player> players) {
+			InstanceCooltime ct = DataManager.INSTANCE_COOLTIME_DATA.getInstanceCooltimeByWorldId(instance.getMapId());
+			return players.stream().mapToInt(p -> p.getRace() == Race.ASMODIANS ? ct.getEnterMinLevelDark() : ct.getEnterMinLevelLight()).min().orElse(1);
+		}
+
 		private List<InstanceScalerStatFunction> createStatFunctions(WorldMapInstance instance, int playerCount) {
 			List<InstanceScalerStatFunction> statFunctions = new ArrayList<>();
-			float hpMulti = calculateMultiplier(instance, INSTANCE_SCALING_HP_FLOOR, playerCount);
-			float dmgMulti = calculateMultiplier(instance, INSTANCE_SCALING_DMG_FLOOR, playerCount);
+			float hpMulti = calculateMultiplier(instance, INSTANCE_SCALING_HP_FLOOR, INSTANCE_SCALING_HP_SCALE_FACTOR, playerCount);
+			float dmgMulti = calculateMultiplier(instance, INSTANCE_SCALING_DMG_FLOOR, INSTANCE_SCALING_DMG_SCALE_FACTOR, playerCount);
 			if (hpMulti != 1) {
 				statFunctions.add(new InstanceScalerStatFunction(StatEnum.MAXHP, hpMulti));
 			}
