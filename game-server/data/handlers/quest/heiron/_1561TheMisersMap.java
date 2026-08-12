64880e18ee12c8fe08ee03fd169348d3f4fa3f65
Birthright <birthright5050@gmail.com>
2026-08-12 19:18:40 +0300

    Remove hardcoded item use animations from quest handlers
    Quest start items play their real casting delay in QuestStartAction now, so the
    handlers own animation and hardcoded 3 second delay only duplicated it.

--- game-server/data/handlers/quest/heiron/_1561TheMisersMap.java
+++ game-server/data/handlers/quest/heiron/_1561TheMisersMap.java
@@ -5,15 +5,12 @@ import static com.aionemu.gameserver.model.DialogAction.*;
 import com.aionemu.gameserver.model.gameobjects.Item;
 import com.aionemu.gameserver.model.gameobjects.Npc;
 import com.aionemu.gameserver.model.gameobjects.player.Player;
-import com.aionemu.gameserver.network.aion.serverpackets.SM_ITEM_USAGE_ANIMATION;
 import com.aionemu.gameserver.questEngine.handlers.AbstractQuestHandler;
 import com.aionemu.gameserver.questEngine.handlers.HandlerResult;
 import com.aionemu.gameserver.questEngine.model.QuestEnv;
 import com.aionemu.gameserver.questEngine.model.QuestState;
 import com.aionemu.gameserver.questEngine.model.QuestStatus;
 import com.aionemu.gameserver.services.QuestService;
-import com.aionemu.gameserver.utils.PacketSendUtility;
-import com.aionemu.gameserver.utils.ThreadPoolManager;
 
 /**
  * Find the place marked on the map (182201728).
@@ -33,25 +30,16 @@ public class _1561TheMisersMap extends AbstractQuestHandler {
 	}
 
 	@Override
-	public HandlerResult onItemUseEvent(final QuestEnv env, Item item) {
+	public HandlerResult onItemUseEvent(QuestEnv env, Item item) {
 		env.setQuestId(questId);
-		final Player player = env.getPlayer();
-		final int id = item.getItemTemplate().getTemplateId();
-		final int itemObjId = item.getObjectId();
+		Player player = env.getPlayer();
+		int id = item.getItemTemplate().getTemplateId();
 		QuestState qs = player.getQuestStateList().getQuestState(questId);
 
 		if (qs == null || qs.isStartable()) {
-			PacketSendUtility.broadcastPacket(player, new SM_ITEM_USAGE_ANIMATION(player.getObjectId(), itemObjId, id, 3000, 0, 0), true);
-			ThreadPoolManager.getInstance().schedule(new Runnable() {
-
-				@Override
-				public void run() {
-					PacketSendUtility.broadcastPacket(player, new SM_ITEM_USAGE_ANIMATION(player.getObjectId(), itemObjId, id, 0, 1, 0), true);
-					removeQuestItem(env, id, 1);
-					QuestService.startQuest(env);
-					// sendQuestDialog(env, 4);
-				}
-			}, 3000);
+			removeQuestItem(env, id, 1);
+			QuestService.startQuest(env);
+			// sendQuestDialog(env, 4);
 			return HandlerResult.SUCCESS;
 		}
 		return HandlerResult.UNKNOWN;
--- game-server/data/handlers/quest/morheim/_2324TheSpiritsNotebook.java
+++ game-server/data/handlers/quest/morheim/_2324TheSpiritsNotebook.java
@@ -6,14 +6,12 @@ import com.aionemu.gameserver.model.gameobjects.Item;
 import com.aionemu.gameserver.model.gameobjects.Npc;
 import com.aionemu.gameserver.model.gameobjects.player.Player;
 import com.aionemu.gameserver.network.aion.serverpackets.SM_DIALOG_WINDOW;
-import com.aionemu.gameserver.network.aion.serverpackets.SM_ITEM_USAGE_ANIMATION;
 import com.aionemu.gameserver.questEngine.handlers.AbstractQuestHandler;
 import com.aionemu.gameserver.questEngine.handlers.HandlerResult;
 import com.aionemu.gameserver.questEngine.model.QuestEnv;
 import com.aionemu.gameserver.questEngine.model.QuestState;
 import com.aionemu.gameserver.questEngine.model.QuestStatus;
 import com.aionemu.gameserver.utils.PacketSendUtility;
-import com.aionemu.gameserver.utils.ThreadPoolManager;
 
 /**
  * @author MrPoke, Nephis
@@ -66,22 +64,12 @@ public class _2324TheSpiritsNotebook extends AbstractQuestHandler {
 	}
 
 	@Override
-	public HandlerResult onItemUseEvent(final QuestEnv env, Item item) {
-		final Player player = env.getPlayer();
-		final int id = item.getItemTemplate().getTemplateId();
-		final int itemObjId = item.getObjectId();
+	public HandlerResult onItemUseEvent(QuestEnv env, Item item) {
+		int id = item.getItemTemplate().getTemplateId();
 
 		if (id != 182204123)
 			return HandlerResult.UNKNOWN;
-		PacketSendUtility.broadcastPacket(player, new SM_ITEM_USAGE_ANIMATION(player.getObjectId(), itemObjId, id, 3000, 0, 0), true);
-		ThreadPoolManager.getInstance().schedule(new Runnable() {
-
-			@Override
-			public void run() {
-				PacketSendUtility.broadcastPacket(player, new SM_ITEM_USAGE_ANIMATION(player.getObjectId(), itemObjId, id, 0, 1, 0), true);
-				sendQuestDialog(env, 4);
-			}
-		}, 3000);
+		sendQuestDialog(env, 4);
 		return HandlerResult.SUCCESS;
 	}
 }
