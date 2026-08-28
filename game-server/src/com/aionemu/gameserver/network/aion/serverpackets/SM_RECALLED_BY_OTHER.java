589faaaf48986b40b76a1c67f6f0e94a15a04d96
Igor B. <167009133+SVDNESS@users.noreply.github.com>
2026-08-29 00:43:46 +0300

    Retail implementation of the Recall Instant effect (#177)

---
+++ game-server/src/com/aionemu/gameserver/network/aion/serverpackets/SM_RECALLED_BY_OTHER.java
@@ -0,0 +1,29 @@
+package com.aionemu.gameserver.network.aion.serverpackets;
+
+import com.aionemu.gameserver.network.aion.AionConnection;
+import com.aionemu.gameserver.network.aion.AionServerPacket;
+
+/**
+ * @author SVDNESS
+ */
+public class SM_RECALLED_BY_OTHER extends AionServerPacket {
+
+	public static final int RECALL_REQUEST_ID = 0x0F44;
+	private final String casterName;
+	private final int skillId;
+	private final int timeSeconds;
+
+	public SM_RECALLED_BY_OTHER(String casterName, int skillId, int timeSeconds) {
+		this.casterName = casterName;
+		this.skillId = skillId;
+		this.timeSeconds = timeSeconds;
+	}
+
+	@Override
+	protected void writeImpl(AionConnection con) {
+		writeC(0); //Retail always sends 0.
+		writeS(casterName);
+		writeH(skillId);
+		writeH(timeSeconds);
+	}
+}
