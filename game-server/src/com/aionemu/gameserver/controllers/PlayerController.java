924d4ed825c02efc68479cd2ce95510a3c546a83
Igor B. <167009133+SVDNESS@users.noreply.github.com>
2026-09-16 04:05:41 +0300

    SM_INVINCIBLE_TIME: implement retail packet (#182)

--- game-server/src/com/aionemu/gameserver/controllers/PlayerController.java
+++ game-server/src/com/aionemu/gameserver/controllers/PlayerController.java
@@ -83,6 +83,7 @@ import com.aionemu.gameserver.world.zone.ZoneName;
 public class PlayerController extends CreatureController<Player> {
 
 	private static final Logger log = LoggerFactory.getLogger(PlayerController.class);
+	private static final int PROTECTION_TIME = 60000;
 	private long lastAttackMillis = 0;
 	private long lastAttackedMillis = 0;
 	private long lastAutoAttackMillis = 0;
@@ -632,8 +633,9 @@ public class PlayerController extends CreatureController<Player> {
 			AttackUtil.cancelCastOn(getOwner());
 			AttackUtil.removeTargetFrom(getOwner());
 			PacketSendUtility.broadcastToSightedPlayers(getOwner(), new SM_PLAYER_STATE(getOwner()), true);
-			addTask(TaskId.PROTECTION_ACTIVE, ThreadPoolManager.getInstance().schedule(this::stopProtectionActiveTask, 60000));
 		}
+		PacketSendUtility.sendPacket(getOwner(), new SM_INVINCIBLE_TIME(PROTECTION_TIME));
+		addTask(TaskId.PROTECTION_ACTIVE, ThreadPoolManager.getInstance().schedule(this::stopProtectionActiveTask, PROTECTION_TIME));
 	}
 
 	/**
@@ -645,6 +647,7 @@ public class PlayerController extends CreatureController<Player> {
 		if (player.isSpawned()) {
 			player.unsetVisualState(CreatureVisualState.BLINKING);
 			PacketSendUtility.broadcastToSightedPlayers(player, new SM_PLAYER_STATE(player), true);
+			PacketSendUtility.sendPacket(player, new SM_INVINCIBLE_TIME(0));
 			notifyAIOnMove();
 		}
 	}
